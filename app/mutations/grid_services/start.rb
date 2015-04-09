d8428fb2f76b13d64f67f8a69915daf584d649fb
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-04-09 11:29:29 +0300

    add GridServices::Start specs

--- app/mutations/grid_services/start.rb
+++ app/mutations/grid_services/start.rb
@@ -9,14 +9,21 @@ module GridServices
       prev_state = self.grid_service.state
       begin
         self.grid_service.set_state('starting')
-        self.grid_service.containers.each do |container|
-          Docker::ContainerStarter.new(container).start_container
+        self.grid_service.containers.scoped.each do |container|
+          starter_for(container).start_container
         end
         self.grid_service.set_state('running')
       rescue => exc
         self.grid_service.set_state(prev_state)
         raise exc
       end
+    end
+
+    ##
+    # @param [Container] container
+    # @return [Docker::ContainerStarter]
+    def starter_for(container)
+      Docker::ContainerStarter.new(container)
     end
   end
 end
---
+++ spec/mutations/grid_services/start_spec.rb
@@ -0,0 +1,50 @@
+require_relative '../../spec_helper'
+
+describe GridServices::Start do
+  let(:user) { User.create!(email: 'joe@domain.com')}
+  let(:grid) {
+    grid = Grid.create!(name: 'test-grid')
+    grid.users << user
+    grid
+  }
+  let(:starter) { spy(:starter) }
+  let(:redis_service) { GridService.create(grid: grid, name: 'redis', image_name: 'redis:2.8')}
+  let(:subject) { described_class.new(current_user: user, grid_service: redis_service)}
+
+  describe '#run' do
+    it 'starts service containers' do
+      redis_service.containers.create!(name: 'redis-1-volume', container_id: '12', container_type: 'volume')
+      container = redis_service.containers.create!(name: 'redis-1', container_id: '34')
+      allow(starter).to receive(:start_container)
+      allow(subject).to receive(:starter_for).with(container).once.and_return(starter)
+
+      subject.run
+    end
+
+    it 'sets service state to running' do
+      allow(starter).to receive(:start_container)
+      redis_service.containers.create!(name: 'redis-1', container_id: '34')
+      allow(subject).to receive(:starter_for).and_return(starter)
+      subject.run
+      expect(redis_service.state).to eq('running')
+    end
+
+    it 'returns service to previous state if exception is raised' do
+      prev_state = redis_service.state
+      redis_service.containers.create!(name: 'redis-1', container_id: '34')
+      subject = described_class.new(current_user: user, grid_service: redis_service)
+      expect(subject).to receive(:starter_for).and_raise(StandardError.new('error'))
+      expect {
+        subject.run
+      }.to raise_exception
+      expect(redis_service.state).to eq(prev_state)
+    end
+  end
+
+  describe '#starter_for' do
+    it 'returns Docker::ContainerStarter' do
+      container = redis_service.containers.create!(name: 'redis-1', container_id: '34')
+      expect(subject.starter_for(container)).to be_instance_of(Docker::ContainerStarter)
+    end
+  end
+end
