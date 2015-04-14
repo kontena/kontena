4dca5546385473e9c9e2dd48893d29aa6c8c0177
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-04-14 20:18:30 +0300

    save container.name to container logs

--- spec/models/container_log_spec.rb
+++ spec/models/container_log_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 describe ContainerLog do
   it { should be_timestamped_document }
-  it { should have_fields(:type, :data)}
+  it { should have_fields(:type, :data, :name)}
 
   it { should belong_to(:grid) }
   it { should belong_to(:grid_service) }
@@ -12,5 +12,6 @@ describe ContainerLog do
   it { should have_index_for(container_id: 1) }
   it { should have_index_for(grid_id: 1) }
   it { should have_index_for(grid_service_id: 1) }
+  it { should have_index_for(name: 1) }
   it { should have_index_for(data: 'text') }
 end
