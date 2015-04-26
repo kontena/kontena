7b78971ed94849b972c9dcc3de36de8d8b971730
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-04-26 21:01:01 +0300

    cleanup

--- app/jobs/container_cleanup_job.rb
+++ app/jobs/container_cleanup_job.rb
@@ -6,7 +6,6 @@ class ContainerCleanupJob
   recurs { minutely }
 
   def perform
-    puts 'ASDASDASDASD'
     Container.where(:updated_at.lt => 2.minutes.ago).each do |c|
       if c.host_node && c.host_node.connected?
         c.mark_for_delete
