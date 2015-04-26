85151301302cfa561bff9e4762d57ca431c50a4a
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-04-26 10:28:57 +0300

    replace sidekiq with sucker_punch

--- app/jobs/container_cleanup_job.rb
+++ app/jobs/container_cleanup_job.rb
@@ -1,10 +1,12 @@
-class ContainerCleanupWorker
-  include Sidekiq::Worker
-  include Sidetiq::Schedulable
 
-  recurrence { minutely }
+class ContainerCleanupJob
+  include SuckerPunch::Job
+  include FistOfFury::Recurrent
+
+  recurs { minutely }
 
   def perform
+    puts 'ASDASDASDASD'
     Container.where(:updated_at.lt => 2.minutes.ago).each do |c|
       if c.host_node && c.host_node.connected?
         c.mark_for_delete
