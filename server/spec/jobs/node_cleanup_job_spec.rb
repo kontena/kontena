ca97d7899d06e843e6ed12bbf97d17f652dc11d2
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-05-22 16:14:14 +0300

    fix specs

--- server/spec/jobs/node_cleanup_job_spec.rb
+++ server/spec/jobs/node_cleanup_job_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../spec_helper'
-require 'sucker_punch/testing/inline'
 
 describe NodeCleanupJob do
   before(:each) { Celluloid.boot }
@@ -10,9 +9,9 @@ describe NodeCleanupJob do
       HostNode.create!(name: "node-1", connected: false, updated_at: 2.hours.ago)
       HostNode.create!(name: "node-2", connected: true, updated_at: 2.hours.ago)
       HostNode.create!(name: "node-3", connected: true, updated_at: 10.minutes.ago)
-      
+
       expect {
-        subject.async.perform
+        subject.perform
       }.to change{ HostNode.count }.by(-1)
     end
   end
