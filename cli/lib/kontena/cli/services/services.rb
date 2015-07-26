815a1c956525ff6f35c02120a977fc31234126b1
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-07-26 10:18:59 +0300

    do not show full id path when listing services

--- cli/lib/kontena/cli/services/services.rb
+++ cli/lib/kontena/cli/services/services.rb
@@ -15,7 +15,7 @@ module Kontena::Cli::Services
       puts "%-30.30s %-40.40s %-10s %-8s" % ['NAME', 'IMAGE', 'INSTANCES', 'STATEFUL']
       grids['services'].each do |service|
         state = service['stateful'] ? 'yes' : 'no'
-        puts "%-30.30s %-40.40s %-10.10s %-8s" % [service['id'], service['image'], service['container_count'], state]
+        puts "%-30.30s %-40.40s %-10.10s %-8s" % [service['name'], service['image'], service['container_count'], state]
       end
     end
 
