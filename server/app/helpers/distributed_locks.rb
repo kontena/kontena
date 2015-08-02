2b00a0315c09861101c003536106b01d8294eeba
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-08-02 21:34:48 +0300

    rename with_lock -> with_dlock

--- server/app/helpers/distributed_locks.rb
+++ server/app/helpers/distributed_locks.rb
@@ -2,7 +2,7 @@ module DistributedLocks
 
   # @param [String] name
   # @param [Integer] timeout
-  def with_lock(name, timeout = 10)
+  def with_dlock(name, timeout = 10)
     DistributedLock.with_lock(name, timeout) {
       yield
     }
