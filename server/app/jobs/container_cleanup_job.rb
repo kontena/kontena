b20c7455118fd861f4960e906afcc3f56cbffef7
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-08-03 20:31:04 +0300

    move locks to inside jobs

--- server/app/jobs/container_cleanup_job.rb
+++ server/app/jobs/container_cleanup_job.rb
@@ -1,13 +1,16 @@
 
 class ContainerCleanupJob
   include SuckerPunch::Job
+  include DistributedLocks
 
   def perform
-    Container.where(:updated_at.lt => 2.minutes.ago).each do |c|
-      if c.host_node && c.host_node.connected?
-        c.mark_for_delete
-      elsif c.host_node.nil?
-        c.destroy
+    with_dlock('container_cleanup_job', 0) do
+      Container.where(:updated_at.lt => 2.minutes.ago).each do |c|
+        if c.host_node && c.host_node.connected?
+          c.mark_for_delete
+        elsif c.host_node.nil?
+          c.destroy
+        end
       end
     end
   end
--- server/app/services/sucker_punch_scheduler.rb
+++ server/app/services/sucker_punch_scheduler.rb
@@ -1,7 +1,6 @@
 class SuckerPunchScheduler
   include Celluloid
   include Celluloid::Logger
-  include DistributedLocks
 
   def initialize
     @timers = []
@@ -12,22 +11,16 @@ class SuckerPunchScheduler
     CollectionIndexerJob.new.async.perform
 
     @timers << every(1.minute.to_i) do
-      with_dlock('container_cleanup_job', 0) {
-        info 'starting ContainerCleanupJob'
-        ContainerCleanupJob.new.async.perform
-      }
+      info 'starting ContainerCleanupJob'
+      ContainerCleanupJob.new.async.perform
     end
     @timers << every(5.minute.to_i) do
-      with_dlock('distributed_lock_cleanup', 0) {
-        info 'starting ContainerCleanupJob'
-        DistributedLockCleanupJob.new.async.perform
-      }
+      info 'starting ContainerCleanupJob'
+      DistributedLockCleanupJob.new.async.perform
     end
     @timers << every(1.hour.to_i) do
-      with_dlock('node_cleanup_job', 0) {
-        info 'starting NodeCleanupJob'
-        NodeCleanupJob.new.async.perform
-      }
+      info 'starting NodeCleanupJob'
+      NodeCleanupJob.new.async.perform
     end
 
     @timers
