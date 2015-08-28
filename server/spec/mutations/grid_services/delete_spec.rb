de938bc34b198817a7c46c95a88617f397a563cd
Lauri Nevala <lauri.nevala@gmail.com>
2015-08-28 09:59:25 +0300

    return error when trying to delete service that is linked to another service

--- server/spec/mutations/grid_services/delete_spec.rb
+++ server/spec/mutations/grid_services/delete_spec.rb
@@ -8,6 +8,14 @@ describe GridServices::Delete do
     grid
   }
   let(:redis_service) { GridService.create(grid: grid, name: 'redis', image_name: 'redis:2.8')}
+  let(:web_service) do
+    link = GridServiceLink.new(
+        linked_grid_service: redis_service,
+        alias: 'redis'
+    )
+    GridService.create(grid: grid, name: 'web', image_name: 'web:latest', grid_service_links: [link])
+
+  end
 
   describe '#run' do
     it 'deletes a service' do
@@ -33,6 +41,19 @@ describe GridServices::Delete do
       subject = described_class.new(current_user: user, grid_service: redis_service)
       expect(subject).to receive(:remover_for).with(container).once.and_return(remover)
       subject.run
+    end
+
+    context 'when service is linked to another service' do
+      it 'returns error' do
+        web_service
+
+        service = redis_service
+        expect {
+          outcome = described_class.new(current_user: user, grid_service: service).run
+          expect(outcome.success?).to be_falsey
+          expect(outcome.errors.message["service"]).to eq("Cannot delete service that is linked to another service (web)")
+        }.to change{ GridService.count }.by(0)
+      end
     end
   end
 end
