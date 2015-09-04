7fd85002c96199e88849e8d9e231cb1d58e8521b
Lauri Nevala <lauri.nevala@gmail.com>
2015-09-04 11:51:52 +0300

    implement restart command

---
+++ cli/lib/kontena/cli/nodes/aws/restart_command.rb
@@ -0,0 +1,28 @@
+module Kontena::Cli::Nodes::Aws
+  class RestartCommand < Clamp::Command
+    include Kontena::Cli::Common
+
+    parameter "NAME", "Node name"
+    option "--access-key", "ACCESS_KEY", "AWS access key", required: true
+    option "--secret-key", "SECRET_KEY", "AWS secret key", required: true
+    option "--region", "REGION", "Region", required: true
+
+    def execute
+      require_api_url
+      require_current_grid
+
+      require 'kontena/machine/aws'
+
+      client = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => access_key, :aws_secret_access_key => secret_key, :region => region)
+      instance = client.servers.all({'tag:kontena_name' => name}).first
+      if instance
+        instance.reboot
+        ShellSpinner "Restarting AWS instance #{name.colorize(:cyan)} " do
+          instance.wait_for { ready? }
+        end
+      else
+        abort "Cannot find droplet #{name.colorize(:cyan)} in DigitalOcean"
+      end
+    end
+  end
+end
