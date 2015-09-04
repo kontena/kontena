800d9a91bd420817b97773d0c320321981bb54da
Lauri Nevala <lauri.nevala@gmail.com>
2015-09-04 12:18:48 +0300

    ask user to install fog-aws gem instead of fog

--- cli/lib/kontena/machine/aws.rb
+++ cli/lib/kontena/machine/aws.rb
@@ -2,7 +2,7 @@ begin
   require "fog/aws"
 rescue LoadError
   puts "It seems that you don't have gem for AWS API installed."
-  puts "Install it using: gem install fog"
+  puts "Install it using: gem install fog-aws"
   exit 1
 end
 
