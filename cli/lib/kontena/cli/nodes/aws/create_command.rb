c0999a134412af9b281be61ce0285ebd302f20ff
Lauri Nevala <lauri.nevala@gmail.com>
2015-09-04 12:07:43 +0300

    update option descriptions

--- cli/lib/kontena/cli/nodes/aws/create_command.rb
+++ cli/lib/kontena/cli/nodes/aws/create_command.rb
@@ -3,12 +3,12 @@ module Kontena::Cli::Nodes::Aws
     include Kontena::Cli::Common
 
     option "--name", "NAME", "Node name"
-    option "--access-key", "ACCESS_KEY", "AWS access key", required: true
+    option "--access-key", "ACCESS_KEY", "AWS access key ID", required: true
     option "--secret-key", "SECRET_KEY", "AWS secret key", required: true
-    option "--region", "REGION", "Region", default: 'eu-west-1'
-    option "--key-pair", "SSH_KEY_PAIR", "AWS SSH key pair", required: true
+    option "--region", "REGION", "EC2 Region", default: 'eu-west-1'
+    option "--key-pair", "KEY_PAIR", "EC2 Key Pair", required: true
     option "--type", "SIZE", "Instance type", default: 't2.small'
-    option "--storage", "STORAGE", "Storage size (GB)", default: '30'
+    option "--storage", "STORAGE", "Storage size (GiB)", default: '30'
     option "--version", "VERSION", "Define installed Kontena version", default: 'latest'
 
     def execute
