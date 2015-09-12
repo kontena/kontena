fcd2555ead459dcf3587375930e25339f8c4a656
Lauri Nevala <lauri.nevala@gmail.com>
2015-09-12 12:37:16 +0300

    read services from Procfile and app.json

--- cli/lib/kontena/cli/apps/init_command.rb
+++ cli/lib/kontena/cli/apps/init_command.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'securerandom'
 
 module Kontena::Cli::Apps
   class InitCommand < Clamp::Command
@@ -20,7 +21,23 @@ module Kontena::Cli::Apps
       if File.exist?(docker_compose_file)
         puts "Found #{docker_compose_file}."
       else
-        create_docker_compose_yml if create_docker_compose_yml?
+        if File.exist?('Procfile')
+          procfile = YAML.load(File.read('Procfile'))
+        else
+          procfile = {}
+        end
+
+        if File.exist?('app.json')
+          app_json = File.read('app.json')
+          app_json = JSON.parse(app_json)
+          app_env = create_env_file(app_json)
+          addons = app_json['addons'] || []
+        else
+          app_env = nil
+          addons = []
+        end
+
+        create_docker_compose_yml(procfile, addons, app_env) if create_docker_compose_yml?
       end
 
       services = generate_kontena_services(docker_compose_file)
@@ -41,6 +58,24 @@ module Kontena::Cli::Apps
       %W(y yes #{''}).include? ask('Dockerfile not found. Do you want to create it? [Yn]: ').downcase
     end
 
+    def create_env_file(app_json)
+
+      if app_json['env']
+        app_env = File.new('.env', 'w')
+        app_json['env'].each do |key, env|
+          if env['generator'] == 'secret'
+            value = SecureRandom.hex(64)
+          else
+            value = env['value']
+          end
+          app_env.puts "#{key}=#{value}"
+        end
+        app_env.close
+        return '.env'
+      end
+      nil
+    end
+
     def current_user
       token = require_token
       client(token).get('user')
@@ -51,6 +86,7 @@ module Kontena::Cli::Apps
       dockerfile = File.new('Dockerfile', 'w')
       dockerfile.puts "FROM #{base_image}"
       dockerfile.puts "MAINTAINER #{current_user['email']}"
+      dockerfile.puts 'CMD ["/start", "web"]'
       dockerfile.close
     end
 
@@ -68,9 +104,29 @@ module Kontena::Cli::Apps
       %W(y yes #{''}).include? ask("#{docker_compose_file} not found. Do you want to create it? [Yn]: ").downcase
     end
 
-    def create_docker_compose_yml
+    def create_docker_compose_yml(procfile, addons, env_file)
       puts "Creating #{docker_compose_file.colorize(:cyan)}"
-      create_yml({'app' => { 'build' => '.'}}, docker_compose_file)
+      if procfile.keys.size > 0
+        docker_compose = {}
+        procfile.keys.each do |service|
+
+          docker_compose[service] = {'build' => '.', 'command' => "/start #{service}"}
+          docker_compose[service]['env_file'] = env_file if env_file
+          addons.each do |addon|
+            if valid_addons.has_key?(addon.split(":")[0])
+              docker_compose[service]['links'] = [] unless docker_compose[service]['links']
+              docker_compose[service]['links'] << "#{camelize(addon)}:#{camelize(addon)}"
+              docker_compose[service]['environment'] = [] unless docker_compose[service]['environment']
+              docker_compose[service]['environment'] += valid_addons[addon]['environment']
+              docker_compose[camelize(addon)] = {'image' => valid_addons[addon]['image']}
+            end
+          end
+        end
+      else
+        docker_compose = {'web' => { 'build' => '.'}}
+        docker_compose['web']['env_file'] = env_file if env_file
+      end
+      create_yml(docker_compose, docker_compose_file)
     end
 
     def generate_kontena_services(docker_compose = nil)
@@ -80,7 +136,7 @@ module Kontena::Cli::Apps
         compose_services.each do |name, options|
           services[name] = {'extends' => { 'file' => 'docker-compose.yml', 'service' => name }}
           if options.has_key?('build')
-            image = image_name || "registry.kontena.local/#{File.basename(Dir.getwd)}-#{name}:latest"
+            image = image_name || "registry.kontena.local/#{File.basename(Dir.getwd)}:latest"
             services[name]['image'] = image
             options.delete('build')
           end
@@ -95,6 +151,35 @@ module Kontena::Cli::Apps
       yml = File.new(file, 'w')
       yml.puts services.to_yaml
       yml.close
+    end
+
+    def valid_addons
+      {
+        'heroku-redis' => {
+          'image' => 'redis:latest',
+          'environment' => ['REDIS_URL=redis://herokuRedis:6379']
+        },
+        'rediscloud' => {
+          'image' => 'redis:latest',
+          'environment' => ['REDISCLOUD_URL=redis://rediscloud:6379']
+        },
+        'heroku-postgresql' => {
+          'image' => 'postgres:latest',
+          'environment' => ['DATABASE_URL=postgres://postgres:@herokuPostgresql:5432/postgres' ]
+        },
+        'mongolab' => {
+          'image' => 'mongo:latest',
+          'environment' => ['MONGOLAB_URI=mongolab:27017']
+        },
+        'memcachedcloud' => {
+          'image' => 'memcached:latest',
+          'enviroment' => ['MEMCACHEDCLOUD_SERVERS=memcachedcloud:11211']
+        }
+      }
+    end
+
+    def camelize(str)
+      str.split('-').inject([]){ |buffer,e| buffer.push(buffer.empty? ? e : e.capitalize) }.join
     end
 
   end
