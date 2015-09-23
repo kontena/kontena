03510bdeae883d812e4594d447abce2134828565
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-09-23 17:41:29 +0300

    fix agent specs

--- agent/spec/lib/kontena/rpc/docker_container_api_spec.rb
+++ agent/spec/lib/kontena/rpc/docker_container_api_spec.rb
@@ -18,7 +18,8 @@ describe Kontena::Rpc::DockerContainerApi do
       expect(Docker::Container).to receive(:create).with({
         'Image' => 'nginx:latest',
         'Entrypoint' => ['/w/w'],
-        'Cmd' => image.info['Config']['Cmd']
+        'Cmd' => image.info['Config']['Cmd'],
+        'HostConfig' => hash_including('VolumesFrom' => ['weavewait:ro'])
       }).and_return(spy(:container))
       opts = {
         'Image' => 'nginx:latest'
