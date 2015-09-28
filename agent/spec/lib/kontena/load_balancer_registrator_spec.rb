4a4739d9049e410f350afd385ea3e22d52acf1da
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-09-28 18:56:07 +0300

    add specs

--- agent/spec/lib/kontena/load_balancer_registrator_spec.rb
+++ agent/spec/lib/kontena/load_balancer_registrator_spec.rb
@@ -9,6 +9,14 @@ describe Kontena::LoadBalancerRegistrator do
   let(:event) { spy(:event, id: 'foobar', status: 'start') }
   let(:container) { spy(:container, id: '12345', info: {'Name' => 'test'}) }
 
+  describe '#initialize' do
+    it 'starts to listen container events' do
+      expect(subject).to receive(:on_container_event).once.with(event)
+      Kontena::Pubsub.publish('container:event', event)
+      sleep 0.05
+    end
+  end
+
   describe '#on_container_event' do
     it 'calls #register_container on start event' do
       allow(Docker::Container).to receive(:get).with(event.id).and_return(container)
@@ -59,6 +67,67 @@ describe Kontena::LoadBalancerRegistrator do
         })
       )
       expect(subject.etcd_running?).to eq(true)
+    end
+  end
+
+  describe '#register_container' do
+    let(:container) {
+      spy(:container, id: '12345', json: {
+        'Name' => 'test',
+        'Config' => {
+          'Labels' => {}
+        }
+      })
+    }
+    let(:http_container) {
+      spy(:container, id: '12345', json: {
+        'Name' => 'test',
+        'Config' => {
+          'Labels' => {
+            'io.kontena.load_balancer.name' => 'lb1',
+            'io.kontena.service.name' => 'web',
+            'io.kontena.container.name' => 'web-2',
+            'io.kontena.container.overlay_cidr' => '10.81.3.24/19',
+            'io.kontena.load_balancer.internal_port' => '8080',
+            'io.kontena.load_balancer.mode' => 'http'
+          }
+        }
+      })
+    }
+
+    it 'registers container ip:port to etcd' do
+      expect(subject.etcd).to receive(:set).with(anything, {value: '10.81.3.24:8080'})
+      subject.register_container(http_container)
+    end
+
+    it 'registers container info to cache' do
+      allow(subject.etcd).to receive(:set)
+      expect(subject.cache).to receive(:[]=).with(
+        http_container.id, hash_including(lb: 'lb1', service: 'web', container: 'web-2')
+      )
+      subject.register_container(http_container)
+    end
+
+    it 'does nothing if container does not have lb info' do
+      expect(subject.etcd).not_to receive(:set)
+      subject.register_container(container)
+    end
+  end
+
+  describe '#unregister_container' do
+    it 'unregisters container if id exists in cache' do
+      allow(subject.cache).to receive(:[]).with(event.id).and_return(true)
+      allow(subject.cache).to receive(:delete).with(event.id).and_return({
+        lb: 'lb1', service: 'web', container: 'web-2'
+      })
+
+      expect(subject.etcd).to receive(:delete)
+      subject.unregister_container(event.id)
+    end
+
+    it 'does nothing if id is not in cache' do
+      expect(subject.etcd).not_to receive(:delete)
+      subject.unregister_container(event.id)
     end
   end
 end
