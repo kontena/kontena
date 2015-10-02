fd1d77c415f96d4839bf61fc5db21c3ad159ca7d
Lauri Nevala <lauri.nevala@gmail.com>
2015-10-03 00:38:45 +0300

    implement Docker image build and push routines
    kontena app commands have undergone heave refactoring in order to make code cleaner

---
+++ cli/lib/kontena/cli/apps/docker_helper.rb
@@ -0,0 +1,28 @@
+module Kontena::Cli::Apps
+  module DockerHelper
+
+    def process_docker_images(services)
+      services.each do |name, service|
+        if service['build'] && !image_exist?(service['image'])
+          puts "Building image #{service['image'].colorize(:cyan)}"
+          build_docker_image(service['image'], service['build'])
+
+          puts "Pushing image #{service['image'].colorize(:cyan)} to registry"
+          push_docker_image(service['image'])
+        end
+      end
+    end
+
+    def build_docker_image(name, path)
+      system("docker build -t #{name} #{path}")
+    end
+
+    def push_docker_image(image)
+      system("docker push #{image}")
+    end
+
+    def image_exist?(image)
+      `docker history #{image} 2>&1` ; $?.success?
+    end
+  end
+end
