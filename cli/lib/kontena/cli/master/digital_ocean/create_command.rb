2bdd71156d4be4b556761a5330aab0bf30c4fdb7
Lauri Nevala <lauri.nevala@gmail.com>
2015-10-03 15:39:45 +0300

    implement DigitalOcean master provision

---
+++ cli/lib/kontena/cli/master/digital_ocean/create_command.rb
@@ -0,0 +1,30 @@
+module Kontena::Cli::Master::DigitalOcean
+  class CreateCommand < Clamp::Command
+    include Kontena::Cli::Common
+
+    option "--token", "TOKEN", "DigitalOcean API token", required: true
+    option "--ssh-key", "SSH_KEY", "Path to ssh public key", required: true
+    option "--ssl-cert", "SSL CERT", "SSL certificate file"
+    option "--size", "SIZE", "Droplet size", default: '1gb'
+    option "--region", "REGION", "Region", default: 'ams2'
+    option "--version", "VERSION", "Define installed Kontena version", default: 'latest'
+    option "--auth-provider-url", "AUTH_PROVIDER_URL", "Define authentication provider url"
+
+
+    def execute
+
+      require 'kontena/machine/digital_ocean'
+
+      provisioner = Kontena::Machine::DigitalOcean::MasterProvisioner.new(token)
+      provisioner.run!(
+          ssh_key: ssh_key,
+          ssl_cert: ssl_cert,
+          size: size,
+          region: region,
+          version: version,
+          auth_server: auth_provider_url,
+      )
+    end
+
+  end
+end
--- cli/lib/kontena/cli/master_command.rb
+++ cli/lib/kontena/cli/master_command.rb
@@ -1,8 +1,10 @@
 require_relative 'master/vagrant_command'
+require_relative 'master/digital_ocean_command'
 
 class Kontena::Cli::MasterCommand < Clamp::Command
 
   subcommand "vagrant", "Vagrant specific commands", Kontena::Cli::Master::VagrantCommand
+  subcommand "digitalocean", "DigitalOcean specific commands", Kontena::Cli::Master::DigitalOceanCommand
 
   def execute
   end
