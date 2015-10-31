ba58c490e3d7ae82b0f8a877e2c3ddbeb13d5b07
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2015-10-31 12:02:41 +0200

    fixes vagrant start command, which referenced non-existent name variable

--- cli/lib/kontena/cli/master/vagrant/start_command.rb
+++ cli/lib/kontena/cli/master/vagrant/start_command.rb
@@ -5,9 +5,9 @@ module Kontena::Cli::Master::Vagrant
     def execute
       require 'kontena/machine/vagrant'
       vagrant_path = "#{Dir.home}/.kontena/vagrant_master"
-      abort("Cannot find Vagrant node #{name}".colorize(:red)) unless Dir.exist?(vagrant_path)
+      abort("Cannot find Vagrant node kontena-master".colorize(:red)) unless Dir.exist?(vagrant_path)
       Dir.chdir(vagrant_path) do
-        ShellSpinner "Starting Vagrant machine #{name.colorize(:cyan)} " do
+        ShellSpinner "Starting Vagrant machine kontena-master " do
           Open3.popen2('vagrant up') do |stdin, output, wait|
             while o = output.gets
               print o if ENV['DEBUG']
