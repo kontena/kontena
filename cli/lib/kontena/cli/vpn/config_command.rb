d0fcc8fe6bb83d22220d35dbf9f54772c2ffe6d5
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-10-31 13:55:20 +0200

    append resolv-conf lines in linux

--- cli/lib/kontena/cli/vpn/config_command.rb
+++ cli/lib/kontena/cli/vpn/config_command.rb
@@ -3,10 +3,22 @@ module Kontena::Cli::Vpn
     include Kontena::Cli::Common
 
     def execute
+      require 'rbconfig'
       require_api_url
       payload = {cmd: ['/usr/local/bin/ovpn_getclient', 'KONTENA_VPN_CLIENT']}
       stdout, stderr = client(require_token).post("containers/#{current_grid}/vpn/vpn-1/exec", payload)
+      if linux?
+        stdout << "\n"
+        stdout << "up /etc/openvpn/update-resolv-conf\n"
+        stdout << "down /etc/openvpn/update-resolv-conf\n"
+      end
       puts stdout
+    end
+
+    # @return [Boolean]
+    def linux?
+      host_os = RbConfig::CONFIG['host_os']
+      host_os.include?('linux')
     end
   end
 end
