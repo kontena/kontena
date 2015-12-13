19bbd0d9230cf183175e4a1d6d7cc4b2e494c9ce
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2015-12-13 14:43:25 +0200

    read secret from stdin if no value is provided as a parameter

--- cli/lib/kontena/cli/vault/write_command.rb
+++ cli/lib/kontena/cli/vault/write_command.rb
@@ -2,15 +2,20 @@ module Kontena::Cli::Vault
   class WriteCommand < Clamp::Command
     include Kontena::Cli::Common
 
-    parameter "NAME", "Secret name"
-    parameter "VALUE", "Secret value"
+    parameter 'NAME', 'Secret name'
+    parameter '[VALUE]', 'Secret value'
 
     def execute
       require_api_url
       token = require_token
+      secret = value
+      if secret.to_s == ''
+        secret = STDIN.read
+      end
+      abort('No value provided') if secret.to_s == ''
       data = {
           name: name,
-          value: value
+          value: secret
       }
       client(token).post("grids/#{current_grid}/secrets", data)
     end
