7c53c93bd87228c93642f1c3b0963d5d8c2531a7
Hans Donner <hdonner@nonred.nl>
2015-12-16 23:48:39 +0100

    node list --all to show all nodes for all grids

--- cli/lib/kontena/cli/nodes/list_command.rb
+++ cli/lib/kontena/cli/nodes/list_command.rb
@@ -2,26 +2,52 @@ module Kontena::Cli::Nodes
   class ListCommand < Clamp::Command
     include Kontena::Cli::Common
 
+    option ["--all"], :flag, "List nodes for all grids", default: false
+
     def execute
       require_api_url
       require_current_grid
       token = require_token
 
-      grids = client(token).get("grids/#{current_grid}/nodes")
-      puts "%-30s %-40s %-15s %-30s %-10s" % ['Name', 'OS', 'Driver', 'Labels', 'Status']
-      grids['nodes'].each do |node|
-        if node['connected']
-          status = 'online'
-        else
-          status = 'offline'
+      if all?
+        grids = client(token).get("grids")
+        puts "%-30s %-30s %-40s %-15s %-30s %-10s" % [ 'Grid', 'Name', 'OS', 'Driver', 'Labels', 'Status']
+
+        grids['grids'].each do |grid|
+          nodes = client(token).get("grids/#{grid['name']}/nodes")
+          nodes['nodes'].each do |node|
+            if node['connected']
+              status = 'online'
+            else
+              status = 'offline'
+            end
+            puts "%-30.30s %-30.30s %-40.40s %-15s %-30.30s %-10s" % [
+              grid['name'],
+              node['name'],
+              "#{node['os']} (#{node['kernel_version']})",
+              node['driver'],
+              (node['labels'] || ['-']).join(","),
+              status
+            ]
+          end
         end
-        puts "%-30.30s %-40.40s %-15s %-30.30s %-10s" % [
-          node['name'],
-          "#{node['os']} (#{node['kernel_version']})",
-          node['driver'],
-          (node['labels'] || ['-']).join(","),
-          status
-        ]
+      else
+        nodes = client(token).get("grids/#{current_grid}/nodes")
+        puts "%-30s %-40s %-15s %-30s %-10s" % ['Name', 'OS', 'Driver', 'Labels', 'Status']
+        nodes['nodes'].each do |node|
+          if node['connected']
+            status = 'online'
+          else
+            status = 'offline'
+          end
+          puts "%-30.30s %-40.40s %-15s %-30.30s %-10s" % [
+            node['name'],
+            "#{node['os']} (#{node['kernel_version']})",
+            node['driver'],
+            (node['labels'] || ['-']).join(","),
+            status
+          ]
+        end
       end
     end
   end
