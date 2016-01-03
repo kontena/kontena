dd31e68ed37df59e896138c026489d319afeb7ae
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-01-03 18:33:31 +0200

    initial support for running master on heroku

--- server/db/migrations/03_migrate_container_logs_to_capped.rb
+++ server/db/migrations/03_migrate_container_logs_to_capped.rb
@@ -2,10 +2,11 @@ class MigrateContainerLogsToCapped < Mongodb::Migration
 
   def self.up
     unless ContainerLog.collection.capped?
+      size = (ENV['CONTAINER_LOGS_CAPPED_SIZE'] || 5000).to_i
       ContainerLog.collection.session.command(
         convertToCapped: ContainerLog.collection.name,
         capped: true,
-        size: 5.gigabytes
+        size: size.megabytes
       )
     end
   end
