7295faafe4783b371524157bd7c6deb7b4d52554
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-02-07 10:37:31 +0200

    improve master use sub-command output/information

--- cli/lib/kontena/cli/master/use_command.rb
+++ cli/lib/kontena/cli/master/use_command.rb
@@ -8,8 +8,18 @@ module Kontena::Cli::Master
       master = find_master_by_name(name)
       if !master.nil?
         self.current_master = master['name']
-        puts "Using master: #{master['name'].cyan}"
-        puts "URL: #{master['url'].cyan}"
+        puts "Using master: #{master['name'].cyan} (#{master['url']})"
+        puts "Using grid: #{current_grid.cyan}" if current_grid
+        grids = client(require_token).get('grids')['grids']
+        if grids.size > 1
+          puts ""
+          puts "You have access to following grids and can switch between them using 'kontena grid use <name>'"
+          puts ""
+          grids.each do |grid|
+            puts "  * #{grid['name']}"
+          end
+          puts ""
+        end
       else
         abort "Could not resolve master by name [#{name}]. For a list of known masters please run: kontena master list".colorize(:red)
       end
