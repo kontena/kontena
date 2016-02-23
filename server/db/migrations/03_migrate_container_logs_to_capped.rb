ed3bb6dc150cfc310e7c43a3c73a9391aae077fc
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-02-23 08:01:22 +0200

    make default logs/stats capped collection smaller

--- server/db/migrations/03_migrate_container_logs_to_capped.rb
+++ server/db/migrations/03_migrate_container_logs_to_capped.rb
@@ -2,7 +2,7 @@ class MigrateContainerLogsToCapped < Mongodb::Migration
 
   def self.up
     unless ContainerLog.collection.capped?
-      size = (ENV['CONTAINER_LOGS_CAPPED_SIZE'] || 5000).to_i
+      size = (ENV['CONTAINER_LOGS_CAPPED_SIZE'] || 1000).to_i
       ContainerLog.collection.session.command(
         convertToCapped: ContainerLog.collection.name,
         capped: true,
