8546f744d66537d0843080fabdbb2d6a01d7c9a4
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-04-16 10:39:53 +0300

    add memory filter to scheduler

---
+++ server/app/services/scheduler/filter/memory.rb
@@ -0,0 +1,54 @@
+module Scheduler
+  module Filter
+    class Memory
+
+      ##
+      # @param [GridService] service
+      # @param [Integer] instance_number
+      # @param [Array<HostNode>] nodes
+      # @return [Array<HostNode>]
+      def for_service(service, instance_number, nodes)
+        candidates = nodes.dup
+        memory = service.memory || service.memory_swap
+        unless memory
+          container = service.containers.first
+          if container
+            stats = container.container_stats.last
+            memory = stats.memory['usage'] * 1.25
+          end
+        end
+
+        return candidates unless memory # we cannot calculate so let's return all candidates
+
+        instance_name = "#{service.name}-#{instance_number}"
+        candidates.delete_if{|c|
+          reject_candidate?(c, memory, instance_name)
+        }
+
+        candidates
+      end
+
+      # @param [HostNode] candidate
+      # @param [Float] memory
+      # @param [String] instance_name
+      def reject_candidate?(candidate, memory, instance_name)
+        return true if candidate.mem_total.to_i < memory
+
+        node_stat = candidate.host_node_stats.last
+        return false if node_stat.nil?
+
+        all_used = node_stat.memory['total'] - node_stat.memory['free']
+        mem_used = all_used - (node_stat.memory['cached'] + node_stat.memory['buffers'])
+        mem_free = node_stat.memory['total'] - mem_used
+
+        if instance = candidate.containers.find_by(name: instance_name)
+          mem_free += memory
+        end
+
+        return true if mem_free < memory
+
+        false
+      end
+    end
+  end
+end
