5c439890bda3bf7673df4491e7188350eca28112
Lauri Nevala <lauri.nevala@gmail.com>
2016-04-19 16:16:36 +0300

    merge secrets when extending services

--- cli/lib/kontena/cli/apps/common.rb
+++ cli/lib/kontena/cli/apps/common.rb
@@ -75,7 +75,8 @@ module Kontena::Cli::Apps
     # @return [Hash]
     def extend_options(options, file, service_name, prefix)
       parent_options = parse_services(file, service_name, prefix)
-      options['environment'] = extend_env_vars(parent_options, options)
+      options['environment'] = extend_env_vars(parent_options['environment'], options['environment'])
+      options['secrets'] = extend_secrets(parent_options['secrets'], options['secrets'])
       parent_options.merge(options)
     end
 
@@ -86,17 +87,30 @@ module Kontena::Cli::Apps
       end
     end
 
-    # @param [Hash] from
-    # @param [Hash] to
+    # @param [Array] from
+    # @param [Array] to
     # @return [Array]
     def extend_env_vars(from, to)
-      env_vars = to['environment'] || []
-      if from['environment']
-        from['environment'].each do |env|
-          env_vars << env unless to['environment'] && to['environment'].find {|key| key.split('=').first == env.split('=').first}
+      env_vars = to || []
+      if from
+        from.each do |env|
+          env_vars << env unless to && to.find {|key| key.split('=').first == env.split('=').first}
         end
       end
       env_vars
+    end
+
+    # @param [Array] from
+    # @param [Array] to
+    # @return [Array]
+    def extend_secrets(from, to)
+      secrets = to || []
+      if from
+        from.each do |from_secret|
+          secrets << from_secret unless to && to.any? {|to_secret| to_secret['secret'] == from_secret['secret']}
+        end
+      end
+      secrets
     end
 
     # @param [Hash] services
