c38cac974a395158405508bc0b0d4e09f1ed8528
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-04-26 22:07:11 +0300

    load aws coreos amis dynamically from official feed

--- cli/lib/kontena/machine/aws/common.rb
+++ cli/lib/kontena/machine/aws/common.rb
@@ -6,19 +6,14 @@ module Kontena
         # @param [String] region
         # @return String
         def resolve_ami(region)
-          images = {
-              'eu-central-1' => 'ami-fee2fb92',
-              'ap-northeast-1' => 'ami-26033d48',
-              'us-gov-west-1' => 'ami-bdf04cdc',
-              'sa-east-1' => 'ami-10c5457c',
-              'ap-southeast-2' => 'ami-dc8baebf',
-              'ap-southeast-1' => 'ami-6969a50a',
-              'us-east-1' => 'ami-23260749',
-              'us-west-2' => 'ami-20927640',
-              'us-west-1' => 'ami-c2e490a2',
-              'eu-west-1' => 'ami-7e72c70d'
-          }
-          images[region]
+          response = Excon.get("https://coreos.com/dist/aws/aws-stable.json")
+          images = JSON.parse(response.body)
+          info = images[region]
+          if info
+            info['hvm']
+          else
+            nil
+          end
         end
 
         # @param [String] vpc_id
