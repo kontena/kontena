acd8a001aab5910d1218ca3af6caa83b02abb038
Mikko Piuhola <mikkopiu@users.noreply.github.com>
2016-05-09 09:23:15 +0300

    Add kontena master current command (#613)

--- cli/lib/kontena/cli/master_command.rb
+++ cli/lib/kontena/cli/master_command.rb
@@ -5,6 +5,7 @@ require_relative 'master/azure_command'
 require_relative 'master/use_command'
 require_relative 'master/list_command'
 require_relative 'master/users_command'
+require_relative 'master/current_command'
 
 class Kontena::Cli::MasterCommand < Clamp::Command
 
@@ -15,6 +16,7 @@ class Kontena::Cli::MasterCommand < Clamp::Command
   subcommand ["list", "ls"], "List masters where client has logged in", Kontena::Cli::Master::ListCommand
   subcommand "use", "Switch to use selected master", Kontena::Cli::Master::UseCommand
   subcommand "users", "Users specific commands", Kontena::Cli::Master::UsersCommand
+  subcommand "current", "Show current master details", Kontena::Cli::Master::CurrentCommand
   def execute
   end
 end
