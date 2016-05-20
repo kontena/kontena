bd69cf40a5051f5abd66b481a63563e7aaf7c8ae
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-05-20 15:49:01 +0300

    add support for ROLLBAR_ENVIRONMENT

--- server/app/initializers/rollbar.rb
+++ server/app/initializers/rollbar.rb
@@ -1,8 +1,9 @@
-if ENV['ROLLBAR_TOKEN'].to_s != ''
+unless ENV['ROLLBAR_TOKEN'].to_s.empty?
   require 'rollbar'
 
   Rollbar.configure do |config|
     config.access_token = ENV['ROLLBAR_TOKEN']
+    config.environment = ENV['ROLLBAR_ENVIRONMENT']
   end
 
   Celluloid.exception_handler { |ex| Rollbar.error(ex) }
