71344d1c1a44ede7f42ae99c93b993ae54acfd2a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-05-30 20:31:10 +0300

    fix specs

--- server/spec/mutations/host_nodes/remove_spec.rb
+++ server/spec/mutations/host_nodes/remove_spec.rb
@@ -9,21 +9,24 @@ describe HostNodes::Remove do
   let(:node_b) { HostNode.create!(name: 'node-b', grid: grid, node_id: 'BB') }
 
   describe '#run' do
+    let(:subject) { described_class.new(host_node: node_a) }
+    before(:each) { allow(subject).to receive(:worker).and_return(spy(:worker)) }
+
     it 'removes node' do
       node_a; node_b
       expect {
-        described_class.new(
-          host_node: node_a,
-        ).run
+        subject.run
       }.to change{ grid.host_nodes.count }.by(-1)
     end
 
+    it 'triggers grid_scheduler worker' do
+      expect(subject).to receive(:worker).once.with(:grid_scheduler)
+      subject.run
+    end
+
     it 'notifies grid nodes' do
-      mutation = described_class.new(
-        host_node: node_a,
-      )
-      expect(mutation).to receive(:notify_grid).once.with(grid)
-      mutation.run
+      expect(subject).to receive(:notify_grid).once.with(grid)
+      subject.run
     end
   end
 end
