810a4a9874899707528fc15613d98f2431beea7a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-06-01 18:03:19 +0300

    fix log_opts disappearing after service update

---
+++ cli/spec/kontena/cli/services/update_command_spec.rb
@@ -0,0 +1,56 @@
+require_relative "../../../spec_helper"
+require "kontena/cli/grid_options"
+require "kontena/cli/services/update_command"
+
+describe Kontena::Cli::Services::UpdateCommand do
+
+  include ClientHelpers
+
+  describe '#execute' do
+
+    before(:each) do
+      allow(subject).to receive(:update_service).and_return({})
+    end
+
+    it 'requires api url' do
+      expect(subject).to receive(:require_api_url).once
+      subject.run(['service'])
+    end
+
+    it 'requires token' do
+      expect(subject).to receive(:require_token).once
+      subject.run(['service'])
+    end
+
+    it 'sends update command' do
+      expect(subject).to receive(:update_service).with(token, 'service', {privileged: false})
+      subject.run(['service'])
+    end
+
+    it 'sends --cap-add' do
+      expect(subject).to receive(:update_service).with(token, 'service', hash_including(cap_add: ['NET_ADMIN']))
+      subject.run(['--cap-add', 'NET_ADMIN', 'service'])
+    end
+
+    it 'sends --cap-drop' do
+      expect(subject).to receive(:update_service).with(token, 'service', hash_including(cap_drop: ['MKNOD']))
+      subject.run(['--cap-drop', 'MKNOD', 'service'])
+    end
+
+    it 'sends --log-driver' do
+      expect(subject).to receive(:update_service).with(token, 'service', hash_including(log_driver: 'syslog'))
+      subject.run(['--log-driver', 'syslog', 'service'])
+    end
+
+    it 'sends --log-opt' do
+      expect(subject).to receive(:update_service).with(
+        token, 'service', hash_including(log_opts: {
+          'gelf-address'  => 'udp://log_forwarder-logstash_internal:12201'
+        })
+      )
+      subject.run([
+        '--log-opt', 'gelf-address=udp://log_forwarder-logstash_internal:12201', 'service'
+      ])
+    end
+  end
+end
