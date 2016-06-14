8d34fa56c464221741ca1ac247380cfa100e6004
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-06-14 18:54:49 +0300

    Enable eventmachine epoll (#804)

--- agent/lib/kontena/agent.rb
+++ agent/lib/kontena/agent.rb
@@ -91,6 +91,7 @@ module Kontena
     end
 
     def start_em
+      EM.epoll
       Thread.new { EventMachine.run } unless EventMachine.reactor_running?
       sleep 0.01 until EventMachine.reactor_running?
     end
