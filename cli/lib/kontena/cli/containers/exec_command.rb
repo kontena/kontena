5b5d94055c1fb38b71ee581f7cd04ca6bb44f812
Christoffer Hamberg <christoffer.hamberg@gmail.com>
2016-08-09 08:54:36 +0300

    Fix container exec command to handle whitespace and quoted strings correctly (#803)

--- cli/lib/kontena/cli/containers/exec_command.rb
+++ cli/lib/kontena/cli/containers/exec_command.rb
@@ -1,7 +1,11 @@
+require_relative '../grid_options'
+require_relative './containers_helper'
+
 module Kontena::Cli::Containers
   class ExecCommand < Clamp::Command
     include Kontena::Cli::Common
     include Kontena::Cli::GridOptions
+    include ContainersHelper
 
     parameter "CONTAINER_ID", "Container id"
     parameter "CMD ...", "Command"
@@ -10,9 +14,12 @@ module Kontena::Cli::Containers
       require_api_url
       token = require_token
 
-      payload = {cmd: ['sh', '-c', cmd_list.join(' ')]}
-      service_name = container_id.match(/(.+)-(\d+)/)[1]
+      cmd = build_command(cmd_list)
+      payload = {cmd: ["sh", "-c", cmd]}
+
+      service_name = container_id.match(/(.+)-(\d+)/)[1] rescue nil
       result = client(token).post("containers/#{current_grid}/#{service_name}/#{container_id}/exec", payload)
+
       puts result[0].join(" ") unless result[0].size == 0
       STDERR.puts result[1].join(" ") unless result[1].size == 0
       exit result[2]
