4b6f9ff809393a8310a2f5f6483e8ea6f4f220a1
Jussi Nummelin <jussi.nummelin@kontena.io>
2016-08-09 13:33:22 +0300

    added updated_at timestamp to secret listing

--- cli/lib/kontena/cli/vault/list_command.rb
+++ cli/lib/kontena/cli/vault/list_command.rb
@@ -10,10 +10,10 @@ module Kontena::Cli::Vault
       token = require_token
       result = client(token).get("grids/#{current_grid}/secrets")
 
-      column_width_paddings = '%-54s %-25.25s'
-      puts column_width_paddings % ['NAME', 'CREATED AT']
+      column_width_paddings = '%-54s %-25.25s %-25.25s'
+      puts column_width_paddings % ['NAME', 'CREATED AT', 'UPDATED AT']
       result['secrets'].sort_by { |s| s['name'] }.each do |secret|
-        puts column_width_paddings % [secret['name'], secret['created_at']]
+        puts column_width_paddings % [secret['name'], secret['created_at'], secret['updated_at']]
       end
     end
   end
