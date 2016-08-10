5add0c6c82f2c76ed38a3c5075fd04c056b2d921
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-08-10 15:30:44 +0300

    Update config helper to create class methods

--- server/app/helpers/config_helper.rb
+++ server/app/helpers/config_helper.rb
@@ -1,24 +1,41 @@
 module ConfigHelper
+
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+
+  def salt
+    self.class.salt
+  end
+
+  def encrypt(string)
+    self.class.encrypt(string)
+  end
+
   def config
-    return @config if @config
-    if Object::const_defined?('Server')
-      @config = Server.config
-    elsif Object::const_defined?('Configuration')
-      @config = Configuration
-    else
-      require 'ostruct'
-      @config = OpenStruct.new
+    self.class.config
+  end
+
+  module ClassMethods
+    def salt
+      return @salt if @salt
+      @salt = config[:salt] ||= BCrypt::Engine.generate_salt
+    end
+
+    def encrypt(string)
+      BCrypt::Engine.hash_secret(string, salt)
+    end
+    
+    def config
+      return @config if @config
+      if Object::const_defined?('Server')
+        @config = Server.config
+      elsif Object::const_defined?('Configuration')
+        @config = Configuration
+      else
+        require 'ostruct'
+        @config = OpenStruct.new
+      end
     end
   end
-
-  # Get or generate a global salt
-  def self.salt
-    return @salt if @salt
-    @salt = config[:salt] ||= BCrypt::Engine.generate_salt
-  end
-
-  def self.encrypt(string)
-    BCrypt::Engine.hash_secret(string, salt)
-  end
- 
 end
