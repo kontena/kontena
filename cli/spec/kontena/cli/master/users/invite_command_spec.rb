f0b7fd303a41160c45c7a13c21c34ccdb4ceabb7
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-08-15 10:39:10 +0300

    Change invite tests to reflect authentication changes

--- cli/spec/kontena/cli/master/users/invite_command_spec.rb
+++ cli/spec/kontena/cli/master/users/invite_command_spec.rb
@@ -12,8 +12,8 @@ describe Kontena::Cli::Master::Users::InviteCommand do
 
   describe "#invite" do
     it 'makes invitation request for all given users' do
-      expect(client).to receive(:post).with("users", {email: 'john@example.org'}).once
-      expect(client).to receive(:post).with("users", {email: 'jane@example.org'}).once
+      expect(client).to receive(:post).with("/oauth2/authorize", {email: 'john@example.org', response_type: "invite"}).once
+      expect(client).to receive(:post).with("/oauth2/authorize", {email: 'jane@example.org', response_type: "invite"}).once
 
       subject.run(['john@example.org', 'jane@example.org'])
     end
