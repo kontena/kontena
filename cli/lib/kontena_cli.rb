47815da8601e62c01c3900f6aa47520b37011e8a
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-08-22 11:50:59 +0300

    Load the Kontena::Command library on startup

--- cli/lib/kontena_cli.rb
+++ cli/lib/kontena_cli.rb
@@ -1,6 +1,7 @@
 module Kontena; end
 
 require 'ruby_dig'
+require_relative 'kontena/command'
 require_relative 'kontena/client'
 require_relative 'kontena/plugin_manager'
 require_relative 'kontena/main_command'
