ba2878f122be1e2050740fdce819fb1e9424ad93
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-08-29 13:09:56 +0300

    remove TTIN signal handler

--- agent/lib/kontena/agent.rb
+++ agent/lib/kontena/agent.rb
@@ -54,15 +54,6 @@ module Kontena
         EM.stop
         @supervisor.shutdown
         raise Interrupt
-      when 'TTIN'
-        Thread.list.each do |thread|
-          warn "Thread TID-#{thread.object_id.to_s(36)} #{thread['label']}"
-          if thread.backtrace
-            warn thread.backtrace.join("\n")
-          else
-            warn "no backtrace available"
-          end
-        end
       end
     end
 
