94ed356b7c2a20cfa796a4426523368a2aa6015b
Tero Marttila <terom@fixme.fi>
2016-09-09 16:18:16 +0300

    Reimplement app logs, with spec tests (#987)

--- cli/lib/kontena/cli/apps/logs_command.rb
+++ cli/lib/kontena/cli/apps/logs_command.rb
@@ -1,9 +1,12 @@
 require_relative 'common'
+require_relative '../helpers/log_helper'
 
 module Kontena::Cli::Apps
   class LogsCommand < Clamp::Command
     include Kontena::Cli::Common
     include Kontena::Cli::GridOptions
+    include Kontena::Cli::Helpers::LogHelper
+
     include Common
 
     option ['-f', '--file'], 'FILE', 'Specify an alternate Kontena compose file', attribute_name: :filename, default: 'kontena.yml'
@@ -13,60 +16,48 @@ module Kontena::Cli::Apps
     option ["-t", "--tail"], :flag, "Tail (follow) logs", default: false
     parameter "[SERVICE] ...", "Show only specified service logs"
 
-    attr_reader :services
-
     def execute
       require_config_file(filename)
 
-      @services = services_from_yaml(filename, service_list, service_prefix)
-      if services.size > 0
-        show_logs(services)
-      elsif !service_list.empty?
-        puts "No such service: #{service_list.join(', ')}".colorize(:red)
+      services = services_from_yaml(filename, service_list, service_prefix)
+
+      if services.empty? && !service_list.empty?
+        signal_error "No such service: #{service_list.join(', ')}"
+      elsif services.empty?
+        signal_error "No services for application"
       end
 
-    end
+      query_services = services.map{|service_name, opts| prefixed_name(service_name)}.join ','
+      query_params = {
+        services: query_services,
+        limit: lines,
+      }
+      query_params[:since] = since if since
 
-    def show_logs(services)
-      last_id = nil
-      loop do
-        query_params = []
-        query_params << "from=#{last_id}" unless last_id.nil?
-        query_params << "limit=#{lines}"
-        query_params << "since=#{since}" if !since.nil? && last_id.nil?
-        logs = []
-        services.each do |service_name, opts|
-          service = get_service(token, prefixed_name(service_name)) rescue false
-          result = client(token).get("services/#{service['id']}/container_logs?#{query_params.join('&')}") if service
-          logs = logs + result['logs'] if result && result['logs']
-        end
-        logs.sort!{|x,y| DateTime.parse(x['created_at']) <=> DateTime.parse(y['created_at'])}
-        logs.each do |log|
-          color = color_for_container(log['name'])
-          prefix = "#{log['created_at']} #{log['name']}:".colorize(color)
-          puts "#{prefix} #{log['data']}"
-          last_id = log['id']
-        end
-        break unless tail?
-        sleep(2)
+      if tail?
+        tail_logs(services, query_params)
+      else
+        show_logs(services, query_params)
       end
     end
 
-    def color_for_container(container_id)
-      color_maps[container_id] = colors.shift unless color_maps[container_id]
-      color_maps[container_id].to_sym
-    end
-
-    def color_maps
-      @color_maps ||= {}
-    end
-
-    def colors
-      if(@colors.nil? || @colors.size == 0)
-        @colors = [:green, :magenta, :yellow, :cyan, :red,
-                   :light_green, :light_yellow, :ligh_magenta, :light_cyan, :light_red]
+    def tail_logs(services, query_params)
+      stream_logs("grids/#{current_grid}/container_logs", query_params) do |log|
+        show_log(log)
       end
-      @colors
+    end
+
+    def show_logs(services, query_params)
+      result = client(token).get("grids/#{current_grid}/container_logs", query_params)
+      result['logs'].each do |log|
+        show_log(log)
+      end
+    end
+
+    def show_log(log)
+      color = color_for_container(log['name'])
+      prefix = "#{log['created_at']} #{log['name']}:".colorize(color)
+      puts "#{prefix} #{log['data']}"
     end
   end
 end
