654e2b942b6a13bc539a435d5c456d83fb6fe19a
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-09-15 12:54:19 +0300

    Mongo Pubsub subscription moved to boot

--- server/app/initializers/mongo_pubsub.rb
+++ server/app/initializers/mongo_pubsub.rb
@@ -2,6 +2,3 @@ require_relative 'mongoid'
 require_relative '../services/mongo_pubsub'
 require_relative '../models/pubsub_channel'
 
-unless ENV['RACK_ENV'] == 'test' || ENV['NO_MONGO_PUBSUB']
-  MongoPubsub.start!(PubsubChannel.collection)
-end
