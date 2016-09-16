b8b3ecae9aa10246c439f9d54f48725c61882d20
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-09-16 09:55:26 +0300

    Revert migration runner to that of PR #999

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -3,4 +3,6 @@ threads_count = Integer(ENV['MAX_THREADS'] || 8)
 threads threads_count, threads_count
 on_worker_boot do
   require_relative '../app/boot'
+  require_relative '../app/services/mongodb/migrator'
+  Mongodb::Migrator.new.migrate
 end
