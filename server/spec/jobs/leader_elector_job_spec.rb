54d01507818961789b10e1a3ed4205963648503e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-09-16 13:48:51 +0300

    raise timeouts because of travis failures

--- server/spec/jobs/leader_elector_job_spec.rb
+++ server/spec/jobs/leader_elector_job_spec.rb
@@ -11,7 +11,7 @@ describe LeaderElectorJob do
     it 'elects only one candidate' do
       candidate1 = LeaderElectorJob.new
       candidate2 = LeaderElectorJob.new
-      Timeout.timeout(1) do
+      Timeout.timeout(5) do
         sleep 0.1 until candidate1.leader? || candidate2.leader?
       end
       expect(candidate1.leader? != candidate2.leader?).to be_truthy
