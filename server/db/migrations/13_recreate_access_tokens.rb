8fdf4976fc8f5f37fabcf3adea222c56c71a493f
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-09-19 11:55:18 +0300

    Convert existing tokens during migration

--- server/db/migrations/13_recreate_access_tokens.rb
+++ server/db/migrations/13_recreate_access_tokens.rb
@@ -1,7 +1,19 @@
 class RecreateAccessTokens < Mongodb::Migration
   def self.up
+    access_tokens = AccessToken.all.to_a
     AccessToken.collection.drop
     AccessToken.create_indexes
+
+    access_tokens.each do |token|
+      new_token = AccessToken.create(
+        user: token.user,
+        token_plain: token.token,
+        expires_at: nil,
+        refresh_token: nil,
+        scopes: token.scopes,
+        internal: true
+      )
+    end
   end
 end
 
