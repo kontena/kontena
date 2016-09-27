0fb4ac13cfd3932770555fe14a46acb5a3552483
Jussi Nummelin <jussi@kontena.io>
2016-09-27 22:47:36 +0200

    change instance env to be more explicit

--- server/app/services/docker/service_creator.rb
+++ server/app/services/docker/service_creator.rb
@@ -101,7 +101,7 @@ module Docker
       env << "KONTENA_SERVICE_NAME=#{grid_service.name}"
       env << "KONTENA_GRID_NAME=#{grid_service.grid.try(:name)}"
       env << "KONTENA_NODE_NAME=#{host_node.name}"
-      env << "KONTENA_SERVICE_INSTANCE=#{instance_number}"
+      env << "KONTENA_SERVICE_INSTANCE_NUMBER=#{instance_number}"
       env
     end
 
--- server/spec/services/docker/service_creator_spec.rb
+++ server/spec/services/docker/service_creator_spec.rb
@@ -131,7 +131,7 @@ describe Docker::ServiceCreator do
         expect(env).to include("KONTENA_SERVICE_NAME=#{service.name.to_s}")
         expect(env).to include("KONTENA_GRID_NAME=#{service.grid.name.to_s}")
         expect(env).to include("KONTENA_NODE_NAME=#{node.name.to_s}")
-        expect(env).to include("KONTENA_SERVICE_INSTANCE=2")
+        expect(env).to include("KONTENA_SERVICE_INSTANCE_NUMBER=2")
       end
     end
 
