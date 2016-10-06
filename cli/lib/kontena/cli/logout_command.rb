34385c7da73d052382c68a39729e6e0c77c2ab80
Lauri Nevala <lauri.nevala@gmail.com>
2016-10-06 18:31:14 +0300

    Implement kontena master logout command

--- cli/lib/kontena/cli/logout_command.rb
+++ cli/lib/kontena/cli/logout_command.rb
@@ -1,38 +1,10 @@
 class Kontena::Cli::LogoutCommand < Kontena::Command
   include Kontena::Cli::Common
 
-  option ['-A', '--all'], :flag, 'Log out from all masters. By default only log out from current master.'
-  option ['--accounts'], :flag, 'Log out from cloud platform accounts', hidden: true
-
-  def use_refresh_token(server)
-    return unless server.token
-    return unless server.token.refresh_token
-    return if server.token.expired?
-    client = Kontena::Client.new(server.url, server.token)
-    ENV["DEBUG"] && puts("Trying to invalidate refresh token on #{server.name}")
-    client.refresh_token
-  rescue
-    ENV["DEBUG"] && puts("Refreshing failed: #{$!} : #{$!.message}")
-  end 
+  banner "Command removed, use 'kontena master logout' to log out of the Kontena Master"
+  banner "or 'kontena cloud logout' to log out of the Kontena Cloud", false
 
   def execute
-    if self.all?
-      config.servers.each do |server|
-        use_refresh_token(server)
-        server.token = nil
-      end
-    elsif self.accounts?
-      config.accounts.each do |account|
-        use_refresh_token(account)
-        account.token = nil
-      end
-    elsif config.current_master
-      use_refresh_token(config.current_master)
-      config.current_master.token = nil
-    else
-      puts "Current master has not been selected"
-      exit 0 # exiting with 0 not 1, it's not really an error situation (kontena logout && kontena master login...)
-    end
-    config.write
+    exit_with_error("Command removed. Use #{"kontena master logout".colorize(:yellow)} to log out of the Kontena Master")
   end
 end
