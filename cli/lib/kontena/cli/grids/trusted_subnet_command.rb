3d22b13b43ddbb9f72460497e531d8ff7d583a2a
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-10-06 16:47:16 +0300

    Search & Replace artifacts

--- cli/lib/kontena/cli/grids/trusted_subnet_command.rb
+++ cli/lib/kontena/cli/grids/trusted_subnet_command.rb
@@ -1,9 +1,10 @@
 module Kontena::Cli::Grids
-
-
   class TrustedSubnetCommand < Kontena::Command
-    subcommand ["list", "ls"], "List trusted subnets", Trustedload_subcommand('subnets/list_command')
-    subcommand "add", "Add trusted subnet", Trustedload_subcommand('subnets/add_command')
-    subcommand ["remove", "rm"], "Remove trusted subnet", Trustedload_subcommand('subnets/remove_command')
+    subcommand ["list", "ls"], "List trusted subnets", load_subcommand('grids/trusted_subnets/list_command')
+    subcommand "add", "Add trusted subnet", load_subcommand('grids/trusted_subnets/add_command')
+    subcommand ["remove", "rm"], "Remove trusted subnet", load_subcommand('grids/trusted_subnets/remove_command')
+
+    def execute
+    end
   end
 end
