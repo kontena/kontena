082c658d134025207f105ff2ba4fb5f169567279
Lauri Nevala <lauri.nevala@gmail.com>
2016-10-07 07:50:57 +0300

    Implement kontena master logout command (#1097)

--- cli/lib/kontena/cli/master_command.rb
+++ cli/lib/kontena/cli/master_command.rb
@@ -5,6 +5,7 @@ require_relative 'master/users_command'
 require_relative 'master/current_command'
 require_relative 'master/config_command'
 require_relative 'master/login_command'
+require_relative 'master/logout_command'
 require_relative 'master/join_command'
 require_relative 'master/audit_log_command'
 require_relative 'master/token_command'
@@ -23,6 +24,7 @@ class Kontena::Cli::MasterCommand < Kontena::Command
   subcommand "users", "Users specific commands", Kontena::Cli::Master::UsersCommand
   subcommand "current", "Show current master details", Kontena::Cli::Master::CurrentCommand
   subcommand "login", "Authenticate to Kontena Master", Kontena::Cli::Master::LoginCommand
+  subcommand "logout", "Log out of Kontena Master", Kontena::Cli::Master::LogoutCommand
   subcommand "token", "Manage Kontena Master access tokens", Kontena::Cli::Master::TokenCommand
   subcommand "join", "Join Kontena Master using an invitation code", Kontena::Cli::Master::JoinCommand
   subcommand "audit-log", "Show master audit logs", Kontena::Cli::Master::AuditLogCommand
---
+++ cli/spec/kontena/cli/master/logout_command_spec.rb
@@ -0,0 +1,85 @@
+require_relative "../../../spec_helper"
+require 'kontena/cli/master/logout_command'
+
+describe Kontena::Cli::Master::LogoutCommand do
+
+  include ClientHelpers
+
+  let(:server) do
+    spy('server')
+  end
+
+  let(:subject) do
+    described_class.new(File.basename($0))
+  end
+
+  describe '#logout' do
+    context 'with all options' do
+      before(:each) do
+        allow(Kontena::Cli::Config.instance).to receive(:servers).and_return([server])
+        allow(Kontena::Cli::Config.instance).to receive(:write)
+        allow(server).to receive(:token=).with(nil)
+      end
+
+      it 'reads servers from config' do
+        expect(Kontena::Cli::Config.instance).to receive(:servers).and_return([server])
+        subject.run(['--all'])
+      end
+
+      it 'invalidates refresh_token for each server' do
+        expect(subject).to receive(:use_refresh_token).with(server)
+        subject.run(['--all'])
+      end
+
+      it 'clears token' do
+        expect(server).to receive(:token=).with(nil)
+        subject.run(['--all'])
+      end
+
+      it 'writes config file' do
+        allow(subject).to receive(:use_refresh_token).with(server)
+        expect(Kontena::Cli::Config.instance).to receive(:write)
+        subject.run(['--all'])
+      end
+    end
+
+    context 'without all options' do
+      before(:each) do
+        allow(Kontena::Cli::Config.instance).to receive(:current_master).and_return(server)
+        allow(Kontena::Cli::Config.instance).to receive(:write)
+        allow(server).to receive(:token=).with(nil)
+      end
+
+      it 'reads current master from config' do
+        expect(Kontena::Cli::Config.instance).to receive(:current_master).and_return(server)
+        subject.run([])
+      end
+
+      it 'invalidates refresh_token for current master' do
+        expect(subject).to receive(:use_refresh_token).with(server)
+        subject.run([])
+      end
+
+      it 'clears token' do
+        allow(subject).to receive(:use_refresh_token).with(server)
+        expect(server).to receive(:token=).with(nil)
+        subject.run([])
+      end
+
+      it 'writes config file' do
+        allow(subject).to receive(:use_refresh_token).with(server)
+        expect(Kontena::Cli::Config.instance).to receive(:write)
+        subject.run([])
+      end
+    end
+
+    context 'current master is not selected' do
+      it 'outputs warning' do
+        allow(Kontena::Cli::Config.instance).to receive(:current_master).and_return(nil)
+        expect {
+          subject.run([])
+        }.to output("Current master has not been selected\n").to_stderr
+      end
+    end
+  end
+end
