05e78d5603b90ef428ba017b8f33c92636e94a43
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-10-07 11:06:53 +0300

    Suggest installing a plugin when a known plugin subcommand is run without the plugin

--- cli/lib/kontena/plugin_manager.rb
+++ cli/lib/kontena/plugin_manager.rb
@@ -36,3 +36,24 @@ module Kontena
     end
   end
 end
+
+require 'clamp/subcommand/execution'
+module Clamp::Subcommand::Execution
+  KNOWN_PLUGINS = {
+    'aws'          => 'kontena-plugin-aws',
+    'azure'        => 'kontena-plugin-azure',
+    'vagrant'      => 'kontena-plugin-vagrant',
+    'digitalocean' => 'kontena-plugin-digitalocean',
+    'packet'       => 'kontena-plugin-packet',
+    'upcloud'      => 'kontena-plugin-upcloud'
+  }
+
+  def find_subcommand_class(name)
+    if KNOWN_PLUGINS.keys.include?(name) && !Kontena::PluginManager.instance.plugins.find {|p| p.name == KNOWN_PLUGINS[name]}
+      signal_usage_error("The #{name} plugin has not been installed. Use: kontena plugin install #{name}")
+    end
+    subcommand_def = self.class.find_subcommand(name) || signal_usage_error(Clamp.message(:no_such_subcommand, :name => name))
+    subcommand_def.subcommand_class
+  end
+end
+
