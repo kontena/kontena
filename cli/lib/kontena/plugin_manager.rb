d94649237e880942abfe77a507fa4253e78a4326
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-10-10 18:27:32 +0300

    Show error if installed cli plugin is too old (#1116)

--- cli/lib/kontena/plugin_manager.rb
+++ cli/lib/kontena/plugin_manager.rb
@@ -4,6 +4,9 @@ module Kontena
   class PluginManager
     include Singleton
 
+    CLI_GEM = 'kontena-cli'.freeze
+    MIN_CLI_VERSION = '0.15.99'.freeze
+
     attr_reader :plugins
 
     def initialize
@@ -17,10 +20,16 @@ module Kontena
           plugin = File.join(spec.gem_dir, require_path, 'kontena_cli_plugin.rb')
           if File.exist?(plugin) && !@plugins.find{ |p| p.name == spec.name }
             begin
-              load(plugin)
-              @plugins << spec
+              if spec_has_valid_dependency?(spec)
+                load(plugin)
+                @plugins << spec
+              else
+                plugin_name = spec.name.sub('kontena-plugin-', '')
+                STDERR.puts " [#{Kontena.pastel.red('error')}] Plugin #{Kontena.pastel.cyan(plugin_name)} (#{spec.version}) is not compatible with the current cli version."
+                STDERR.puts "         To update plugin, run 'kontena plugin install #{plugin_name}'"
+              end
             rescue LoadError => exc
-              STDERR.puts "failed to load plugin: #{spec.name}"
+              STDERR.puts " [#{Kontena.pastel.red('error')}] Failed to load plugin: #{spec.name}"
               if ENV['DEBUG']
                 STDERR.puts exc.message
                 STDERR.puts exc.backtrace.join("\n")
@@ -33,6 +42,15 @@ module Kontena
       @plugins
     rescue => exc
       STDERR.puts exc.message
+    end
+
+    # @param [Gem::Specification] spec
+    # @return [Boolean]
+    def spec_has_valid_dependency?(spec)
+      kontena_cli = spec.runtime_dependencies.find{ |d| d.name == CLI_GEM }
+      !kontena_cli.match?(CLI_GEM, MIN_CLI_VERSION)
+    rescue
+      false
     end
   end
 end
