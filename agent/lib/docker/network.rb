b60e9736991af72a45c7870c68772dcd45780ca2
Tero Marttila <tero.marttila@kontena.io>
2016-10-13 13:57:03 +0300

    fix Docker::Network#connect opts

--- agent/lib/docker/network.rb
+++ agent/lib/docker/network.rb
@@ -5,7 +5,7 @@ module Docker
 
     # Monkey patched temporarily, we should create a real PR
     def connect(container, opts = {})
-      endpoint_config = opt.delete(:endpoint_config)
+      endpoint_config = opts.delete(:endpoint_config)
       Docker::Util.parse_json(
         connection.post(path_for('connect'), opts,
                         body: { container: container, endpointconfig: endpoint_config }.to_json)
