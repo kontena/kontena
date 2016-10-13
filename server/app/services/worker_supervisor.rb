ddf80b68f9ddc611336a3e14ec0f3e7afd6b3126
Tero Marttila <tero.marttila@kontena.io>
2016-10-13 14:59:34 +0300

    clean out remaining references to OverlayCidr

--- server/app/services/worker_supervisor.rb
+++ server/app/services/worker_supervisor.rb
@@ -1,6 +1,5 @@
 class WorkerSupervisor < Celluloid::SupervisionGroup
   pool GridSchedulerWorker, as: :grid_scheduler_worker
   pool GridServiceSchedulerWorker, as: :grid_service_scheduler_worker, size: 4
-  pool GridSubnetInitializerWorker, as: :grid_subnet_initializer_worker
   pool GridServiceRemoveWorker, as: :grid_service_remove_worker
 end
