f384f0e1dd9b65ac32a06f1e982a45d318ab2b52
Tero Marttila <tero.marttila@kontena.io>
2016-10-13 17:32:37 +0300

    Fix Kontena::Workers::ContainerStarterWorker spec for autostart?

--- agent/spec/lib/kontena/workers/container_starter_worker_spec.rb
+++ agent/spec/lib/kontena/workers/container_starter_worker_spec.rb
@@ -2,8 +2,7 @@ require_relative '../../../spec_helper'
 
 describe Kontena::Workers::ContainerStarterWorker do
 
-  let(:container) { spy(:container) }
-  let(:network_adapter) { spy(:network_adapter) }
+  let(:network_adapter) { double(:network_adapter) }
   let(:subject) { described_class.new }
 
   before(:each) { Celluloid.boot }
@@ -18,43 +17,68 @@ describe Kontena::Workers::ContainerStarterWorker do
   end
 
   describe '#ensure_container_running' do
+    context 'for a stopped server container' do
+      let :container do
+        double(:container, name: 'test',
+          running?: false,
+          restarting?: false,
+          service_container?: true,
+          autostart?: true,
+        )
+      end
 
-    it 'starts stopped service container' do
-      expect(container).to receive(:running?).and_return(false)
-      expect(container).to receive(:restarting?).and_return(false)
-      expect(container).to receive(:restart_policy).and_return({'Name' => 'always'})
-      expect(container).to receive(:service_container?).and_return(true)
-      expect(container).to receive(:start)
+      it 'starts the container' do
+        expect(container).to receive(:start)
 
-      subject.ensure_container_running(container)
+        subject.ensure_container_running(container)
+      end
     end
 
-    it 'does not start non-service container' do
-      expect(container).to receive(:running?).and_return(false)
-      expect(container).to receive(:restarting?).and_return(false)
-      expect(container).to receive(:restart_policy).and_return({'Name' => 'always'})
-      expect(container).to receive(:service_container?).and_return(false)
-      expect(container).not_to receive(:start)
+    context 'for a stopped non-service container' do
+      let :container do
+        double(:container, name: 'test',
+          running?: false,
+          restarting?: false,
+          service_container?: false,
+          autostart?: true,
+        )
+      end
 
-      subject.ensure_container_running(container)
+      it 'does not start the container' do
+        expect(container).not_to receive(:start)
+
+        subject.ensure_container_running(container)
+      end
     end
 
-    it 'does not start running container' do
-      expect(container).to receive(:running?).and_return(true)
-      expect(container).not_to receive(:start)
+    context 'for a running container' do
+      let :container do
+        double(:container, name: 'test',
+          running?: true,
+        )
+      end
 
-      subject.ensure_container_running(container)
+      it 'does not start the container' do
+        expect(container).not_to receive(:start)
+
+        subject.ensure_container_running(container)
+      end
     end
 
-    it 'does not start service container with restart policy not "always"' do
-      expect(container).to receive(:running?).and_return(false)
-      expect(container).to receive(:restarting?).and_return(false)
-      expect(container).to receive(:restart_policy).and_return({'Name' => 'never'})
-      expect(container).not_to receive(:start)
+    context 'for a autostart => never container' do
+      let :container do
+        double(:container, name: 'test',
+          running?: false,
+          restarting?: false,
+          autostart?: false,
+        )
+      end
 
-      subject.ensure_container_running(container)
+      it 'does not start service container with restart policy not "always"' do
+        expect(container).not_to receive(:start)
+
+        subject.ensure_container_running(container)
+      end
     end
-
   end
-
 end
