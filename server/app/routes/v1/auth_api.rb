8a03a06968d5f160333488d59ce4b7acb22312b7
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-10-17 10:11:59 +0300

    Remove upgrade method hint

--- server/app/routes/v1/auth_api.rb
+++ server/app/routes/v1/auth_api.rb
@@ -5,7 +5,7 @@ module V1
     route do |r|
       r.post do
         if r.user_agent.to_s.start_with?('kontena-cli')
-          halt_request(403, 'This version of Kontena Master does not support user credential authentication. You need to upgrade your Kontena CLI, use: gem install kontena-cli')
+          halt_request(403, 'This version of Kontena Master does not support user credential authentication. You need to upgrade your Kontena CLI.')
         else
           halt_request(403, 'This version of Kontena Master does not support user credential authentication.')
         end
