1644c9b4aa943f28331db264b07f8064d559af07
Kimmo Lehto <kimmo.lehto@gmail.com>
2016-10-19 10:38:06 +0300

    Upgrade CLI hint when doing legacy login from cli (#1138)
    * Upgrade CLI hint when doing legacy login from cli
    
    * Remove upgrade method hint

--- server/app/routes/v1/auth_api.rb
+++ server/app/routes/v1/auth_api.rb
@@ -4,7 +4,11 @@ module V1
 
     route do |r|
       r.post do
-        halt_request(403, 'This version of Kontena Master does not support user credential authentication.')
+        if r.user_agent.to_s.start_with?('kontena-cli')
+          halt_request(403, 'This version of Kontena Master does not support user credential authentication. You need to upgrade your Kontena CLI.')
+        else
+          halt_request(403, 'This version of Kontena Master does not support user credential authentication.')
+        end
       end
     end
   end
