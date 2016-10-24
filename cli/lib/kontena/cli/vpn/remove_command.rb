d64648ab9f556b272f4ad7589776797ddd51caf1
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-10-24 10:35:15 +0300

    fix vpn remove error

--- cli/lib/kontena/cli/vpn/remove_command.rb
+++ cli/lib/kontena/cli/vpn/remove_command.rb
@@ -1,6 +1,7 @@
 module Kontena::Cli::Vpn
   class RemoveCommand < Kontena::Command
     include Kontena::Cli::Common
+    include Kontena::Cli::GridOptions
 
     option "--force", :flag, "Force remove", default: false, attribute_name: :forced
 
@@ -12,7 +13,7 @@ module Kontena::Cli::Vpn
       vpn = client(token).get("services/#{current_grid}/#{name}") rescue nil
       exit_with_error("#{name} service does not exist") if vpn.nil?
 
-      spinner "Removing #{vpn.colorize(:cyan)} service " do
+      spinner "Removing #{name.colorize(:cyan)} service " do
         client(token).delete("services/#{current_grid}/vpn")
       end
     end
