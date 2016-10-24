428350d4130f5d36550ee302ea1c9c6b0db4dc5b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-10-24 11:06:52 +0300

    fix require

--- cli/lib/kontena/cli/vpn_command.rb
+++ cli/lib/kontena/cli/vpn_command.rb
@@ -1,7 +1,6 @@
 require_relative 'vpn/create_command'
 require_relative 'vpn/config_command'
 require_relative 'vpn/remove_command'
-require_relative 'vpn/delete_command'
 
 class Kontena::Cli::VpnCommand < Kontena::Command
 
