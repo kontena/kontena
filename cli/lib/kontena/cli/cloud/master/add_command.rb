c66e42cbdcc023fa2a05fe779332cb6e42f05ae2
Kimmo Lehto <kimmo.lehto@gmail.com>
2016-10-28 19:57:32 +0300

    Fix master provider save to cloud (#1250)

--- cli/lib/kontena/cli/cloud/master/add_command.rb
+++ cli/lib/kontena/cli/cloud/master/add_command.rb
@@ -34,8 +34,8 @@ module Kontena::Cli::Cloud::Master
 
       response = cloud_client.post('user/masters', { data: { attributes: attributes } })
       exit_with_error "Failed (invalid response)" unless response.kind_of?(Hash)
-      exit_with_error "Failed (no data)" unless response['data']
       exit_with_error "Failed: #{response['error']}" if response['error']
+      exit_with_error "Failed (no data)" unless response['data']
       response
     end
 
@@ -100,13 +100,14 @@ module Kontena::Cli::Cloud::Master
               args = []
               args << "--provider #{self.provider.shellescape}" if self.provider
               args << "--version #{self.version.shellescape}" if self.version
+              args << self.cloud_master_id
               Kontena.run("cloud master update #{args.join(' ')}")
             end
           end
         end
       else
         response = spinner "Registering current Kontena Master '#{current_master.name}' #{" as '#{new_name}' " unless new_name == current_master.name}to Kontena Cloud" do
-          register(new_name, current_master.url)
+          register(new_name, current_master.url, self.provider, nil, self.version)
         end
       end
 
