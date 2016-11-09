14645b041d96ac2a59372593e92ff229d4d8556b
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-09 09:47:17 +0200

    Multiple node labels can now be added / removed at once. Added label list command.

--- cli/lib/kontena/cli/nodes/label_command.rb
+++ cli/lib/kontena/cli/nodes/label_command.rb
@@ -2,9 +2,11 @@ module Kontena::Cli::Nodes
 
   require_relative 'labels/add_command'
   require_relative 'labels/remove_command'
+  require_relative 'labels/list_command'
 
   class LabelCommand < Kontena::Command
 
+    subcommand ["list", "ls"], "List node labels", Labels::ListCommand
     subcommand "add", "Add label to node", Labels::AddCommand
     subcommand ["remove", "rm"], "Remove label from node", Labels::RemoveCommand
 
--- cli/lib/kontena/cli/nodes/labels/list_command.rb
+++ cli/lib/kontena/cli/nodes/labels/list_command.rb
@@ -1,9 +1,8 @@
 module Kontena::Cli::Nodes::Labels
-  class RemoveCommand < Kontena::Command
+  class ListCommand < Kontena::Command
     include Kontena::Cli::Common
 
     parameter "NODE_ID", "Node id"
-    parameter "LABEL ...", "Labels"
 
     requires_current_master
     requires_current_master_token
@@ -11,8 +10,8 @@ module Kontena::Cli::Nodes::Labels
 
     def execute
       node = client.get("grids/#{current_grid}/nodes/#{node_id}")
-      data = { labels: Array(node['labels']).reject {|label| label_list.include?(label) } }
-      client.put("nodes/#{node['id']}", data, {}, {'Kontena-Grid-Token' => node['grid']['token']})
+      puts Array(node['labels']).join("\n")
     end
   end
 end
+
