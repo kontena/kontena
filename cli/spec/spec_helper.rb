2e751866615a1cba6ac70e9f873aca642befe1b8
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-11-15 16:53:16 +0200

    cleanup spec_helper

--- cli/spec/spec_helper.rb
+++ cli/spec/spec_helper.rb
@@ -54,18 +54,6 @@ RSpec.configure do |config|
   config.after(:each) do
     $stdout = STDOUT
   end
-
-  original_stderr = $stderr
-  original_stdout = $stdout
-  config.before(:all) do
-    # Redirect stderr and stdout
-    $stderr = File.open(File::NULL, "w")
-    $stdout = File.open(File::NULL, "w")
-  end
-  config.after(:all) do
-    $stderr = original_stderr
-    $stdout = original_stdout
-  end
 end
 
 require_relative 'support/client_helpers'
