ea3b3daaf1c2db1ac798231dec2719194fc8a495
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-11-15 15:38:07 +0200

    improve create stacks migration

--- server/db/migrations/17_create_stacks.rb
+++ server/db/migrations/17_create_stacks.rb
@@ -2,10 +2,13 @@ class CreateStacks < Mongodb::Migration
   def self.up
     Stack.create_indexes
     Grid.all.each do |grid|
-      default_stack = Stack.create!(
-        name: 'default',
-        grid: grid
-      )
+      default_stack = grid.stacks.find_by(name: 'default')
+      unless default_stack
+        default_stack = Stack.create!(
+          name: 'default',
+          grid: grid
+        )
+      end
       grid.grid_services.where(stack_id: nil).each do |service|
         service.set(stack_id: default_stack.id)
       end
