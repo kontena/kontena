cfa6846a4d501f98da598f701d9e757aad0ffc8d
Jussi Nummelin <jussi@kontena.io>
2016-11-18 22:45:09 +0200

    fix weave specs

--- agent/spec/lib/kontena/network_adapters/weave_spec.rb
+++ agent/spec/lib/kontena/network_adapters/weave_spec.rb
@@ -73,23 +73,15 @@ describe Kontena::NetworkAdapters::Weave do
       expect(subject.running?).to be_falsey
     end
 
-    it 'return false if weave container running but ipam not' do
+    it 'return false if weave container running but weave api not ready' do
       expect(subject.wrapped_object).to receive(:weave_container_running?).and_return(true)
-      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(false)
-      expect(subject.running?).to be_falsey
-    end
-
-    it 'return false if weave container and ipam running but weave api not ready' do
-      expect(subject.wrapped_object).to receive(:weave_container_running?).and_return(true)
-      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(true)
       expect(subject.wrapped_object).to receive(:weave_api_ready?).and_return(false)
       expect(subject.running?).to be_falsey
     end
 
 
-    it 'return false if weave container, ipam and weave api running but weave bridge not configured' do
+    it 'return false if weave container and weave api running but weave bridge not configured' do
       expect(subject.wrapped_object).to receive(:weave_container_running?).and_return(true)
-      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(true)
       expect(subject.wrapped_object).to receive(:weave_api_ready?).and_return(true)
       expect(subject.wrapped_object).to receive(:interface_ip).and_return(nil)
       expect(subject.running?).to be_falsey
@@ -97,22 +89,29 @@ describe Kontena::NetworkAdapters::Weave do
 
     it 'returns true only if all components running' do
       expect(subject.wrapped_object).to receive(:weave_container_running?).and_return(true)
-      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(true)
       expect(subject.wrapped_object).to receive(:weave_api_ready?).and_return(true)
       expect(subject.wrapped_object).to receive(:interface_ip).and_return('10.81.0.1')
       expect(subject.running?).to be_truthy
     end
+  end
 
-
-
-    it 'returns false if weave is running but ipam not running' do
-      weave = spy(:weave, :running? => true)
-      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(false)
-      allow(Docker::Container).to receive(:get).with('weave').and_return(weave)
-      expect(subject.running?).to be_falsey
+  describe '#network_ready?' do
+    it 'return false is weave not running' do
+      expect(subject.wrapped_object).to receive(:running?).and_return(false)
+      expect(subject.network_ready?).to be_falsey
     end
 
+    it 'return false is weave running but ipam not' do
+      expect(subject.wrapped_object).to receive(:running?).and_return(true)
+      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(false)
+      expect(subject.network_ready?).to be_falsey
+    end
 
+    it 'return true is weave and ipam running' do
+      expect(subject.wrapped_object).to receive(:running?).and_return(true)
+      expect(subject.wrapped_object).to receive(:ipam_running?).and_return(true)
+      expect(subject.network_ready?).to be_truthy
+    end
   end
 
   describe '#config_changed?' do
