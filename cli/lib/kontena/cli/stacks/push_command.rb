b982031f418a55e073a3fbeb16e9df39783fba8e
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-18 09:53:30 +0200

    Initial registry push command

--- cli/lib/kontena/cli/stacks/push_command.rb
+++ cli/lib/kontena/cli/stacks/push_command.rb
@@ -5,7 +5,14 @@ module Kontena::Cli::Stacks
     include Kontena::Cli::Common
     include Common
 
+    parameter "FILENAME", "Stack file path"
+
+    requires_current_account_token
+
     def execute
+      content = File.read(self.filename)
+      yaml = YAML.load(content)
+      stacks_client.push(yaml['stack'], yaml['version'], content)
     end
   end
 end
