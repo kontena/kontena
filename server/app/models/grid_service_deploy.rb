88785b216165018579d5d2d752106dc7ff315aa0
Jussi Nummelin <jnummelin@users.noreply.github.com>
2016-11-18 10:47:17 +0200

    Enhanced deployment tracking (#1348)

--- server/app/models/grid_service_deploy.rb
+++ server/app/models/grid_service_deploy.rb
@@ -1,10 +1,12 @@
 class GridServiceDeploy
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Enum
 
   field :started_at, type: DateTime
   field :finished_at, type: DateTime
   field :reason, type: String
+  enum :deploy_state, [:created, :ongoing, :success, :error], default: :created
 
   index({ grid_service_id: 1 })
   index({ created_at: 1 })
--- server/app/views/v1/grid_service_deploys/_grid_service_deploy.json.jbuilder
+++ server/app/views/v1/grid_service_deploys/_grid_service_deploy.json.jbuilder
@@ -3,4 +3,5 @@ json.created_at @grid_service_deploy.created_at
 json.started_at @grid_service_deploy.started_at
 json.finished_at @grid_service_deploy.finished_at
 json.service_id @grid_service_deploy.grid_service.to_path
+json.state @grid_service_deploy.deploy_state
 json.reason @grid_service_deploy.reason
