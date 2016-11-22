321e9d324ee3669c8b77ecac80246327bbd3a846
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-22 17:01:10 +0200

    Remove debugger

--- cli/lib/kontena/cli/stacks/yaml/opto/secret_type.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/secret_type.rb
@@ -8,7 +8,6 @@ module Kontena::Cli::Stacks
       def after_set(option)
         if option.valid?
           require 'shellwords'
-          require 'byebug'; byebug
           ENV["DEBUG"] && puts("Setting to vault: #{options[:secret][:secret]}")
           Kontena.run("vault write --silent #{options[:secret][:secret]} #{option.value.to_s.shellescape}")
         else
