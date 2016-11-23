f69ae35b4bd118f8c09bde9c0158eee131b3efcd
Lauri Nevala <lauri.nevala@gmail.com>
2016-11-23 15:50:43 +0200

    Fix kontena stack upgrade command (#1384)

--- cli/spec/kontena/cli/stacks/upgrade_command_spec.rb
+++ cli/spec/kontena/cli/stacks/upgrade_command_spec.rb
@@ -15,19 +15,34 @@ describe Kontena::Cli::Stacks::UpgradeCommand do
     end
 
     it 'requires api url' do
-      allow(subject).to receive(:stack_from_yaml).with('kontena.yml').and_return(stack)
+      allow(subject).to receive(:require_config_file).and_return(true)
+      allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
       expect(subject).to receive(:require_api_url).once
       subject.run(['stack-name', './path/to/kontena.yml'])
     end
 
     it 'requires token' do
-      allow(subject).to receive(:stack_from_yaml).with('kontena.yml').and_return(stack)
+      allow(subject).to receive(:require_config_file).and_return(true)
+      allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
       expect(subject).to receive(:require_token).and_return(token)
       subject.run(['stack-name', './path/to/kontena.yml'])
     end
 
+    it 'requires stack file' do
+      allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
+      allow(subject).to receive(:require_token).and_return(token)
+      expect(subject).to receive(:require_config_file).with('./path/to/kontena.yml').and_return(true)
+      subject.run(['stack-name', './path/to/kontena.yml'])
+    end
+
+    it 'uses kontena.yml as default stack file' do
+      allow(subject).to receive(:require_token).and_return(token)
+      expect(subject).to receive(:require_config_file).with('kontena.yml').and_return(true)
+      expect(subject).to receive(:stack_from_yaml).with('kontena.yml').and_return(stack)
+      subject.run(['stack-name'])
+    end
+
     it 'sends stack to master' do
-      
       allow(subject).to receive(:require_config_file).with('./path/to/kontena.yml').and_return(true)
       allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
       expect(client).to receive(:put).with(
@@ -45,6 +60,31 @@ describe Kontena::Cli::Stacks::UpgradeCommand do
         'stacks/test-grid/stack-b', anything
       )
       subject.run(['stack-b',  './path/to/kontena.yml'])
+    end
+
+    context '--deploy option' do
+      context 'when given' do
+        it 'triggers deploy' do
+          allow(subject).to receive(:require_config_file).with('./path/to/kontena.yml').and_return(true)
+          allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
+          allow(client).to receive(:put).with(
+            'stacks/test-grid/stack-a', anything
+          ).and_return({})
+          expect(Kontena).to receive(:run).with("stack deploy stack-a").once
+          subject.run(['--deploy', 'stack-a', './path/to/kontena.yml'])
+        end
+      end
+      context 'when not given' do
+        it 'does not trigger deploy' do
+          allow(subject).to receive(:require_config_file).with('./path/to/kontena.yml').and_return(true)
+          allow(subject).to receive(:stack_from_yaml).with('./path/to/kontena.yml').and_return(stack)
+          allow(client).to receive(:put).with(
+            'stacks/test-grid/stack-a', anything
+          ).and_return({})
+          expect(Kontena).not_to receive(:run).with("stack deploy stack-a")
+          subject.run(['stack-a', './path/to/kontena.yml'])
+        end
+      end
     end
   end
 end
