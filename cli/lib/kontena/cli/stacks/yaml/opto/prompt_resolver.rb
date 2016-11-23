0f98fbcc13c86dc5f2f20cce9540755ef7dd0d2e
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-23 15:46:23 +0200

    Finetune prompt resolver and add enum support

--- cli/lib/kontena/cli/stacks/yaml/opto/prompt_resolver.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/prompt_resolver.rb
@@ -3,8 +3,30 @@ module Kontena::Cli::Stacks
     class Prompt < Opto::Resolver
       include Kontena::Cli::Common
 
+      def enum?
+        option.type == 'enum'
+      end
+
+      def question_text
+        (!hint.nil? && hint != option.name) ? "#{hint} :" : "#{enum? ? "Select" : "Enter"} #{option.label || option.name} :"
+      end
+
+      def enum
+        prompt.select(question_text) do |menu|
+          option.handler.options[:options].each do |opt| # quite ugly way to access the option's value list definition
+            menu.choice opt[:label], opt[:value]
+          end
+        end
+      end
+
+      def ask
+        TTY::Prompt.new.ask(question_text)
+      end
+
+
       def resolve
-        prompt.ask(hint)
+        return nil if option.skip?
+        enum? ? enum : ask
       end
     end
   end
