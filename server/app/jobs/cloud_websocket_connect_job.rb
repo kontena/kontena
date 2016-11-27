1fd44c9aa0cf019f297c392875c38357163c2563
Lauri Nevala <lauri.nevala@gmail.com>
2016-11-27 13:36:42 -0800

    Refactor CloudWebsocketConnectJob and add initial tests (#1407)
    * Refactor CloudWebsocketConnectJob and add initial tests
    
    * fix cloud_enabled?

--- server/app/jobs/cloud_websocket_connect_job.rb
+++ server/app/jobs/cloud_websocket_connect_job.rb
@@ -1,5 +1,6 @@
 require 'celluloid'
 require_relative '../services/logging'
+require_relative '../services/auth_provider'
 
 class CloudWebsocketConnectJob
   include Celluloid
@@ -13,26 +14,59 @@ class CloudWebsocketConnectJob
   def perform
     sleep 0.1
     start_em
-    loop do
-      if cloud_enabled?
-        connect
-      else
-        disconnect
-      end
+    while running?
+      update_connection
       sleep 30
     end
   end
 
+  def running? # we can mock this in tests to return false
+    true
+  end
+
+  def update_connection
+    if cloud_enabled?
+      connect
+    else
+      disconnect
+    end
+  end
+
   def cloud_enabled?
-    config['cloud.enabled'] && !Cloud::WebsocketClient.api_uri.to_s.empty? && config['oauth2.client_id'] && config['oauth2.client_secret']
+    kontena_auth_provider? &&
+      oauth_app_credentials? &&
+      cloud_enabled_in_config? &&
+      socket_api_uri?
+  end
+
+  def kontena_auth_provider?
+    AuthProvider.instance.is_kontena?
+  end
+
+  def cloud_enabled_in_config?
+    config['cloud.enabled'].to_s == 'true'
+  end
+
+  def socket_api_uri?
+    !Cloud::WebsocketClient.api_uri.to_s.empty?
+  end
+
+  def oauth_app_credentials?
+    config['oauth2.client_id'] && config['oauth2.client_secret']
   end
 
   def connect
     if @client.nil?
-      @client = Cloud::WebsocketClient.new(config['oauth2.client_id'], config['oauth2.client_secret'])
+      @client = init_ws_client(config['oauth2.client_id'], config['oauth2.client_secret'])
       @client.ensure_connect
     end
     @client
+  end
+
+  ##
+  # return [Cloud::WebsocketClient]
+  def init_ws_client(client_id, client_secret)
+    Cloud::WebsocketClient.new(client_id, client_secret)
   end
 
   def disconnect
@@ -46,4 +80,11 @@ class CloudWebsocketConnectJob
     Thread.new { EventMachine.run } unless EventMachine.reactor_running?
     sleep 0.01 until EventMachine.reactor_running?
   end
+
+  protected
+
+  def client
+    @client
+  end
+
 end
