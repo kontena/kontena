bcb6f6b4177037bc266fdd140bb58e4cb1b1c057
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-28 14:06:37 +0200

    Remove delete command, add "the" to stacks registry

--- cli/lib/kontena/cli/stack_command.rb
+++ cli/lib/kontena/cli/stack_command.rb
@@ -12,7 +12,6 @@ require_relative 'stacks/pull_command'
 require_relative 'stacks/search_command'
 require_relative 'stacks/install_command'
 require_relative 'stacks/info_command'
-require_relative 'stacks/delete_command'
 
 class Kontena::Cli::StackCommand < Kontena::Command
 
@@ -25,11 +24,10 @@ class Kontena::Cli::StackCommand < Kontena::Command
   subcommand "logs", "Show logs from stack services", Kontena::Cli::Stacks::LogsCommand
   subcommand "monitor", "Monitor stack services", Kontena::Cli::Stacks::MonitorCommand
   subcommand ["remove","rm"], "Remove a deployed stack", Kontena::Cli::Stacks::RemoveCommand
-  subcommand "push", "Push a stack into stacks registry", Kontena::Cli::Stacks::PushCommand
-  subcommand "pull", "Pull a stack from stacks registry", Kontena::Cli::Stacks::PullCommand
-  subcommand "search", "Search for stacks in stacks registry", Kontena::Cli::Stacks::SearchCommand
-  subcommand "info", "Show info about a stack in stacks registry", Kontena::Cli::Stacks::InfoCommand
-  subcommand ["del", "delete"], "Delete a stack in stacks registry", Kontena::Cli::Stacks::DeleteCommand
+  subcommand "push", "Push (or delete) a stack into the stacks registry", Kontena::Cli::Stacks::PushCommand
+  subcommand "pull", "Pull a stack from the stacks registry", Kontena::Cli::Stacks::PullCommand
+  subcommand "search", "Search for stacks in the stacks registry", Kontena::Cli::Stacks::SearchCommand
+  subcommand "info", "Show info about a stack in the stacks registry", Kontena::Cli::Stacks::InfoCommand
   
   def execute
   end
