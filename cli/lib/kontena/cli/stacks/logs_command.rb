8d3490d2f1df774b12930ba6198fb479679fb8f5
Tero Marttila <SpComb@users.noreply.github.com>
2016-12-05 17:52:23 +0200

    CLI: Fix kontena stack logs duplicated options/parameters (#1430)

--- cli/lib/kontena/cli/stacks/logs_command.rb
+++ cli/lib/kontena/cli/stacks/logs_command.rb
@@ -7,19 +7,12 @@ module Kontena::Cli::Stacks
     banner "Shows logs from services in a stack"
 
     parameter "NAME", "Stack name"
-    option ["-t", "--tail"], :flag, "Tail (follow) logs", default: false
-    option ["-l", "--lines"], "LINES", "How many lines to show", default: '100'
-    option "--since", "SINCE", "Show logs since given timestamp"
 
     requires_current_master
     requires_current_master_token
 
     def execute
-      query_params = {}
-      query_params[:limit] = lines if lines
-      query_params[:since] = since if since
-
-      show_logs("stacks/#{current_grid}/#{name}/container_logs", query_params) do |log|
+      show_logs("stacks/#{current_grid}/#{name}/container_logs") do |log|
         show_log(log)
       end
     end
