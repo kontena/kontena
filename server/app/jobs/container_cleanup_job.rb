e81ab081c04b2e6942365ebd3ac03e574d865e0c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-12-05 12:22:46 +0200

    terminate containers that does not have service definition anymore

--- server/app/jobs/container_cleanup_job.rb
+++ server/app/jobs/container_cleanup_job.rb
@@ -12,12 +12,12 @@ class ContainerCleanupJob
 
   def perform
     info 'starting to cleanup stale containers'
-    sleep 0.1
     loop do
+      sleep 1.minute.to_i
       if leader?
         destroy_deleted_containers
+        terminate_ghost_containers
       end
-      sleep 1.minute.to_i
     end
   end
 
@@ -25,5 +25,29 @@ class ContainerCleanupJob
     Container.deleted.where(:deleted_at.lt => 1.minutes.ago).each do |c|
       c.destroy
     end
+  end
+
+  def terminate_ghost_containers
+    Container.unscoped.where(:grid_service_id.ne => nil).each do |c|
+      if c.grid_service.nil?
+        terminate_ghost_container(c)
+      end
+    end
+  end
+
+  # @param [Docker::Container] container
+  def terminate_ghost_container(container)
+    if container.host_node
+      info "terminating #{container.to_path}"
+      terminator = Docker::ServiceTerminator.new(container.host_node)
+      terminator.request_terminate_service(
+        container.grid_service_id.to_s, container.instance_number, {lb: true}
+      )
+    else
+      info "removing #{container.to_path} because host node not found"
+      container.destroy
+    end
+  rescue => exc
+    error "#{exc.class.name}: #{exc.message}"
   end
 end
--- server/spec/jobs/container_cleanup_job_spec.rb
+++ server/spec/jobs/container_cleanup_job_spec.rb
@@ -8,6 +8,9 @@ describe ContainerCleanupJob do
   let(:node1) { HostNode.create!(name: "node-1", connected: false, last_seen_at: 2.hours.ago) }
   let(:node2) { HostNode.create!(name: "node-2", connected: true, last_seen_at: 2.seconds.ago) }
   let(:node3) { HostNode.create!(name: "node-3", connected: true, last_seen_at: 3.seconds.ago) }
+  let(:service) do
+    GridService.create!(name: 'test', image_name: 'test:latest', grid: grid)
+  end
   let(:subject) { described_class.new(false) }
 
   describe '#destroy_deleted_containers' do
@@ -27,6 +30,36 @@ describe ContainerCleanupJob do
       expect {
         subject.destroy_deleted_containers
       }.to change{ Container.unscoped.count }.by(0)
+    end
+  end
+
+  describe '#terminate_ghost_containers' do
+    it 'terminates containers that does not have a service anymore' do
+      container = Container.create!(
+        name: 'foo-1', host_node: node2, grid: grid, deleted_at: 1.minutes.ago,
+        grid_service_id: GridService.new.id
+      )
+      expect(subject.wrapped_object).to receive(:terminate_ghost_container).with(container)
+      subject.terminate_ghost_containers
+    end
+
+    it 'does not terminate container that has a service' do
+      container = Container.create!(
+        name: 'foo-1', host_node: node2, grid: grid, deleted_at: 1.minutes.ago,
+        grid_service_id: service.id
+      )
+      expect(subject.wrapped_object).not_to receive(:terminate_ghost_container)
+      subject.terminate_ghost_containers
+    end
+
+    it 'removes a container that does not have a host node' do
+      container = Container.create!(
+        name: 'foo-1', host_node: HostNode.new.id, grid: grid, deleted_at: 1.minutes.ago,
+        grid_service_id: GridService.new.id
+      )
+      expect {
+        subject.terminate_ghost_containers
+      }.to change { Container.unscoped.count }.by(-1)
     end
   end
 end
