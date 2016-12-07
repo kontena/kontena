2cc3e7ca0ba8d3a0b3456ec88b44322f0dcbf27f
Jussi Nummelin <jussi@kontena.io>
2016-12-07 16:13:02 +0200

    use stackless KONTENA_SERVICE_NAME env and fix stack on LB side

--- server/app/services/docker/service_creator.rb
+++ server/app/services/docker/service_creator.rb
@@ -90,7 +90,7 @@ module Docker
     def build_env(instance_number)
       env = grid_service.env.dup || []
       env << "KONTENA_SERVICE_ID=#{grid_service.id.to_s}"
-      env << "KONTENA_SERVICE_NAME=#{grid_service.qualified_name}"
+      env << "KONTENA_SERVICE_NAME=#{grid_service.name}"
       env << "KONTENA_GRID_NAME=#{grid_service.grid.try(:name)}"
       env << "KONTENA_STACK_NAME=#{grid_service.stack.try(:name)}"
       env << "KONTENA_NODE_NAME=#{host_node.name}"
