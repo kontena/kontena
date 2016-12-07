118cf0c0e1cb062f3d0decfd1a1416bb766d4251
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-12-07 17:13:19 +0200

    More YAML safe interpolation result

--- cli/lib/kontena/cli/stacks/yaml/reader.rb
+++ cli/lib/kontena/cli/stacks/yaml/reader.rb
@@ -90,8 +90,6 @@ module Kontena::Cli::Stacks
         yaml['version'] || yaml['stack'].to_s[/:(.*)/, 1] || '1'
       end
 
-      private
-
       # A hash such as { "${MYSQL_IMAGE}" => "MYSQL_IMAGE } where the key is the
       # string to be substituted and value is the pure name part
       # @return [Hash]
@@ -221,18 +219,24 @@ module Kontena::Cli::Stacks
       ##
       # @param [String] text - content of YAML file
       def interpolate(text)
-        text.gsub(/(?<!\$)\$(?!\$)\{?\w+\}?/) do |v| # searches $VAR and ${VAR} and not $$VAR
-          var = v.tr('${}', '')
-          val = ENV[var]
-          if val
-            val
-          elsif @replace_missing
-            @replace_missing
-          else
-            puts "Value for #{var} is not set. Substituting with an empty string." unless skip_validation?
-            ''
+        text.split(/[\r\n]/).map do |row|
+          row.gsub(/(?<!\$)\$(?!\$)\{?\w+\}?/) do |v| # searches $VAR and ${VAR} and not $$VAR
+            var = v.tr('${}', '')
+            val = variables.value_of(var) || ENV[var]
+            if val
+              if val.kind_of?(String) && val =~ /[\r\n\"\|]/
+                val.inspect
+              else
+                val
+              end
+            elsif @replace_missing
+              @replace_missing
+            else
+              puts "Value for #{var} is not set. Substituting with an empty string." unless skip_validation?
+              ''
+            end
           end
-        end
+        end.join("\n")
       end
 
       ##
