65f0c885466f36c7232f0ce6968ec9444e7016ea
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-12-08 06:49:37 +0200

    fix specs

--- cli/spec/kontena/cli/stacks/deploy_command_spec.rb
+++ cli/spec/kontena/cli/stacks/deploy_command_spec.rb
@@ -6,6 +6,10 @@ describe Kontena::Cli::Stacks::DeployCommand do
   include ClientHelpers
 
   describe '#execute' do
+    before(:each) do
+      allow(subject).to receive(:wait_for_deploy_to_finish).and_return(spy)
+    end
+
     it 'requires api url' do
       expect(described_class.requires_current_master?).to be_truthy
       subject.run(['test-stack'])
