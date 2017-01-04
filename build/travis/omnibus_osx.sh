5ede2c374986c83badf38749e6855a5fc3bd678f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-01-04 08:59:38 +0200

    build with sudo

--- build/travis/omnibus_osx.sh
+++ build/travis/omnibus_osx.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 sudo mkdir /opt/kontena
-sudo chown travis /opt/kontena
 cd cli/omnibus
 export USE_SYSTEM_GECODE=1
 brew install gecode
 bundle install
-bundle exec omnibus build kontena
+sudo bundle exec omnibus build kontena
