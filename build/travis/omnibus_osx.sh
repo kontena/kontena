1d4cbca443c1bf85fed543a7f27fdd934098ef44
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-01-04 09:09:49 +0200

    fix permissions

--- build/travis/omnibus_osx.sh
+++ build/travis/omnibus_osx.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 sudo mkdir /opt/kontena
+sudo chown travis /opt/kontena /usr/local/bin
 cd cli/omnibus
 export USE_SYSTEM_GECODE=1
 brew install gecode
 bundle install
-sudo bundle exec omnibus build kontena
+bundle exec omnibus build kontena
