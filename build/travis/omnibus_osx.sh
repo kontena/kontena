a1a3feea4ccfdc260d07f55277f129c7f6148cb5
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-01-05 13:16:24 +0200

    upload pkg to github

--- build/travis/omnibus_osx.sh
+++ build/travis/omnibus_osx.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 set -e
-wget https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2
-tar xjf linux-amd64-github-release.tar.bz2
+
 sudo mkdir /opt/kontena
 sudo chown travis /opt/kontena /usr/local/bin
 cd cli/omnibus
+
+# faster bundle install
 export USE_SYSTEM_GECODE=1
-brew install gecode
+brew install gecode github-release
+
+# install omnibus bundle
 bundle install
+
+# build kontena pkg
 bundle exec omnibus build kontena --log-level info
+
+# upload kontena pkg to github
+github-release upload \
+    --user kontena \
+    --repo kontena \
+    --tag v1.0.4.rc2 \
+    --name "kontena-cli-osx-amd64" \
+    --file pkg/kontena-*.pkg
