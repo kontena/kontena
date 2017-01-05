ab8ff216c32a245d55b677bf5dfc06ffbd3b618e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-01-05 12:36:49 +0200

    add github-release cli

--- build/travis/omnibus_osx.sh
+++ build/travis/omnibus_osx.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+set -e
+wget https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2
+tar xjf linux-amd64-github-release.tar.bz2
 sudo mkdir /opt/kontena
 sudo chown travis /opt/kontena /usr/local/bin
 cd cli/omnibus
 export USE_SYSTEM_GECODE=1
 brew install gecode
 bundle install
-bundle exec omnibus build kontena
+bundle exec omnibus build kontena --log-level info
