9e6836785d62eb1e7bba7a312cdff454c19a4907
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-01-10 11:45:14 +0200

    Add server version header X-Kontena-Version to all responses

---
+++ server/app/middlewares/version_injector.rb
@@ -0,0 +1,14 @@
+class VersionInjector
+
+  X_KONTENA = 'X-Kontena-Version'.freeze
+
+  def initialize(app, version)
+    @app, @version = app, version
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    headers[X_KONTENA] = version
+    [status, headers, body]
+  end
+end
