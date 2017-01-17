36557f118297c6b5109b48018ecf752a0fa745a1
Tero Marttila <SpComb@users.noreply.github.com>
2017-01-17 11:18:54 +0200

    Agent: fix cAdvisor stats to ignore systemd Docker container mount cgroups (#1657)
    * agent: Use explicit cdavisor docker/.... container path
    
    * fix different cadvisor container api response format
    
    * specfix
    
    * collect stats from cadvisor api /subcontainers
    
    * fixup
    
    * specs
    
    * Fix cadvisor stats container id, name

--- agent/lib/kontena/workers/stats_worker.rb
+++ agent/lib/kontena/workers/stats_worker.rb
@@ -47,25 +47,29 @@ module Kontena::Workers
 
     def collect_stats
       info 'starting collection'
-      begin
-        Docker::Container.all.each do |container|
-          if container.running?
-            data = self.collect_container_stats(container.id)
-            send_container_stats(data) if data
-            sleep 0.5
-          end
+
+      if response = get("/api/v1.2/subcontainers")
+        response.each do |data|
+          next unless data[:namespace] == 'docker'
+
+          # Skip systemd mount units that confuse cadvisor
+          # @see https://github.com/kontena/kontena/issues/1656
+          next if data[:name].end_with? '.mount'
+
+          send_container_stats(data)
         end
-      rescue => exc
-        error "error on stats fetching: #{exc.message}"
-        error exc.backtrace.join("\n")
       end
+
+    rescue => exc
+      error "error on stats fetching: #{exc.message}"
+      error exc.backtrace.join("\n")
     end
 
 
-    def collect_container_stats(container_id)
+    def get(path)
       retries = 3
       begin
-        response = client.get(:path => "/api/v1.2/docker/#{container_id}")
+        response = client.get(:path => path)
         if response.status == 200
           JSON.parse(response.body, symbolize_names: true) rescue nil
         else
@@ -77,7 +81,7 @@ module Kontena::Workers
         if retries > 0
           retry
         end
-        error "error getting container(#{container_id}) stats. #{exc.class.name}: #{exc.message}"
+        error "get #{path}: #{exc.class.name}: #{exc.message}"
         nil
       end
     end
@@ -85,24 +89,25 @@ module Kontena::Workers
     ##
     # @param [Hash] container
     def send_container_stats(container)
-      # when single container stats are used cadvisor "prefixes" all the data with some stupid system slice sutff
-      slice_key = container.keys[0]
+      id = container[:id]
+      name = container[:aliases].select{|a| a != id}.first
 
-      prev_stat = container.dig(slice_key, :stats)[-2] if container
+      prev_stat = container.dig(:stats)[-2] if container
       return if prev_stat.nil?
 
-      current_stat = container.dig(slice_key, :stats, -1)
+      current_stat = container.dig(:stats, -1)
       # Need to default to something usable in calculations
       cpu_usages = current_stat.dig(:cpu, :usage, :per_cpu_usage)
       num_cores = cpu_usages ? cpu_usages.count : 1
       raw_cpu_usage = current_stat.dig(:cpu, :usage, :total) - prev_stat.dig(:cpu, :usage, :total)
       interval_in_ns = get_interval(current_stat.dig(:timestamp), prev_stat.dig(:timestamp))
 
+
       event = {
         event: 'container:stats'.freeze,
         data: {
-          id: container.dig(slice_key, :aliases, 1),
-          spec: container.dig(slice_key, :spec),
+          id: id,
+          spec: container.dig(:spec),
           cpu: {
             usage: raw_cpu_usage,
             usage_pct: (((raw_cpu_usage / interval_in_ns ) / num_cores ) * 100).round(2)
@@ -117,7 +122,7 @@ module Kontena::Workers
         }
       }
       self.queue << event
-      send_statsd_metrics(container.dig(slice_key, :aliases, 0), event[:data])
+      send_statsd_metrics(name, event[:data])
     end
 
     def client
--- agent/spec/lib/kontena/workers/stats_workers_spec.rb
+++ agent/spec/lib/kontena/workers/stats_workers_spec.rb
@@ -22,38 +22,52 @@ describe Kontena::Workers::StatsWorker do
 
   describe '#collect_stats' do
     it 'loops through all containers' do
-      containers = [container, spy(:container, id: 'bar', labels: {}, running?: false)]
-      allow(Docker::Container).to receive(:all).and_return(containers)
-      expect(subject.wrapped_object).to receive(:collect_container_stats).once.with('foo').and_return({})
-      expect(subject.wrapped_object).to receive(:send_container_stats).once
+      expect(subject.wrapped_object).to receive(:get).once.with('/api/v1.2/subcontainers').and_return([
+        { namespace: 'docker', id: 'id', name: '/docker/id' },
+      ])
+      expect(subject.wrapped_object).to receive(:send_container_stats).once { |args| expect(args[:id]).to eq 'id' }
+      subject.collect_stats
+    end
+
+    it 'ignores systemd mount cgroups' do
+      expect(subject.wrapped_object).to receive(:get).once.with('/api/v1.2/subcontainers').and_return([
+        { namespace: 'docker', id: 'id1', name: '/docker/id' },
+        { namespace: 'docker', id: 'id2', name: '/system.slice/var-lib-docker-containers-id-shm.mount' },
+      ])
+      expect(subject.wrapped_object).to receive(:send_container_stats).once { |args| expect(args[:id]).to eq 'id1' }
+      subject.collect_stats
+    end
+
+    it 'does nothing on get error' do
+      expect(subject.wrapped_object).to receive(:get).once.with('/api/v1.2/subcontainers').and_return(nil)
+      expect(subject.wrapped_object).not_to receive(:send_container_stats)
       subject.collect_stats
     end
 
     it 'does not call send_stats if no container stats found' do
-      allow(Docker::Container).to receive(:all).and_return([container])
-      expect(subject.wrapped_object).to receive(:collect_container_stats).once.with('foo').and_return(nil)
+      expect(subject.wrapped_object).to receive(:get).once.with('/api/v1.2/subcontainers').and_return({})
       expect(subject.wrapped_object).not_to receive(:send_container_stats)
       subject.collect_stats
     end
   end
 
-  describe '#collect_container_stats' do
+  describe '#get' do
     it 'gets cadvisor stats for given container' do
       excon = double
       response = double
       allow(subject.wrapped_object).to receive(:client).and_return(excon)
-      expect(excon).to receive(:get).with(:path => '/api/v1.2/docker/foo').and_return(response)
+      expect(excon).to receive(:get).with(:path => '/api/v1.2/foo').and_return(response)
       allow(response).to receive(:status).and_return(200)
       allow(response).to receive(:body).and_return('{"foo":"bar"}')
-      expect(subject.collect_container_stats('foo')).to eq({:foo => "bar"})
+      expect(subject.get('/api/v1.2/foo')).to eq({:foo => "bar"})
     end
 
     it 'retries 3 times' do
       excon = double
       allow(subject.wrapped_object).to receive(:client).and_return(excon)
-      allow(excon).to receive(:get).with(:path => '/api/v1.2/docker/foo').and_raise(Excon::Errors::Error)
+      allow(excon).to receive(:get).with(:path => '/api/v1.2/foo').and_raise(Excon::Errors::Error)
       expect(excon).to receive(:get).exactly(3).times
-      subject.collect_container_stats('foo')
+      subject.get('/api/v1.2/foo')
     end
 
 
@@ -61,10 +75,10 @@ describe Kontena::Workers::StatsWorker do
       excon = double
       response = double
       allow(subject.wrapped_object).to receive(:client).and_return(excon)
-      allow(excon).to receive(:get).with(:path => '/api/v1.2/docker/foo').and_return(response)
+      allow(excon).to receive(:get).with(:path => '/api/v1.2/foo').and_return(response)
       allow(response).to receive(:status).and_return(500)
       allow(response).to receive(:body).and_return('{"foo":"bar"}')
-      expect(subject.collect_container_stats('foo')).to eq(nil)
+      expect(subject.get('/api/v1.2/foo')).to eq(nil)
     end
 
   end
@@ -147,9 +161,9 @@ describe '#send_container_stats' do
             usage: 1024,
             working_set: 2048
           },
-          filesystem: event.dig(event.keys[0], :stats, -1, :filesystem),
-          diskio: event.dig(event.keys[0], :stats, -1, :diskio),
-          network: event.dig(event.keys[0], :stats, -1, :network)
+          filesystem: event.dig(:stats, -1, :filesystem),
+          diskio: event.dig(:stats, -1, :diskio),
+          network: event.dig(:stats, -1, :network)
         }
       ))
       expect {
@@ -158,7 +172,7 @@ describe '#send_container_stats' do
     end
 
     it 'does not fail on missing cpu stats' do
-      event[event.keys[0]][:stats][-1][:cpu][:usage][:per_cpu_usage] = nil
+      event[:stats][-1][:cpu][:usage][:per_cpu_usage] = nil
       expect(subject.wrapped_object).to receive(:send_statsd_metrics).with('weave', hash_including({
           id: 'a675a5cd5f36ba747c9495f3dbe0de1d5f388a2ecd2aaf5feb00794e22de6c5e',
           spec: 'spec',
@@ -170,9 +184,9 @@ describe '#send_container_stats' do
             usage: 1024,
             working_set: 2048
           },
-          filesystem: event.dig(event.keys[0], :stats, -1, :filesystem),
-          diskio: event.dig(event.keys[0], :stats, -1, :diskio),
-          network: event.dig(event.keys[0], :stats, -1, :network)
+          filesystem: event.dig(:stats, -1, :filesystem),
+          diskio: event.dig(:stats, -1, :diskio),
+          network: event.dig(:stats, -1, :network)
         }
       ))
       expect {
