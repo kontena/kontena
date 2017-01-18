7a71f8883941ee8755297a30cfeb0e011f12361a
Tero Marttila <tero.marttila@kontena.io>
2017-01-18 18:37:29 +0200

    cli: etcd health

---
+++ cli/lib/kontena/cli/etcd/health_command.rb
@@ -0,0 +1,44 @@
+require_relative 'common'
+
+module Kontena::Cli::Etcd
+  class HealthCommand < Kontena::Command
+    include Kontena::Cli::Common
+    include Kontena::Cli::GridOptions
+
+    parameter "[NODE]", "Show health for specific node"
+
+    def execute
+      require_api_url
+      token = require_token
+
+      health = true
+
+      if node
+        node = client(token).get("nodes/#{current_grid}/#{node}")
+        node_health = client(token).get("nodes/#{current_grid}/#{node}/health")
+
+        health = show_node_health node, node_health['etcd']
+      else
+        nodes = client(token).get("grids/#{current_grid}/nodes")
+
+        nodes['nodes'].each do |node|
+          node_health = client(token).get("nodes/#{current_grid}/#{node['id']}/health")
+
+          health = false unless show_node_health node, node_health['etcd']
+        end
+      end
+
+      return health
+    end
+
+    def show_node_health(node, etcd_health)
+      if etcd_health['health']
+        puts "Node #{node['name']} is healthy"
+      elsif etcd_health['error']
+        puts "Node #{node['name']} is unhealthy: #{etcd_health['error']}"
+      else
+        puts "Node #{node['name']} is unhealthy"
+      end
+    end
+  end
+end
