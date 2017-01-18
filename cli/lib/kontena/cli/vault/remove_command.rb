0183682d97abc14a4a0d59f86a1236afbef828c2
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-01-18 10:47:22 +0200

    Vault import/export (#1655)
    [CLI] Add kontena vault import / export

--- cli/lib/kontena/cli/vault/remove_command.rb
+++ cli/lib/kontena/cli/vault/remove_command.rb
@@ -5,6 +5,7 @@ module Kontena::Cli::Vault
 
     parameter "NAME", "Secret name"
     option "--force", :flag, "Force remove", default: false, attribute_name: :forced
+    option "--silent", :flag, "Reduce output verbosity"
 
     def execute
       require_api_url
@@ -12,7 +13,7 @@ module Kontena::Cli::Vault
       confirm_command(name) unless forced?
 
       token = require_token
-      spinner "Removing #{name.colorize(:cyan)} from the vault " do
+      vspinner "Removing #{name.colorize(:cyan)} from the vault " do
         client(token).delete("secrets/#{current_grid}/#{name}")
       end
     end
