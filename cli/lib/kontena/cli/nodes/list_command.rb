bdeb59e91115672fed048bb447b9e09de19b99d5
Tero Marttila <tero.marttila@kontena.io>
2017-01-18 12:06:46 +0200

    Fix kontena node ls to only show a single icon per node

--- cli/lib/kontena/cli/nodes/list_command.rb
+++ cli/lib/kontena/cli/nodes/list_command.rb
@@ -8,19 +8,21 @@ module Kontena::Cli::Nodes
 
     option ["--all"], :flag, "List nodes for all grids", default: false
 
-    def node_status(node)
-      if node['connected']
-        return health_symbol(:ok), "online"
+    def node_health(node, grid_health)
+      if !node['connected']
+        return :offline
+      elsif !node['initial_member']
+        return :ok
       else
-        return health_symbol(:error), "offline"
+        return grid_health[:health]
       end
     end
 
-    def node_intial(node, grid_health)
-      if !node['initial_member']
-        return health_symbol(nil), "-"
+    def node_initial(node, grid)
+      if node['initial_member']
+        return "#{node['node_number']} / #{grid['initial_size']}"
       else
-        return health_symbol(node['connected'] ? grid_health[:health] : :unknown), "#{node['node_number']} / #{grid_health[:initial]}"
+        return "-"
       end
     end
 
@@ -29,16 +31,17 @@ module Kontena::Cli::Nodes
     end
 
     def show_grid_nodes(grid, multi: false)
-      grid_nodes = client(require_token).get("grids/#{current_grid}/nodes")
+      grid_nodes = client(require_token).get("grids/#{grid['id']}/nodes")
       grid_health = check_grid_health(grid, grid_nodes['nodes'])
 
       nodes = grid_nodes['nodes'].sort_by{|n| n['node_number'] }
       nodes.each do |node|
         puts [
+          "%s" % health_icon(node_health(node, grid_health)),
           "%-70.70s" % [multi ? "#{grid['name']}/#{node['name']}" : node['name']],
           "%-10s" % node['agent_version'],
-          "%s %-10s" % node_status(node),
-          "%s %-10s" % node_intial(node, grid_health),
+          "%-10s" % (node['connected'] ? "online" : "offline"),
+          "%-10s" % node_initial(node, grid),
           "%s" % [node_labels(node)],
         ].join ' '
       end
@@ -49,7 +52,7 @@ module Kontena::Cli::Nodes
       require_current_grid
       token = require_token
 
-      puts "%-70s %-10s %-12s %-12s %-40s" % ["Name", "Version", "  Status", "  Initial", "Labels"]
+      puts "%s %-70s %-10s %-10s %-10s %-s" % [health_icon(nil), "Name", "Version", "Status", "Initial", "Labels"]
 
       if all?
         grids = client(token).get("grids")
