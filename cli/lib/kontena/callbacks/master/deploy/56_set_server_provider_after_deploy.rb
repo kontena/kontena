91da74c914b2a24df76774b3a9f901748eba13e9
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-01-19 13:02:16 +0200

    If master deploy returns :provider set it to config server.provider (#1675)
    [CLI] Set master server.provider during deploy

---
+++ cli/lib/kontena/callbacks/master/deploy/56_set_server_provider_after_deploy.rb
@@ -0,0 +1,25 @@
+module Kontena
+  module Callbacks
+    class SetServerProviderAfterDeploy < Kontena::Callback
+
+      include Kontena::Cli::Common
+
+      matches_commands 'master create'
+
+      def after
+        return unless command.exit_code == 0
+        return unless config.current_master
+        return unless config.current_master.name == command.result[:name]
+        return unless command.result[:provider]
+
+        require 'shellwords'
+
+        cmd = ['master', 'config', 'set', "server.provider=#{command.result[:provider]}"]
+        spinner "Setting Master configuration server.provider to '#{command.result[:provider]}'" do
+          Kontena.run(cmd.shelljoin)
+        end
+      end
+    end
+  end
+end
+
