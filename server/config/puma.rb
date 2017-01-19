b6ebbef3b3608805a5746102be85a05ce24c064c
Jussi Nummelin <jussi@kontena.io>
2017-01-19 19:51:10 +0200

    remove worker check from migration launch, distributed lock should cover that

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -4,10 +4,7 @@ workers Integer(ENV['WEB_CONCURRENCY'] || Etc.nprocessors)
 threads_count = Integer(ENV['MAX_THREADS'] || 8)
 threads threads_count, threads_count
 on_worker_boot do |worker_num|
-  # Run migrations only on first worker, worker_num is zero based
-  if worker_num == 0
-    require_relative '../app/boot'
-    require_relative '../app/services/mongodb/migrator'
-    Mongodb::Migrator.new.migrate
-  end
+  require_relative '../app/boot'
+  require_relative '../app/services/mongodb/migrator'
+  Mongodb::Migrator.new.migrate
 end
