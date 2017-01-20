1179f84c950ed8913a0fd91b0e48aefeaa8f84ca
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-01-20 11:13:51 +0200

    Add log filtering specs

--- server/spec/api/root_spec.rb
+++ server/spec/api/root_spec.rb
@@ -10,4 +10,16 @@ describe '/' do
     get '/', nil, {}
     expect(json_response['version']).not_to be_nil
   end
+
+  context 'logging' do
+    it 'should not log requests with ?health' do
+      expect_any_instance_of(Logger).not_to receive(:write)
+      get '/?health', nil, {}
+    end
+
+    it 'should log requests without ?health' do
+      expect_any_instance_of(Logger).to receive(:write)
+      get '/', nil, {}
+    end
+  end
 end
