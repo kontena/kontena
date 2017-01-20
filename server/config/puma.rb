f69396ce891885e655f8b4676ff454ac110a180e
Jussi Nummelin <jnummelin@users.noreply.github.com>
2017-01-20 09:37:11 +0200

    Set puma workers based on available CPU cores (#1683)
    [Master] Set web concurrency based on available CPU cores

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -1,7 +1,9 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+require 'etc'
+
+workers Integer(ENV['WEB_CONCURRENCY'] || Etc.nprocessors)
 threads_count = Integer(ENV['MAX_THREADS'] || 8)
 threads threads_count, threads_count
-on_worker_boot do
+on_worker_boot do |worker_num|
   require_relative '../app/boot'
   require_relative '../app/services/mongodb/migrator'
   Mongodb::Migrator.new.migrate
