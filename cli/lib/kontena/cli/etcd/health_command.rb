079530b0584dccf3dfb1aba8214b712a2b59c027
Tero Marttila <tero.marttila@kontena.io>
2017-01-27 12:37:08 +0200

    cli etcd health: fix to always show all nodes, use new node_health API

--- cli/lib/kontena/cli/etcd/health_command.rb
+++ cli/lib/kontena/cli/etcd/health_command.rb
@@ -8,6 +8,9 @@ module Kontena::Cli::Etcd
 
     parameter "[NODE]", "Show health for specific node"
 
+    requires_current_master
+    requires_current_grid
+
     def execute
       require_api_url
       token = require_token
@@ -15,49 +18,39 @@ module Kontena::Cli::Etcd
       health = true
 
       if self.node
-        node = client(token).get("nodes/#{current_grid}/#{self.node}")
+        node_health = client.get("nodes/#{current_grid}/#{self.node}/health")
 
-        health = show_node(node)
+        health = show_node_health(node_health)
       else
-        nodes = client(token).get("grids/#{current_grid}/nodes")
+        nodes = client.get("grids/#{current_grid}/nodes")['nodes']
 
-        nodes['nodes'].each do |node|
-          health &&= show_node(node)
+        nodes.each do |node|
+          node_health = client.get("nodes/#{current_grid}/#{node['name']}/health")
+
+          if !show_node_health(node_health)
+            health = false
+          end
         end
       end
 
       return health
     end
 
-    def show_node(node)
-      token = require_token
+    # @return [Boolean]
+    def show_node_health(node_health)
+      etcd_health = node_health['etcd_health']
 
-      if !node['connected']
-        puts "#{health_icon :offline} Node #{node['name']} is offline"
+      if !node_health['connected']
+        puts "#{health_icon :offline} Node #{node_health['name']} is offline"
         return false
-      end
-
-      begin
-        node_health = client(token).get("nodes/#{current_grid}/#{node['id']}/health")
-      rescue Kontena::Errors::StandardError => error
-        raise unless error.status == 503
-
-        puts "#{health_icon :warning} Node #{node['name']} health error: #{error}"
-        return false
-      end
-
-      return show_node_health node, node_health['etcd']
-    end
-
-    def show_node_health(node, etcd_health)
-      if etcd_health['health']
-        puts "#{health_icon :ok} Node #{node['name']} is healthy"
+      elsif etcd_health['health']
+        puts "#{health_icon :ok} Node #{node_health['name']} is healthy"
         return true
       elsif etcd_health['error']
-        puts "#{health_icon :error} Node #{node['name']} is unhealthy: #{etcd_health['error']}"
+        puts "#{health_icon :error} Node #{node_health['name']} is unhealthy: #{etcd_health['error']}"
         return false
       else
-        puts "#{health_icon :error} Node #{node['name']} is unhealthy"
+        puts "#{health_icon :error} Node #{node_health['name']} is unhealthy"
         return false
       end
     end
