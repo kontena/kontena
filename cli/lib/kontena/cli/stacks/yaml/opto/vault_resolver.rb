418629705275fa85ed1b611a67306fe3988c8234
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-01-27 14:49:19 +0200

    Better error message when vault key is nil/empty in stack vault resolver (#1728)
    * Better error message when vault key nil/empty in vault resolver
    
    * Change / to "or"

--- cli/lib/kontena/cli/stacks/yaml/opto/vault_resolver.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/vault_resolver.rb
@@ -2,6 +2,7 @@ module Kontena::Cli::Stacks
   module YAML
     class Opto::Resolvers::Vault < Opto::Resolver
       def resolve
+        raise RuntimeError, "Missing or empty vault secret name" if hint.to_s.empty?
         require 'shellwords'
         Kontena.run("vault read --return #{hint.shellescape}", returning: :result)
       end
