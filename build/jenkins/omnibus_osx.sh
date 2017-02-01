5f38778b58c8d040775ffd1f0131712b32440bed
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-02-01 14:42:44 +0200

    fix jenkins build script

--- build/jenkins/omnibus_osx.sh
+++ build/jenkins/omnibus_osx.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
-
+unset BUILD_ID
 rm -rf /opt/kontena/*
 rm -f /usr/local/bin/kontena || true
+rm -f pkg/*
+
 cd cli/omnibus
 
 # faster bundle install
 export USE_SYSTEM_GECODE=1
+
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+chruby 2.3.3
 
 # install omnibus bundle
 bundle install
@@ -18,6 +23,6 @@ bundle exec omnibus build kontena --log-level info
 github-release upload \
     --user kontena \
     --repo kontena \
-    --tag $TRAVIS_TAG \
-    --name "kontena-cli-osx-${TRAVIS_TAG}-amd64.pkg" \
+    --tag $GIT_TAG_NAME \
+    --name "kontena-cli-osx-${GIT_TAG_NAME}-amd64.pkg" \
     --file pkg/kontena-*.pkg
