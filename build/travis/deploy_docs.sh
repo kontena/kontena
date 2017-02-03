982aee97400fe7a0d304bc0abe48df68aecf3551
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-02-03 11:00:54 +0200

    split travis deploy/release tasks

--- build/travis/deploy_docs.sh
+++ build/travis/deploy_docs.sh
@@ -8,5 +8,7 @@ gem install --no-ri --no-doc bundler rake colorize dotenv
 
 cd $TRAVIS_BUILD_DIR
 rake release:setup
-rake release:push
-rake release:push_ubuntu
+
+export GA_CODE=$DOCS_GA
+export HUBSPOT_CODE=$DOCS_HS
+rake release:push_docs
