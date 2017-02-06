ff9a8f8dba8db4f5bba9aff12447a7ce6d83c92b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-02-06 12:27:11 +0200

    add DEBUG_KOMMANDO env

--- test/spec/features/grid_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe 'grid commands' do
-  
-end
--- test/spec/support/shell.rb
+++ test/spec/support/shell.rb
@@ -1,11 +1,23 @@
 module Shell
 
+  # @param [String] cmd
+  # @param [Hash] opts
+  # @return [Kommando]
   def run(cmd, opts = {})
+    opts[:output] = debug? unless opts.has_key?(:output)
     Kommando.run(cmd, opts)
   end
 
+  # @param [String] cmd
+  # @param [Hash] opts
+  # @return [Kommando]
   def kommando(cmd, opts = {})
+    opts[:output] = debug? unless opts.has_key?(:output)
     Kommando.new(cmd, opts)
+  end
+
+  def debug?
+    ENV.has_key?('DEBUG')
   end
 
   def ctrl_c
