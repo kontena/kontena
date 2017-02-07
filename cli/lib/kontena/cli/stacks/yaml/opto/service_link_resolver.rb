25698a1ce98dfe7d59475d0fae0054fba571899b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-02-07 12:34:21 +0200

    fix ServiceLink resolver

--- cli/lib/kontena/cli/stacks/yaml/opto/service_link_resolver.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/service_link_resolver.rb
@@ -1,44 +1,56 @@
 module Kontena::Cli::Stacks::YAML::Opto
-    class ServiceLink < Opto::Resolver
-      include Kontena::Cli::Common
+  class ServiceLink < Opto::Resolver
+    include Kontena::Cli::Common
 
-      def resolve
-        message = hint['prompt']
-        name_filter = hint['name']
-        image_filter = hint['image']
-        raise "prompt missing" unless message
+    def resolve
+      message = hint['prompt']
+      name_filter = hint['name']
+      image_filter = hint['image']
+      raise "prompt missing" unless message
 
-        services = client.get("grids/#{current_grid}/services")['services']
-        services = filter_by_image(services, image_filter) if image_filter
-        services = filter_by_name(services, name_filter) if name_filter
-        prompt.select(message) do |menu|
-          menu.choice "<none>", nil unless option.required?
-          services.each do |s|
-            if s.dig('stack', 'name') == 'null'
-              name = s['name']
-            else
-              name = "#{s.dig('stack', 'name')}/#{s['name']}"
-            end
-            menu.choice name, "#{s.dig('stack', 'name')}/#{s['name']}"
+      services = client.get("grids/#{current_grid}/services")['services']
+      services = filter_by_image(services, image_filter) if image_filter
+      services = filter_by_name(services, name_filter) if name_filter
+      return nil if services.size == 0
+      prompt.select(message) do |menu|
+        menu.default(default_index(services)) if option.default
+        menu.choice "<none>", nil unless option.required?
+        services.each do |s|
+          if s.dig('stack', 'name') == 'null'
+            name = s['name']
+          else
+            name = "#{s.dig('stack', 'name')}/#{s['name']}"
           end
+          menu.choice name, "#{s.dig('stack', 'name')}/#{s['name']}"
         end
       end
+    end
 
-      def filter_by_image(services, image)
-        services.select { |s|
-          s['image'].include?(image)
-        }
-      end
+    # @param [Array<Hash>] services
+    # @return [Integer]
+    def default_index(services)
+      services.index {|s| service_link(s) == option.default }.to_i + 1
+    end
 
-      def filter_by_name(services, name)
-        services.select { |s|
-          s['name'].include?(name)
-        }
-      end
+    # @param [Hash] service
+    def service_link(service)
+      "#{service.dig('stack', 'name')}/#{service['name']}"
+    end
 
-      def stack
-        ENV['STACK']
-      end
+    def filter_by_image(services, image)
+      services.select { |s|
+        s['image'].include?(image)
+      }
+    end
+
+    def filter_by_name(services, name)
+      services.select { |s|
+        s['name'].include?(name)
+      }
+    end
+
+    def stack
+      ENV['STACK']
     end
   end
 end
