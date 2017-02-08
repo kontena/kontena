6f5ad7c76bc160596759922906799fa8b991439d
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-08 09:45:40 +0200

    Specs

---
+++ cli/spec/kontena/main_command_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../spec_helper'
+require 'kontena/main_command'
+
+describe Kontena::MainCommand do
+  let(:subject) { described_class.new('kontena') }
+
+  describe '--version' do
+    it 'outputs the version number and exits' do
+      expect do
+        expect{subject.run(['--version'])}.to output(/kontena-cli #{Kontena::Cli::VERSION}/).to_stdout
+      end.to raise_error(SystemExit) do |exc|
+        expect(exc.status).to eq 0
+      end
+    end
+  end
+end
