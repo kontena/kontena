3e1b14e7034e19efffc0265537c08514d1f13b0e
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-22 14:26:30 +0200

    Add ".pre.head_abcd1234" to Kontena::Cli::VERSION when path includes HEAD-abcd1234

--- cli/lib/kontena/cli/version.rb
+++ cli/lib/kontena/cli/version.rb
@@ -1,7 +1,13 @@
 module Kontena
   module Cli
     unless const_defined?(:VERSION)
-      VERSION = File.read(File.realpath(File.join(__dir__, '../../../VERSION'))).strip + ENV["_HOMEBREW_HEAD"].to_s
+      require 'pathname'
+      version_file = Pathname.new(__FILE__).dirname.join('../../../VERSION').realpath
+      if ENV["_KONTENA_HOMEBREW"]
+        head_path = version_file.to_s.split(File::SEPARATOR).detect { |component| component.start_with?('HEAD') }
+        head = head_path.nil? ? nil : ".pre.#{head_path.gsub("-", '_').downcase}"
+      end
+      VERSION = "#{version_file.read.strip}#{head}"
     end
   end
 end
