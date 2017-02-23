5741f0e77359c15f88ea8872745f0f92c10222da
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-23 16:19:26 +0200

    Extra dash

--- cli/lib/kontena/cli/version.rb
+++ cli/lib/kontena/cli/version.rb
@@ -4,7 +4,7 @@ module Kontena
       require 'pathname'
       version_file = Pathname.new(__FILE__).dirname.join('../../../VERSION').realpath
       is_head = ENV["KONTENA_EXTRA_BUILDTAGS"].to_s.include?('head')
-      VERSION = "#{version_file.read.strip}-#{"head" if is_head}"
+      VERSION = "#{version_file.read.strip}#{"-head" if is_head}"
     end
   end
 end
