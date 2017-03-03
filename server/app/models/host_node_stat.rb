96f97e5afbe9eabf63f9cf8bdea367900598f892
Daniel Battaglia <daniel.battaglia@kontena.io>
2017-03-03 13:23:05 -0500

    Adding code to gather host node CPU averages

--- server/app/models/host_node_stat.rb
+++ server/app/models/host_node_stat.rb
@@ -6,10 +6,12 @@ class HostNodeStat
   field :memory, type: Hash
   field :filesystem, type: Array
   field :usage, type: Hash
+  field :cpu_average, type: Hash
 
   belongs_to :grid
   belongs_to :host_node
 
   index({ grid_id: 1 })
   index({ host_node_id: 1 })
+  index({ created_at: 1 })
 end
