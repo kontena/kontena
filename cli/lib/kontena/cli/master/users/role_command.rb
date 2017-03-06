d45ff95be3fb00fecb8cbe44d27e394477f3e736
Daniel Battaglia <daniel.battaglia@kontena.io>
2017-03-06 12:28:46 -0500

    Fixing paths for nested sub commands in calls to load_subcommand.

--- cli/lib/kontena/cli/master/users/role_command.rb
+++ cli/lib/kontena/cli/master/users/role_command.rb
@@ -2,7 +2,7 @@ module Kontena::Cli::Master::Users
 
 
   class RoleCommand < Kontena::Command
-    subcommand "add", "Add role to user", load_subcommand('roles/add_command')
-    subcommand ["remove", "rm"], "Remove role from user", load_subcommand('roles/remove_command')
+    subcommand "add", "Add role to user", load_subcommand('master/users/roles/add_command')
+    subcommand ["remove", "rm"], "Remove role from user", load_subcommand('master/users/roles/remove_command')
   end
 end
--- cli/lib/kontena/cli/services/env_command.rb
+++ cli/lib/kontena/cli/services/env_command.rb
@@ -2,8 +2,8 @@ module Kontena::Cli::Services
 
 
   class EnvCommand < Kontena::Command
-    subcommand ["list", "ls"], "List service environment variables", load_subcommand('envs/list_command')
-    subcommand "add", "Add environment variable", load_subcommand('envs/add_command')
-    subcommand ["remove", "rm"], "Remove environment variable", load_subcommand('envs/remove_command')
+    subcommand ["list", "ls"], "List service environment variables", load_subcommand('services/envs/list_command')
+    subcommand "add", "Add environment variable", load_subcommand('services/envs/add_command')
+    subcommand ["remove", "rm"], "Remove environment variable", load_subcommand('services/envs/remove_command')
   end
 end
