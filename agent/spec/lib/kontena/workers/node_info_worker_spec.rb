31a8c0ee9e7ada0e0239f61ed380e64d0def5972
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-03-07 16:58:58 +0200

    Remove unnecessary require spec_helpers

--- agent/spec/lib/kontena/workers/node_info_worker_spec.rb
+++ agent/spec/lib/kontena/workers/node_info_worker_spec.rb
@@ -1,4 +1,3 @@
-require_relative '../../../spec_helper'
 
 describe Kontena::Workers::NodeInfoWorker do
   include RpcClientMocks
--- agent/spec/lib/kontena/workers/stats_workers_spec.rb
+++ agent/spec/lib/kontena/workers/stats_workers_spec.rb
@@ -1,4 +1,3 @@
-require_relative '../../../spec_helper'
 require_relative '../../helpers/fixtures_helpers'
 
 describe Kontena::Workers::StatsWorker do
