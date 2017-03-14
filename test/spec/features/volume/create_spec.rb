ec0bcca09fc0f845ffb7258188c163a12cebd9e3
Jussi Nummelin <jussi@kontena.io>
2017-03-14 15:23:10 +0200

    fix e2e specs for volumes

--- test/spec/features/volume/create_spec.rb
+++ test/spec/features/volume/create_spec.rb
@@ -2,25 +2,36 @@ require 'spec_helper'
 
 describe 'volume create' do
   after(:each) do
-    run "kontena volume rm null/testVol"
+    run "kontena volume rm testVol"
   end
 
   it 'creates a volume' do
-    k = run "kontena volume create null testVol"
+    k = run "kontena volume create --driver local --scope instance testVol"
     expect(k.code).to eq(0)
     k = run "kontena volume ls"
-    expect(k.out.match(/null\/testVol/)).to be_truthy
+    expect(k.out.match(/testVol/)).to be_truthy
+  end
+
+  it 'fails to create volume without driver' do
+    k = run "kontena volume create --scope instance testVol"
+    expect(k.code).not_to eq(0)
+  end
+
+
+  it 'fails to create volume without scope' do
+    k = run "kontena volume create --driver local testVol"
+    expect(k.code).not_to eq(0)
   end
 
 
   it 'removes a volume' do
-    k = run "kontena volume create null testVol"
+    k = run "kontena volume create --driver local --scope instance testVol"
     expect(k.code).to eq(0)
 
-    k = run "kontena volume rm null/testVol"
+    k = run "kontena volume rm testVol"
     expect(k.code).to eq(0)
 
     k = run "kontena volume ls"
-    expect(k.out.match(/null\/testVol/)).to be_falsey
+    expect(k.out.match(/testVol/)).to be_falsey
   end
 end
--- test/spec/features/volume/service_volumes.rb
+++ test/spec/features/volume/service_volumes.rb
@@ -4,11 +4,11 @@ require 'json'
 describe 'service volumes' do
   after(:each) do
     run "kontena service rm --force null/redis"
-    run "kontena volume rm null/testVol"
+    run "kontena volume rm testVol"
   end
 
   it 'service uses a volume' do
-    k = run "kontena volume create null testVol"
+    k = run "kontena volume create --driver local --scope instance testVol"
     expect(k.code).to eq(0)
     k = run "kontena service create -v testVol:/data redis redis:alpine"
     expect(k.code).to eq(0)
@@ -19,16 +19,16 @@ describe 'service volumes' do
     json = JSON.parse(k.out)
     mount = json.dig('Mounts').find { |m| m['Name'] =~ /testVol/}
     expect(mount).not_to be_nil
-    expect(mount['Name']).to eq("testVol-redis-1")
+    expect(mount['Name']).to eq("redis.testVol-1")
     expect(mount['Destination']).to eq('/data')
   end
 
   it 'volume cannot be removed when still used by a service' do
-    k = run "kontena volume create null testVol"
+    k = run "kontena volume create --driver local --scope instance testVol"
     expect(k.code).to eq(0)
     k = run "kontena service create -v testVol:/data redis redis:alpine"
     expect(k.code).to eq(0)
-    k = run "kontena volume rm null/testVol"
+    k = run "kontena volume rm testVol"
     expect(k.code).not_to eq(0)
   end
 
