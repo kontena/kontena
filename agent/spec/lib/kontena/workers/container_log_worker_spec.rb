9ae0b24fd43acf2f92e2dcf3ec2e5d4d5867c4cd
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-03-17 13:57:59 +0200

    Backport #1993 to v1.1 (#1994)
    * discard logs if queue grows too big
    
    * skip log parsing if queue is full

--- agent/spec/lib/kontena/workers/container_log_worker_spec.rb
+++ agent/spec/lib/kontena/workers/container_log_worker_spec.rb
@@ -34,5 +34,19 @@ describe Kontena::Workers::ContainerLogWorker do
         subject.on_message('id', 'stdout', '2016-02-29T07:26:07.798612937Z log message')
       }.to change{ queue.length }.by(1)
     end
+
+    it 'adds message to queue if queue size is under limit' do
+      allow(queue).to receive(:size).and_return(described_class::QUEUE_LIMIT - 1)
+      expect {
+        subject.on_message('id', 'stdout', '2016-02-29T07:26:07.798612937Z log message')
+      }.to change{ queue.length }.by(1)
+    end
+
+    it 'does not add message to queue if queue size is over limit' do
+      allow(queue).to receive(:size).once.and_return(described_class::QUEUE_LIMIT + 1)
+      expect {
+        subject.on_message('id', 'stdout', '2016-02-29T07:26:07.798612937Z log message')
+      }.to change{ queue.length }.by(0)
+    end
   end
 end
