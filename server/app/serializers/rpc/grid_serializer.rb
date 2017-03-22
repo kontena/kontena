5194b58ef47d05f34eb830ccf8e073273caef8a0
Tero Marttila <tero.marttila@kontena.io>
2017-03-22 13:24:22 +0200

    fix Rpc::GridSerializer to include stats, logs fields

--- server/app/serializers/rpc/grid_serializer.rb
+++ server/app/serializers/rpc/grid_serializer.rb
@@ -8,9 +8,26 @@ module Rpc
     attribute :trusted_subnets
     attribute :subnet
     attribute :supernet
+    attribute :stats
+    attribute :logs
 
     def id
       object.to_path
+    end
+
+    def stats
+      {
+        statsd: object.stats['statsd'],
+      }
+    end
+
+    def logs
+      if object.grid_logs_opts
+        {
+          forwarder: object.grid_logs_opts.forwarder,
+          opts: object.grid_logs_opts.opts,
+        }
+      end
     end
   end
 end
