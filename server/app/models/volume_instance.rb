8a1ff8e0b864c8529c01817d7409c175fed1503d
Jussi Nummelin <jussi@kontena.io>
2017-03-30 21:05:29 +0300

    fix volume instance unique scope

--- server/app/models/volume_instance.rb
+++ server/app/models/volume_instance.rb
@@ -8,7 +8,7 @@ class VolumeInstance
 
   belongs_to :volume
 
-  validates_uniqueness_of :name, scope: [:volume]
+  validates_uniqueness_of :name, scope: [:host_node]
 
   index({ host_node_id: 1 })
   index({ volume_id: 1 })
