fab35e75f5fb05003efd2ee9f4889d8c8c62d50c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-03-30 11:32:26 +0300

    grid/stack/service event_log api endpoints

--- server/app/routes/v1/services/service_event_logs.rb
+++ server/app/routes/v1/services/service_event_logs.rb
@@ -3,7 +3,7 @@ V1::ServicesApi.route('service_event_logs') do |r|
   # GET /v1/services/:grid_name/:service_name/event_logs
   r.get do
     r.is do
-      scope = EventLog.where(grid_service_id: @grid_service.id).includes(:grid, :grid_service, :host_node)
+      scope = EventLog.where(grid_service_id: @grid_service.id).includes(:grid, :stack, :grid_service, :host_node)
       render_event_logs(r, scope)
     end
   end
--- server/spec/api/v1/services_spec.rb
+++ server/spec/api/v1/services_spec.rb
@@ -8,7 +8,7 @@ describe '/v1/services' do
   end
 
   let! :grid do
-    grid = Grid.create!(name: 'terminal-a')
+    Grid.create!(name: 'terminal-a')
   end
 
   let(:david) do
@@ -199,97 +199,6 @@ describe '/v1/services' do
       expect(response.status).to eq(200)
       expect(json_response['containers'].size).to eq(1)
       expect(json_response['containers'].first['id']).to eq(container.to_path)
-    end
-  end
-
-  describe 'GET /:id/container_logs' do
-    it 'returns service container logs' do
-      container = redis_service.containers.create!(name: 'redis-1', container_id: 'bbb')
-      container.container_logs.create!(data: 'foo', type: 'stdout', grid_service: redis_service)
-      get "/v1/services/#{redis_service.to_path}/container_logs", nil, request_headers
-      expect(response.status).to eq(200)
-      expect(json_response['logs'].size).to eq(1)
-      expect(json_response['logs'].first['data']).to eq('foo')
-    end
-
-    context 'when instance parameter is passed' do
-      it 'returns service container logs for related instance' do
-        container = redis_service.containers.create!(name: 'redis-1', container_id: 'aaa')
-        container2 = redis_service.containers.create!(name: 'redis-2', container_id: 'bbb')
-        log1 = container.container_logs.create!(name: 'redis-1', instance_number: 1, data: 'foo', type: 'stdout', grid_service: redis_service)
-        log2 = container2.container_logs.create!(name: 'redis-2', instance_number: 2, data: 'foo2', type: 'stdout', grid_service: redis_service)
-        get "/v1/services/#{redis_service.to_path}/container_logs?instance=1", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(1)
-        expect(json_response['logs'].first['data']).to eq('foo')
-      end
-    end
-
-    context 'when from parameter is passed' do
-      it 'returns service container logs created after passed id' do
-        container = redis_service.containers.create!(name: 'redis-1', container_id: 'aaa')
-        log1 = container.container_logs.create!(data: 'foo', type: 'stdout', grid_service: redis_service)
-        log2 = container.container_logs.create!(data: 'foo2', type: 'stdout', grid_service: redis_service)
-        get "/v1/services/#{redis_service.to_path}/container_logs?from=#{log1.id}", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(1)
-        expect(json_response['logs'].first['data']).to eq('foo2')
-      end
-    end
-
-    context 'when since parameter is passed' do
-      it 'returns service container logs created after passed timestamp' do
-        container = redis_service.containers.create!(name: 'redis-1', container_id: 'aaa')
-        log1 = container.container_logs.create!(
-          data: 'foo', type: 'stdout', grid_service: redis_service, created_at: 5.minutes.ago
-        )
-        log2 = container.container_logs.create!(
-          data: 'foo2', type: 'stdout', grid_service: redis_service, created_at: 3.minutes.ago
-        )
-        log3 = container.container_logs.create!(
-          data: 'foo3', type: 'stdout', grid_service: redis_service, created_at: 1.minutes.ago
-        )
-        get "/v1/services/#{redis_service.to_path}/container_logs?since=#{log2.created_at}", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(2)
-        expect(json_response['logs'].first['data']).to eq('foo2')
-      end
-    end
-
-    context 'when limit parameter is passed' do
-      before do
-        stub_const('LogsHelpers::LOGS_LIMIT_DEFAULT', 10)
-        stub_const('LogsHelpers::LOGS_LIMIT_MAX', 20)
-
-        container = redis_service.containers.create!(name: 'redis-1', container_id: 'aaa')
-        (1..100).each do |i|
-          container.container_logs.create!(data: "log #{i}", type: 'stdout', grid_service: redis_service)
-        end
-      end
-
-      it 'accepts a smaller value' do
-        get "/v1/services/#{redis_service.to_path}/container_logs?limit=2", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(2)
-      end
-
-      it 'ignores an invalid value' do
-        get "/v1/services/#{redis_service.to_path}/container_logs?limit=foo", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(10)
-      end
-
-      it 'ignores an zero value' do
-        get "/v1/services/#{redis_service.to_path}/container_logs?limit=0", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(10)
-      end
-
-      it 'limits the maximum value' do
-        get "/v1/services/#{redis_service.to_path}/container_logs?limit=100", nil, request_headers
-        expect(response.status).to eq(200)
-        expect(json_response['logs'].size).to eq(20)
-      end
     end
   end
 
