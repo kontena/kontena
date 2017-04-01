aaca19df49b694f3e9a0d5243af2207f502cbc5a
Colin Mollenhour <colin@mollenhour.com>
2017-03-31 22:59:49 -0400

    Fix broken update and import.

--- cli/lib/kontena/cli/vault/update_command.rb
+++ cli/lib/kontena/cli/vault/update_command.rb
@@ -7,13 +7,18 @@ module Kontena::Cli::Vault
 
     option ['-u', '--upsert'], :flag, 'Create secret unless already exists', default: false
     option '--silent', :flag, "Reduce output verbosity"
+    option ['-i', '--stdin'], :flag, 'Read value from stdin', default: false
+
+    def default_value
+      exit_with_error('No value provided') unless stdin?
+      STDIN.read.chomp
+    end
 
     def execute
       require_api_url
       require_current_grid
 
       token = require_token
-      value ||= STDIN.read.chomp
       data = {
         name: name,
         value: value,
