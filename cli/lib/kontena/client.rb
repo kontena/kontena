0824add7610dfdcc6c04355bf6a7afe95746641e
Tero Marttila <tero.marttila@kontena.io>
2017-04-03 17:59:15 +0300

    cli: StandardErrors as YAML output

--- cli/lib/kontena/client.rb
+++ cli/lib/kontena/client.rb
@@ -532,7 +532,7 @@ module Kontena
       data = parse_response(response)
 
       if data.is_a?(Hash) && data.has_key?('error')
-        raise Kontena::Errors::StandardError.new(response.status, data['error'])
+        raise Kontena::Errors::StandardErrors.new(response.status, response.reason_phrase, data['error'])
       elsif data.is_a?(String) && !data.empty?
         raise Kontena::Errors::StandardError.new(response.status, data)
       else
--- cli/lib/kontena/errors.rb
+++ cli/lib/kontena/errors.rb
@@ -4,9 +4,32 @@ module Kontena
 
       attr_reader :status
 
+      # @param status [Fixnum] HTTP response status
+      # @param message [String] short error message
       def initialize(status, message)
         @status = status
         super(message)
+      end
+    end
+
+    # The normal {error: {foo: "invalid foo"}} error response format used by the API
+    class StandardErrors < StandardError
+      attr_reader :errors
+
+      # @param errors [Hash]
+      def initialize(status, message, errors)
+        super(status, message)
+        @errors = errors
+      end
+
+      # Render as indented YAML
+      def errors_message(indent: "\t")
+        @errors.to_yaml.lines[1..-1].map{|line| "#{indent}#{line}" }.join
+      end
+
+      # Render the full multi-line message including YAML-formatted errors
+      def message
+        "#{super}:\n#{errors_message}"
       end
     end
   end
