74758b56315010cef485c97e04848aa9e6933f69
Tero Marttila <tero.marttila@kontena.io>
2017-04-04 15:12:25 +0300

    raise server rpc handler errors

--- server/app/services/rpc/node_service_pod_handler.rb
+++ server/app/services/rpc/node_service_pod_handler.rb
@@ -9,8 +9,9 @@ module Rpc
     # @return [Array<Hash>]
     def list(id)
       node = @grid.host_nodes.find_by(node_id: id)
-      return { error: 'Node not found' } unless node
-      return { error: 'Migration not done' } unless migration_done?
+      raise 'Node not found' unless node
+      raise 'Migration not done' unless migration_done?
+
       service_pods = node.grid_service_instances.includes(:grid_service).map { |i|
         ServicePodSerializer.new(i).to_hash if i.grid_service
       }.compact
@@ -22,20 +23,18 @@ module Rpc
     # @param [Hash] pod
     def set_state(id, pod)
       node = @grid.host_nodes.find_by(node_id: id)
-      return { error: 'Node not found' } unless node
+      raise 'Node not found' unless node
 
       service_instance = node.grid_service_instances.find_by(
         grid_service_id: pod['service_id'], instance_number: pod['instance_number']
       )
-      if service_instance
-        service_instance.set(
-          state: pod['state'],
-          rev: pod['rev']
-        )
-        {}
-      else
-        { error: 'Instance not found' }
-      end
+      raise 'Instance not found' unless service_instance
+
+      service_instance.set(
+        state: pod['state'],
+        rev: pod['rev']
+      )
+      {}
     end
 
     # @return [Boolean]
