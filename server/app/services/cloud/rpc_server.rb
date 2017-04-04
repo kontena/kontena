4adb9f1eb12540d034c3e367144ed53856e47e82
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-04-04 18:56:55 +0300

    Fix nil backtraces in rpc errors (#1998)
    * Avoid setting nil backtraces in rpc errors
    
    * More exception finetuning
    
    * Replace backtrace overwriting with remote_backtrace
    
    * Append the remote backtrace to local backtrace
    
    * Indent fail
    
    * Remove artifacts
    
    * Adapt spec
    
    * The RpcServer::Error are only used locally, and do not need any remote_backtrace
    
    * Set backtrace from caller when backtrace.nil
    
    * Override accessor instead of messing with the content
    
    * Capture caller in initializer
    
    * Raise Exception.new never sets a nil backtrace
    
    * Rebase brought the changes back
    
    * Handle nil from super
    
    * cloud rpc server does not have remote backtraces
    
    * agent: server does not return any remote backtraces
    
    * fix RpcClient::Error#backtrace to return nil if no local backtrace is set yet
    
    * fix rpc_client specs
    
    * improve and spec backtrace formatting
    
    * comment

--- server/app/services/cloud/rpc_server.rb
+++ server/app/services/cloud/rpc_server.rb
@@ -7,12 +7,11 @@ module Cloud
     }
 
     class Error < StandardError
-      attr_accessor :code, :message, :backtrace
+      attr_reader :code
 
-      def initialize(code, message, backtrace = nil)
-        self.code = code
-        self.message = message
-        self.backtrace = backtrace
+      def initialize(code, message)
+        @code = code
+        super(message)
       end
     end
 
