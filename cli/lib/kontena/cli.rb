35e649bf744b97f3e53ffb0b1e2854a005695bf1
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-06 15:50:04 +0300

    Kontena::Cli should be in lib/kontena/cli.rb, Kontena should be in lib/kontena.rb

---
+++ cli/lib/kontena/cli.rb
@@ -0,0 +1,22 @@
+module Kontena
+  module Cli
+    autoload :Helpers, 'kontena/cli/helpers'
+    autoload :Common, 'kontena/cli/common'
+    autoload :Spinner, 'kontena/cli/spinner'
+    autoload :GridOptions, 'kontena/cli/grid_options'
+    autoload :Plugins, 'kontena/cli/plugins'
+    autoload :Grids, 'kontena/cli/grids'
+    autoload :Stacks, 'kontena/cli/stacks'
+    autoload :Volumes, 'kontena/cli/volumes'
+    autoload :Nodes, 'kontena/cli/nodes'
+    autoload :Apps, 'kontena/cli/apps'
+    autoload :Etcd, 'kontena/cli/etcd'
+    autoload :Containers, 'kontena/cli/containers'
+    autoload :Registry, 'kontena/cli/registry'
+    autoload :ExternalRegistries, 'kontena/cli/external_registries'
+    autoload :Vpn, 'kontena/cli/vpn'
+    autoload :Certificate, 'kontena/cli/certificate'
+    autoload :Cloud, 'kontena/cli/cloud'
+    autoload :Vault, 'kontena/cli/vault'
+  end
+end
