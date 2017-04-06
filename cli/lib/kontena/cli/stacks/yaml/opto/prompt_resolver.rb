42bbfa37b0a432f2cd28858984f81361f92d5282
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-06 13:07:37 +0300

    How about this?

--- cli/lib/kontena/cli/stacks/yaml/opto/prompt_resolver.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/prompt_resolver.rb
@@ -50,7 +50,7 @@ module Kontena::Cli::Stacks
       end
 
       def bool
-        prompt.yes?(question_text, default: option.default == false ? false : true)
+        prompt.yes?(question_text, default: option.default.nil? || option.default)
       end
 
       def echo?
