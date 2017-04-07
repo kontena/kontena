b53e71204abf11ebb153f53850405bc755ebf6d4
Tero Marttila <tero.marttila@kontena.io>
2017-04-07 14:48:06 +0300

    kontena 1.2

--- cli/lib/kontena/cli/volume_command.rb
+++ cli/lib/kontena/cli/volume_command.rb
@@ -1,7 +1,7 @@
 
 class Kontena::Cli::VolumeCommand < Kontena::Command
 
-  warn Kontena.pastel.yellow('[EXPERIMENTAL] The `kontena volume` commands are still experimental in Kontena 1.1, and may change in future releases')
+  warn Kontena.pastel.yellow('[EXPERIMENTAL] The `kontena volume` commands are still experimental in Kontena 1.2, and may change in future releases')
 
   subcommand "create", "Create a managed volume", load_subcommand('volumes/create_command')
   subcommand "show", "Show details of a volume", load_subcommand('volumes/show_command')
