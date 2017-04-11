c67c9f6266753935d34bead62fe47cf1290d7187
Tero Marttila <tero.marttila@kontena.io>
2017-04-11 10:29:45 +0300

    fix NodeServicePodHandler mgiration version check for misordered migrations

--- server/app/services/rpc/node_service_pod_handler.rb
+++ server/app/services/rpc/node_service_pod_handler.rb
@@ -64,7 +64,7 @@ module Rpc
     # @return [Boolean]
     def migration_done?
       if @migration_done.nil?
-        last = SchemaMigration.last
+        last = SchemaMigration.desc(:version).first
         if last
           @migration_done = last.version >= 20
         else
