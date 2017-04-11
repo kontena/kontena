29fafe3e6446dd390427a86db3facc6f71f655ae
Tero Marttila <tero.marttila@kontena.io>
2017-04-11 16:51:48 +0300

    tweak time intervals

--- server/app/middlewares/websocket_backend.rb
+++ server/app/middlewares/websocket_backend.rb
@@ -5,9 +5,9 @@ require_relative '../services/agent/node_plugger'
 require_relative '../services/agent/node_unplugger'
 
 class WebsocketBackend
-  WATCHDOG_INTERVAL = 0.5 # seconds
-  WATCHDOG_THRESHOLD = 1.0 # seconds
-  WATCHDOG_TIMEOUT = 10.0 # seconds # XXX: testing
+  WATCHDOG_INTERVAL = 0.5.seconds
+  WATCHDOG_THRESHOLD = 1.0.seconds
+  WATCHDOG_TIMEOUT = 60.0.seconds # XXX: testing
 
   KEEPALIVE_TIME = 30 # in seconds
   RPC_MSG_TYPES = %w(request notify)
