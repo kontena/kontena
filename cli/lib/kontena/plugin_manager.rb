23e201d6924e7a0960c328fda6c13f9ac68de02b
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-04-20 15:06:41 +0300

    Warn, don't exit, when a plugin fails to load (#2184)
    * Warn, don't exit, when a plugin fails to load
    
    * Only rescue LoadError and StandardError
    
    * Dont load plugins when running plugin commands
    
    * Only consider the first non option argument
    
    * Add hint about uninstalling offending plugins
    
    * Rescue ScriptError instead of LoadError
    
    * Remove the conditional plugin loading
    
    * Dont tell how to uninstall because it wont work

--- cli/lib/kontena/plugin_manager.rb
+++ cli/lib/kontena/plugin_manager.rb
@@ -167,6 +167,7 @@ module Kontena
           if File.exist?(plugin) && !plugins.find{ |p| p.name == spec.name }
             begin
               if spec_has_valid_dependency?(spec)
+                ENV["DEBUG"] && $stderr.puts("Loading plugin #{spec.name}")
                 load(plugin)
                 plugins << spec
               else
@@ -174,10 +175,9 @@ module Kontena
                 $stderr.puts " [#{Kontena.pastel.red('error')}] Plugin #{Kontena.pastel.cyan(plugin_name)} (#{spec.version}) is not compatible with the current cli version."
                 $stderr.puts "         To update the plugin, run 'kontena plugin install #{plugin_name}'"
               end
-            rescue LoadError => ex
-              $stderr.puts " [#{Kontena.pastel.red('error')}] Failed to load plugin: #{spec.name}"
+            rescue ScriptError, StandardError => ex
+              warn " [#{Kontena.pastel.red('error')}] Failed to load plugin: #{spec.name}\n\tRerun the command with environment DEBUG=true set to get the full exception."
               ENV['DEBUG'] && $stderr.puts("#{ex.class.name} : #{ex.message}\n#{ex.backtrace.join("\n  ")}")
-              exit 1
             end
           end
         end
