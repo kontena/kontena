fc6b883af8b56921d818a39bbad171098bf8dfc0
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-20 11:02:01 +0300

    Rescue ScriptError instead of LoadError

--- cli/lib/kontena/plugin_manager.rb
+++ cli/lib/kontena/plugin_manager.rb
@@ -175,7 +175,7 @@ module Kontena
                 $stderr.puts " [#{Kontena.pastel.red('error')}] Plugin #{Kontena.pastel.cyan(plugin_name)} (#{spec.version}) is not compatible with the current cli version."
                 $stderr.puts "         To update the plugin, run 'kontena plugin install #{plugin_name}'"
               end
-            rescue LoadError, StandardError => ex
+            rescue ScriptError, StandardError => ex
               warn " [#{Kontena.pastel.red('error')}] Failed to load plugin: #{spec.name}\n\tRerun the command with environment DEBUG=true set to get the full exception.\nYou can uninstall the plugin using: kontena plugin uninstall #{spec.name.sub('kontena-plugin-', '')}\n"
               ENV['DEBUG'] && $stderr.puts("#{ex.class.name} : #{ex.message}\n#{ex.backtrace.join("\n  ")}")
             end
