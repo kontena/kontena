13790828044e1f7227b793c503d91a1322892835
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-21 11:02:41 +0300

    Use StackDeployMonitor in stack deploy

--- cli/lib/kontena/cli/stacks/deploy_command.rb
+++ cli/lib/kontena/cli/stacks/deploy_command.rb
@@ -1,4 +1,6 @@
 require_relative 'stacks_helper'
+require_relative 'stack_deploy_monitor'
+require 'tty-cursor'
 
 module Kontena::Cli::Stacks
   class DeployCommand < Kontena::Command
@@ -9,6 +11,10 @@ module Kontena::Cli::Stacks
     banner "Deploys all services of a stack that has been installed in a grid on Kontena Master"
 
     parameter "NAME", "Stack name"
+
+    option '--max-wait', '[SECONDS]', 'Monitor progress for maximum SECONDS seconds', default: 600 do |sec|
+      Integer(sec)
+    end
 
     requires_current_master
     requires_current_master_token
@@ -21,7 +27,41 @@ module Kontena::Cli::Stacks
       spinner "Waiting for deployment to start" do
         wait_for_deployment_to_start(deployment)
       end
-      wait_for_deploy_to_finish(deployment)
+
+      monitor = StackDeployMonitor.new(name, deployment['stack_id'], deployment['id'])
+
+      monitor.make_room
+
+      until monitor.finished? || monitor.time_elapsed > max_wait
+        monitor.advance
+        sleep 0.2 unless monitor.finished?
+      end
+      monitor.advance # once again to draw the 100%
+      puts unless monitor.finished?
+
+      if !monitor.finished? && monitor.time_elapsed > max_wait
+        puts "Deployment has been running over #{max_wait} seconds, will continue in background."
+        exit 0
+      end
+
+      if monitor.failed?
+        $stderr.puts pastel.red("ERROR: #{monitor.throttled['error']}") if monitor.throttled['error']
+        monitor.errored_services.each do |svc|
+          $stderr.puts pastel.red("Service #{svc['service_id']} failed: #{svc['error']}")
+          svc['instance_deploys'].select { |svc| svc['state'] == 'error' }.group_by { |svc| svc['node'] }.each do |node, instances|
+            $stderr.puts "Instance deploy errors on node #{node}:"
+            instances.select { |ins| ins['state'] == 'error' }.each do |instance|
+              $stderr.puts " - #{instance['error']}"
+            end
+          end
+        end
+        exit 1
+      else
+        puts pastel.green("Finished!")
+        puts "Stack #{pastel.cyan(name)} deployed."
+      end
+    ensure
+      print TTY::Cursor.show
     end
 
     def deploy_stack(name)
