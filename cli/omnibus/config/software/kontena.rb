ac005119e3e6b768b1ffa220fafb768163fc8369
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-24 11:42:24 +0300

    Add rb-readline to CLI installer

--- cli/omnibus/config/software/kontena.rb
+++ cli/omnibus/config/software/kontena.rb
@@ -5,8 +5,10 @@ dependency "ruby"
 dependency "rubygems"
 dependency "libxml2"
 dependency "libxslt"
+
 whitelist_file "./wrappers/sh/kontena"
 build do
+  gem "install rb-readline -v 0.5.4 --no-ri --no-doc"
   gem "install nokogiri -v 1.6.8 --no-ri --no-doc"
   gem "install kontena-cli -v #{default_version} --no-ri --no-doc"
   copy "sh/kontena", "#{install_dir}/bin/kontena"
