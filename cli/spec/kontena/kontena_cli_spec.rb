af03b1f1b69bcecbc3d62a99e49a6fbb028ea373
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-24 14:57:26 +0300

    Such specsy much wow

--- cli/spec/kontena/kontena_cli_spec.rb
+++ cli/spec/kontena/kontena_cli_spec.rb
@@ -29,19 +29,41 @@ describe Kontena do
 
     before(:each) do
       expect(Kontena::Cli::WhoamiCommand).to receive(:new).and_return(whoami)
-      expect(whoami).to receive(:run).with(['--bash-completion-path']).and_return(true)
     end
 
     it 'accepts a command line as string' do
+      expect(whoami).to receive(:run).with(['--bash-completion-path']).and_return(true)
       Kontena.run('whoami --bash-completion-path')
     end
 
     it 'accepts a command line as a list of parameters' do
+      expect(whoami).to receive(:run).with(['--bash-completion-path']).and_return(true)
       Kontena.run('whoami', '--bash-completion-path')
     end
 
     it 'accepts a command line as an array' do
+      expect(whoami).to receive(:run).with(['--bash-completion-path']).and_return(true)
       Kontena.run(['whoami', '--bash-completion-path'])
+    end
+
+    it 'Returns the exit status when running with returning: :status when status is 1' do
+      expect(whoami).to receive(:run) { exit 1 }
+      expect(Kontena.run(['whoami'], returning: :status)).to eq 1
+    end
+
+    it 'Returns the exit status when running with returning: :status when status is 0' do
+      expect(whoami).to receive(:run) { exit 0 }
+      expect(Kontena.run(['whoami'], returning: :status)).to be_zero
+    end
+
+    it 'Re-raises the SystemExit without returning: :status when exiting with non-zero status' do
+      expect(whoami).to receive(:run) { exit 1 }
+      expect{Kontena.run(['whoami'])}.to exit_with_error.status(1)
+    end
+
+    it 'Continues as usual without returning: :status when exiting with zero status' do
+      expect(whoami).to receive(:run) { exit 0 }
+      expect{Kontena.run(['whoami'])}.not_to exit_with_error
     end
   end
 end
