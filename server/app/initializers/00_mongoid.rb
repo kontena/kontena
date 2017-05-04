8a9f399030238ac69963520df30d39ee5c719956
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-05-03 21:25:43 -0400

    allow to use mongodb versions >= 3.0

--- server/app/initializers/00_mongoid.rb
+++ server/app/initializers/00_mongoid.rb
@@ -5,7 +5,7 @@ Mongo::Logger.logger.level = Logger::WARN
 # Returns an array such as [3, 0, 12, 0]
 mongo_db_version = Mongoid.default_client.command(buildinfo: 1).documents.first["versionArray"]
 
-unless mongo_db_version[0] == 3 && mongo_db_version[1] == 0
+unless mongo_db_version[0] == 3 && mongo_db_version[1] >= 0
   abort "MongoDB version 3.0 is required for running Kontena Master. Your version #{mongo_db_version[0]}.#{mongo_db_version[1]}.#{mongo_db_version[2]} is incompatible."
 end
 
