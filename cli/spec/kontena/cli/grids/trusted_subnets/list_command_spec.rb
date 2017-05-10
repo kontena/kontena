425480d73b224e9caa927c117cf881a52ea63546
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-05-10 15:58:04 +0300

    Make trusted-subnet subcommands use the current_grid or --grid

--- cli/spec/kontena/cli/grids/trusted_subnets/list_command_spec.rb
+++ cli/spec/kontena/cli/grids/trusted_subnets/list_command_spec.rb
@@ -4,19 +4,24 @@ require "kontena/cli/grids/trusted_subnets/list_command"
 describe Kontena::Cli::Grids::TrustedSubnets::ListCommand do
 
   include ClientHelpers
+  include RequirementsHelper
+
+  expect_to_require_current_master
 
   describe '#execute' do
-    it 'requires grid as param' do
-      expect {
-        subject.run([])
-      }.to raise_error(Clamp::UsageError)
-    end
-
     it 'requests grid details from master' do
       expect(client).to receive(:get).with("grids/test-grid").and_return('trusted_subnets' => [
           '192.168.0.1/24',
       ])
-      expect{subject.run(['test-grid'])}.to output("192.168.0.1/24\n").to_stdout
+      expect{subject.run([])}.to output("192.168.0.1/24\n").to_stdout
+    end
+
+    it 'supports the --grid option' do
+      allow(subject).to receive(:current_grid).and_call_original
+      expect(client).to receive(:get).with("grids/ingrid").and_return('trusted_subnets' => [
+          '192.168.0.1/24',
+      ])
+      expect{subject.run(['--grid', 'ingrid'])}.to output("192.168.0.1/24\n").to_stdout
     end
   end
 end
