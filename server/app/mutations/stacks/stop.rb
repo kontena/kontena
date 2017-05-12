8dfc131a533c7d553352c06c1b9bc151aadf2af8
Jussi Nummelin <jussi@kontena.io>
2017-05-12 09:13:17 +0300

    use common error handling in stack stop&restart mutations

--- server/app/mutations/stacks/stop.rb
+++ server/app/mutations/stacks/stop.rb
@@ -9,7 +9,7 @@ module Stacks
       self.stack.grid_services.each do |service|
         outcome = GridServices::Stop.run(grid_service: service)
         unless outcome.success?
-          add_error(service.to_path, :stop_failed, outcome.errors.message)
+          handle_service_outcome_errors(service.name, outcome.errors)
         end
       end
     end
