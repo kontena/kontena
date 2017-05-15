6afa4a2635f813d1e9ae9466cbd03aa8523247e4
Tero Marttila <tero.marttila@kontena.io>
2017-05-15 16:22:51 +0300

    refactor __links_for_service to just return linked service name

--- server/app/mutations/stacks/sort_helper.rb
+++ server/app/mutations/stacks/sort_helper.rb
@@ -10,9 +10,9 @@ module Stacks
       services.sort{ |a, b|
         a_links = __links_for_service(a)
         b_links = __links_for_service(b)
-        if a_links.any?{ |l| l[:name] == b[:name] }
+        if a_links.any?{ |l| l == b[:name] }
           1
-        elsif b_links.any?{ |l| l[:name] == a[:name] }
+        elsif b_links.any?{ |l| l == a[:name] }
           -1
         else
           a_links.size <=> b_links.size
@@ -22,13 +22,13 @@ module Stacks
 
     # Collect stack-local links for given service.
     #
-    # @param [Array<GridService,Hash>]
-    # @return [Array<Hash>]
+    # @param service [GridService, Hash] GridService model object, or API service hash with symbol keys
+    # @return [Array<String>] names of linked services
     def __links_for_service(service)
       if service.is_a?(GridService)
-        service.grid_service_links.select{|l| l.linked_grid_service.stack == service.stack }.map{ |l| { name: l.linked_grid_service.name } }
+        service.grid_service_links.select{|l| l.linked_grid_service.stack == service.stack }.map{ |l| l.linked_grid_service.name }
       else
-        (service[:links] || []).select{|l| !l[:name].include? '/'}
+        (service[:links] || []).select{|l| !l[:name].include? '/'}.map{|l| l[:name] }
       end
     end
   end
