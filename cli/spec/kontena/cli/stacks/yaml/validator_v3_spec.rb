2af1c356fee6a48598b89a4c584577bbdae8ecfa
Tero Marttila <SpComb@users.noreply.github.com>
2017-05-15 12:49:59 +0300

    Validate service envs on server, simplify syntax for parsing/validation (#2315)
    * simplify GridService#env_hash
    
    * simplify CLI stack YAML validator
    
    * tricky edge cases
    
    * spec and validate service env strings
    
    * simply regexp to just validate prefix
    
    * re-tweak to reject invalid prefixes
    
    * spec multi-line values

--- cli/spec/kontena/cli/stacks/yaml/validator_v3_spec.rb
+++ cli/spec/kontena/cli/stacks/yaml/validator_v3_spec.rb
@@ -108,17 +108,37 @@ describe Kontena::Cli::Stacks::YAML::ValidatorV3 do
       expect(result.errors.key?('environment')).to be_falsey
       result = subject.validate_options('environment' => { 'KEY' => 'VALUE' })
       expect(result.errors.key?('environment')).to be_falsey
+      result = subject.validate_options('environment' => ['KEY=VALUE', 'KEY2=VALUE2', 'KEY3='])
+      expect(result.errors.key?('environment')).to be_falsey
     end
 
     it 'fails validation if environment array includes items without equals sign' do
-      result = subject.validate_options('environment' => ['KEY=VALUE', 'KEY2=VALUE2'])
-      expect(result.errors.key?('environment')).to be_falsey
       result = subject.validate_options('environment' => ['KEY=VALUE', 'KEY2 VALUE'])
+      expect(result.errors.key?('environment')).to be_truthy
+    end
+
+    it "fails validation if environment has invalid key prefix" do
+      result = subject.validate_options('environment' => ['=VALUE'])
+      expect(result.errors.key?('environment')).to be_truthy
+    end
+
+    it "fails validation if environemnt has invalid key prefix with valud key prefix in value" do
+      result = subject.validate_options('environment' => ['=VALUE=VALUE2'])
+      expect(result.errors.key?('environment')).to be_truthy
+    end
+
+    it "fails validation if environment contains invalid key prefix with valid key prefix in multi-line value" do
+      result = subject.validate_options('environment' => ['=ASDF\nKEY=VALUE'])
       expect(result.errors.key?('environment')).to be_truthy
     end
 
     it 'passes validation if environment array includes items with booleans or nils' do
       result = subject.validate_options('environment' => { 'KEY' => true, 'KEY2' => false, 'KEY3' => nil })
+      expect(result.errors.key?('environment')).to be_falsey
+    end
+
+    it 'passes validation if environment array includes items with multi-line values' do
+      result = subject.validate_options('environment' => [ "KEY=foo\nbar" ])
       expect(result.errors.key?('environment')).to be_falsey
     end
 
