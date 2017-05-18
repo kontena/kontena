75ded76e9d80e80b0dc60a741b28f7996c8669be
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-05-18 14:08:24 +0300

    # This is a combination of 12 commits. # This is the 1st commit message: Testing if we could run tests for only changed components
    # This is the commit message #2:
    
    Script syntax fail
    
    # This is the commit message #3:
    
    More tweakery
    
    # This is the commit message #4:
    
    y u no work
    
    # This is the commit message #5:
    
    Now?
    
    # This is the commit message #6:
    
    More debugs
    
    # This is the commit message #7:
    
    Bash conditions are hard
    
    # This is the commit message #8:
    
    Perhaps like this
    
    # This is the commit message #9:
    
    There is TRAVIS_COMMIT_RANGE?
    
    # This is the commit message #10:
    
    Debugs
    
    # This is the commit message #11:
    
    Even more
    
    # This is the commit message #12:
    
    Bash conditions are hard.

--- build/travis/test.sh
+++ build/travis/test.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 set -ue
 
-cd $TEST_DIR && \
-  bundle install --path vendor/bundle && \
-  bundle exec rspec spec/
+CURR_BRANCH=${CURR_BRANCH:-$TRAVIS_BRANCH}
+echo "Current branch: '$CURR_BRANCH' Tag: '$TRAVIS_TAG' Commit range: '$TRAVIS_COMMIT_RANGE'"
+echo "Changed files:"
+git diff --name-only $TRAVIS_COMMIT_RANGE
+
+if [ "$CURR_BRANCH" != "master" ] && [ "$TRAVIS_TAG" == "" ]
+then
+  if [ "$TEST_DIR" == "cli" ]
+  then
+    if git diff --name-only $TRAVIS_COMMIT_RANGE | grep -Eqe "^cli/"
+    then
+      echo "Skipping $TEST_DIR test because there are no changes to $TEST_DIR in branch $CURR_BRANCH"
+      exit 0
+    fi
+  fi
+
+  if [ "$TEST_DIR" == "agent" ]
+  then
+    if git diff --name-only $TRAVIS_COMMIT_RANGE | grep -Eqe "^agent/"
+      echo "Skipping $TEST_DIR test because there are no changes to $TEST_DIR in branch $CURR_BRANCH"
+      exit 0
+    fi
+  fi
+
+  if [ "$TEST_DIR" == "server" ]
+  then
+    if git diff --name-only $TRAVIS_COMMIT_RANGE | grep -Eqe "^server/")
+    then
+      echo "Skipping $TEST_DIR test because there are no changes to $TEST_DIR in branch $CURR_BRANCH"
+      exit 0
+    fi
+  fi
+fi
+
+cd $TEST_DIR && bundle install --path vendor/bundle && bundle exec rspec spec/
