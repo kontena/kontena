0f9a208ab587e64d37db78051a78db8d9f14972d
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-05-19 13:32:17 +0300

    Revert that. ls/add/rm should perhaps in the future be used for managing multiple stack repositories

--- cli/lib/kontena/cli/stacks/registry_command.rb
+++ cli/lib/kontena/cli/stacks/registry_command.rb
@@ -1,9 +1,8 @@
 module Kontena::Cli::Stacks
   class RegistryCommand < Kontena::Command
-
     subcommand "push", "Push a stack into the stacks registry", load_subcommand('stacks/registry/push_command')
     subcommand "pull", "Pull a stack from the stacks registry", load_subcommand('stacks/registry/pull_command')
-    subcommand ["search", "list", "ls"], "Search for stacks in the stacks registry", load_subcommand('stacks/registry/search_command')
+    subcommand ["search"], "Search for stacks in the stacks registry", load_subcommand('stacks/registry/search_command')
     subcommand "show", "Show info about a stack in the stacks registry", load_subcommand('stacks/registry/show_command')
     subcommand ["remove", "rm"], "Remove a stack (or version) from the stacks registry", load_subcommand('stacks/registry/remove_command')
   end
