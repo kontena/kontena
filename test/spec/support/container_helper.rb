2d5ef09ad1a7192f6d5f459547f72dfb66c33644
Jussi Nummelin <jussi@kontena.io>
2017-05-26 13:54:57 +0300

    fix flaky vpn E2E spec

---
+++ test/spec/support/container_helper.rb
@@ -0,0 +1,19 @@
+module ContainerHelper
+
+  # Checks if a deployed container exists with given name
+  # @param [String] name of the container to check
+  def container_exist?(name)
+    k = run "kontena container ls"
+    k.out.include?(name)
+  end
+
+  # Waits until no container exists with the name
+  # @param [String] name of the container to check
+  def wait_until_container_gone(name)
+    loop do
+      return unless container_exist?(name)
+      sleep 1
+    end
+  end
+
+end
