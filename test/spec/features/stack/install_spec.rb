49a02a63bce3836fab7bb73a0cafe4a3451c213a
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-05-27 14:12:35 +0300

    add e2e spec for stack with stop_grace_period

--- test/spec/features/stack/install_spec.rb
+++ test/spec/features/stack/install_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe 'stack install' do
+  
+  after(:each) do
+    run 'kontena stack rm --force simple'
+  end
 
   context 'from file' do
-    after(:each) do
-      run 'kontena stack rm --force simple'
-    end
 
     it 'installs a stack' do
       with_fixture_dir("stack/simple") do
@@ -50,6 +51,17 @@ describe 'stack install' do
         expect(k.code).to eq(1)
         expect(k.out).to match /services:\s*a:\s*links: Linked service 'nope' does not exist/m
       end
+    end
+  end
+
+  context 'For a stack with stop_grace_period' do
+    it 'creates stack service with stop_grace_period' do
+      with_fixture_dir("stack/simple") do
+        run 'kontena stack install stop-period.yml'
+      end
+      k = run 'kontena service show simple/redis'
+      expect(k.code).to eq(0)
+      expect(k.out.match(/stop_grace_period: 23s/)).to be_truthy
     end
   end
 end
