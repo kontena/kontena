1b6f0746a2d00cc8ab3d3c7ff2d203b81a95f697
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-05-30 12:31:08 +0300

    Add a require for the command for good measure

--- cli/lib/kontena/cli/stacks/yaml/opto/service_instances_resolver.rb
+++ cli/lib/kontena/cli/stacks/yaml/opto/service_instances_resolver.rb
@@ -5,6 +5,7 @@ module Kontena::Cli::Stacks
 
       def resolve
         return nil unless current_master && current_grid
+        require 'kontena/cli/stacks/show_command'
         read_command = Kontena::Cli::Stacks::ShowCommand.new([self.stack])
         stack = read_command.fetch_stack(self.stack)
         service = stack['services'].find { |s| s['name'] == hint }
