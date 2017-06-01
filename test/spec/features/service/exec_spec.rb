de8613bc06f3b2dbc67c30ac150c34e89108ae50
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-06-01 16:10:30 +0300

    initial service exec e2e tests

---
+++ test/spec/features/service/exec_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'service exec' do
+
+  before(:all) do
+    run("kontena service create test-1 redis:3.0")
+    run("kontena service deploy test-1")
+  end
+
+  after(:all) do
+    run("kontena service rm --force test-1")
+  end
+
+  it 'runs a command inside a service' do
+    k = kommando("kontena service exec test-1 hostname -s")
+    expect(k.run).to be_truthy
+    expect(k.out).to eq("test-1-1\r\n")
+  end
+
+  it 'returns an error if command not found' do
+    k = kommando("kontena service exec test-1 thisdoesnotexist")
+    k.run
+    expect(k.code).to eq(1)
+  end
+
+  it 'runs a command inside a service on a given instances' do
+    run("kontena service scale test-1 2")
+    k = kommando("kontena service exec --instance 2 test-1 hostname -s")
+    expect(k.run).to be_truthy
+    expect(k.out).to eq("test-1-2\r\n")
+    run("kontena service scale test-1 1")
+  end
+
+  it 'runs a command inside a service with tty' do
+    k = kommando("kontena service exec --interactive test-1 sh")
+    
+    k.out.on("#") do
+      k.in << "ls -la /\r"
+      k.out.on "lib64" do 
+        k.in << "exit\r"
+      end
+    end
+    expect(k.run).to be_truthy
+  end
+end
