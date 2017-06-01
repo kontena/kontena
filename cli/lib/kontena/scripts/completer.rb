0118bb49a81b6107415c6fead8196f4d6d104af0
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-06-01 11:17:56 +0300

    Master user completion was expecting master userS

--- cli/lib/kontena/scripts/completer.rb
+++ cli/lib/kontena/scripts/completer.rb
@@ -157,10 +157,10 @@ begin
         end
       when 'master'
         completion.clear
-        sub_commands = %w(list use users current remove rm config cfg login logout token join audit-log init-cloud)
+        sub_commands = %w(list use user current remove rm config cfg login logout token join audit-log init-cloud)
         if words[1] && words[1] == 'use'
           completion.push helper.master_names
-        elsif words[1] && words[1] == 'users'
+        elsif words[1] && words[1] == 'user'
           users_sub_commands = %(invite list role)
           completion.push users_sub_commands
         elsif words[1] && ['config', 'cfg'].include?(words[1])
