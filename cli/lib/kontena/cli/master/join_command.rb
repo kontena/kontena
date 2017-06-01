895a26e1eab0f20b65fa3dca9f1288857f685f16
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-06-01 10:03:36 +0300

    remove unnecessary shellescaping

--- cli/lib/kontena/cli/master/join_command.rb
+++ cli/lib/kontena/cli/master/join_command.rb
@@ -9,7 +9,7 @@ module Kontena::Cli::Master
 
     def execute
       params = []
-      params += ["--join", self.invite_code.shellescape]
+      params += ["--join", self.invite_code]
       params << "--remote" if self.remote?
       params += ["--name", self.name.shellescape] if self.name
       params << "--verbose" if self.verbose?
