d5e1647fec1053af8f745015fe6b3bc818fd64a7
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-06-01 12:14:43 +0300

    Dont try to run the warnings

--- test/spec/features/cli_spec.rb
+++ test/spec/features/cli_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'cli' do
   it 'can run --help on all subcommands' do
-    run('kontena complete --subcommand-tree').split(/[\r\n]/).each do |command|
+    run('kontena complete --subcommand-tree').out.split(/[\r\n]/).reject { |cmd| cmd.empty? || cmd =~ /\[.+?\]/ }.each do |command|
       k = run(command + ' --help')
       expect(k.code).to eq(0)
       expect(k.out).to match(/Usage:/)
