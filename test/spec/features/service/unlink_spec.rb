56d559d82b99f0ccad30e6ab5e5b0fc350b81b4f
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-06-06 21:45:02 +0300

    more e2e testing for links

--- test/spec/features/service/unlink_spec.rb
+++ test/spec/features/service/unlink_spec.rb
@@ -5,7 +5,7 @@ describe 'service unlink' do
     end
   end
 
-  it 'links service to target' do
+  it 'unlinks service to target' do
     run "kontena service create test-1 redis:3.0"
     run "kontena service create test-2 redis:3.0"
     run "kontena service link test-1 test-2"
@@ -13,6 +13,19 @@ describe 'service unlink' do
     expect(k.code).to eq(0)
     k = run "kontena service show test-1"
     expect(k.out.match(/^\s+- test-2\s*$/)).to be_falsey
+  end
+
+  it 'unlinks service from stack with existing links' do
+    with_fixture_dir("stack/links") do
+      run 'kontena stack install --no-deploy links.yml'
+    end
+    run "kontena service create test-1 redis:3.0"
+    k = run "kontena service link simple/bar test-1"
+    expect(k.code).to eq(0)
+    k = run "kontena service show simple/bar"
+    expect(k.out.match(/^\s+\- test-1\s*$/)).to be_truthy
+    k = run "kontena service unlink simple/bar test-1"
+    expect(k.out.match(/^\s+\- test-1\s*$/)).to be_falsey
   end
 
   it 'returns error if target does not exist' do
