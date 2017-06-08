d9178e3e313fe35bdb4fc4094bbfd1f357df7dc5
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-06-08 12:01:41 +0300

    Fix Kontena.run argument syntax in callback

--- cli/lib/kontena/callbacks/master/deploy/60_configure_auth_provider_after_deploy.rb
+++ cli/lib/kontena/callbacks/master/deploy/60_configure_auth_provider_after_deploy.rb
@@ -7,9 +7,9 @@ module Kontena
       def init_cloud_args
         args = []
         args << '--force'
-        args << "--cloud-master-id #{command.cloud_master_id}" if command.cloud_master_id
-        args << "--provider #{command.result[:provider]}" if command.result[:provider]
-        args << "--version #{command.result[:version]}" if command.result[:version]
+        args += ["--cloud-master-id", command.cloud_master_id] if command.cloud_master_id
+        args += ["--provider", command.result[:provider]] if command.result[:provider]
+        args << ["--version", command.result[:version]] if command.result[:version]
         args
       end
 
