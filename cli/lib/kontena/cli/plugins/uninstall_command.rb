277eca4c8ef19f5f211ece6306c4837e73ad7f66
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-06-13 14:40:41 +0300

    Restore --force to uninstall

--- cli/lib/kontena/cli/plugins/uninstall_command.rb
+++ cli/lib/kontena/cli/plugins/uninstall_command.rb
@@ -7,7 +7,11 @@ module Kontena::Cli::Plugins
 
     parameter 'NAME', 'Plugin name'
 
+    option "--force", :flag, "Force remove", default: false, attribute_name: :forced
+
     def execute
+      exit_with_error "Plugin #{pastel.cyan(name)} is not installed" unless Kontena::PluginManager.instance.installed(name) && !ENV['NO_PLUGINS']
+      confirm unless forced?
       spinner "Uninstalling plugin #{pastel.cyan(name)}" do |spin|
         begin
           Kontena::PluginManager.instance.uninstall_plugin(name)
