d7a8e01822733c93724f340c27c0d1bd5080449f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-06-16 14:45:13 -0400

    truncate vault keys

--- server/app/initializers/01_symmetric_encryption.rb
+++ server/app/initializers/01_symmetric_encryption.rb
@@ -3,8 +3,8 @@ require 'symmetric_encryption/extensions/mongoid/encrypted'
 
 if ENV['VAULT_KEY'] && ENV['VAULT_IV']
   SymmetricEncryption.cipher = SymmetricEncryption::Cipher.new(
-      key:         ENV['VAULT_KEY'],
-      iv:          ENV['VAULT_IV'],
+      key:         ENV['VAULT_KEY'][0..32],
+      iv:          ENV['VAULT_IV'][0..16],
       cipher_name: 'aes-256-cbc'
   )
 end
