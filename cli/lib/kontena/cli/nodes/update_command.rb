cf47883dd68f7da1d9af66f778e49ece2ba255bd
Tero Marttila <tero.marttila@kontena.io>
2017-07-14 09:06:14 +0300

    cli node update: prompt/--force on token update; --no-reset-connection

--- cli/lib/kontena/cli/nodes/update_command.rb
+++ cli/lib/kontena/cli/nodes/update_command.rb
@@ -11,13 +11,19 @@ module Kontena::Cli::Nodes
 
     option ["-l", "--label"], "LABEL", "Node label", multivalued: true
     option "--clear-labels", :flag, "Clear node labels"
+
     option ["--token"], "TOKEN", "Node token"
     option "--generate-token", :flag, "Generate new node token"
+    option "--[no-]reset-connection", :flag, "Reset agent websocket connection", default: true
+    option "--force", :flag, "Force token update"
 
     def update_token
+      confirm("Updating the node token will require you to reconfigure the kontena-agent before it will be able to reconnect. Are you sure?")
+
       data = {}
 
       data[:token] = self.token if self.token
+      data[:reset_connection] = self.reset_connection?
 
       spinner "Updating node #{self.node.colorize(:cyan)} token" do
         client.put("nodes/#{current_grid}/#{self.node}/token", data)
