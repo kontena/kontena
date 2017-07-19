877121fcd8be7cb4e56880af549148d7ea0830fb
Tero Marttila <tero.marttila@kontena.io>
2017-07-19 17:52:34 +0300

    rewrite cli exec helper using Kontena::Websocket::Client

--- cli/lib/kontena/cli/helpers/exec_helper.rb
+++ cli/lib/kontena/cli/helpers/exec_helper.rb
@@ -1,87 +1,119 @@
-require_relative '../../websocket/client'
+require 'io/console'
+require 'kontena-websocket-client'
 
 module Kontena::Cli::Helpers
   module ExecHelper
 
-    # @param [WebSocket::Client::Simple] ws
+    # @param ws [Kontena::Websocket::Client]
+    # @param tty [Boolean] read stdin in raw mode, sending tty escapes for remote pty
+    # @raise [ArgumentError] not a tty
+    # @yield [data]
+    # @yieldparam data [String, nil] nil on EOF
+    # XXX: errors
     # @return [Thread]
-    def stream_stdin_to_ws(ws, tty: nil)
-      require 'io/console'
+    def websocket_exec_write_thread(ws, tty: nil)
+      raise ArgumentError, "the input device is not a TTY" if tty && !STDIN.tty?
+
       Thread.new {
         if tty
           STDIN.raw {
-            while char = STDIN.readpartial(1024)
-              ws.text(JSON.dump({ stdin: char }))
+            # XXX: raise EOF?
+            while data = STDIN.readpartial(1024)
+              ws.send(JSON.dump({ stdin: data }))
             end
+            ws.send(JSON.dump({ stdin: nil }))
           }
         else
-          while char = STDIN.gets
-            ws.text(JSON.dump({ stdin: char }))
+          while line = STDIN.gets
+            ws.send(JSON.dump({ stdin: line }))
           end
-          ws.text(JSON.dump({ stdin: nil }))
+          ws.send(JSON.dump({ stdin: nil }))
         end
       }
     end
 
-    # @param [Hash] msg
-    def handle_message(msg)
-      if msg.has_key?('exit')
-        if msg['message']
-          exit_with_error(msg['message'])
-        else
-          exit msg['exit'].to_i
-        end
-      elsif msg.has_key?('stream')
-        if msg['stream'] == 'stdout'
-          $stdout << msg['chunk']
-        else
-          $stderr << msg['chunk']
-        end
-      end
-    end
-
-    # @param [Websocket::Frame::Incoming] msg
-    def parse_message(msg)
-      JSON.parse(msg.data)
-    rescue JSON::ParserError
-      nil
-    end
-
-    # @param container_id [String] The container id
-    # @param interactive [Boolean] Interactive TTY on/off
-    # @param shell [Boolean] Shell on/of
-    # @param tty [Boolean] TTY on/of
     # @return [String]
-    def ws_url(container_id, interactive: false, shell: false, tty: false)
-      require 'uri' unless Object.const_defined?(:URI)
-      extend Kontena::Cli::Common unless self.respond_to?(:require_current_master)
-
+    def websocket_url(path, query)
       url = URI.parse(require_current_master.url)
       url.scheme = url.scheme.sub('http', 'ws')
-      url.path = "/v1/containers/#{container_id}/exec"
-      if shell || interactive || tty
-        query = {}
-        query.merge!(interactive: true) if interactive
-        query.merge!(shell: true) if shell
-        query.merge!(tty: true) if tty
-        url.query = URI.encode_www_form(query)
-      end
+      url.path = '/v1/' + path
+      url.query = URI.encode_www_form(query)
       url.to_s
     end
 
-    # @param [String] url
-    # @param [String] token
-    # @return [WebSocket::Client::Simple]
-    def connect(url, token)
+    # @param ws [Kontena::Websocket::Client]
+    # @return [Integer] exit code
+    def websocket_exec_read(ws)
+      ws.read do |msg|
+        msg = JSON.parse(msg)
+
+        if msg.has_key?('exit')
+          # breaks the read loop
+          return msg['exit'].to_i
+        elsif msg.has_key?('stream')
+          if msg['stream'] == 'stdout'
+            $stdout << msg['chunk']
+          else
+            $stderr << msg['chunk']
+          end
+        end
+      end
+    end
+
+    # Connect to server websocket, send from stdin, and write out messages
+    #
+    # @param paths [String]
+    # @param options [Hash] @see Kontena::Websocket::Client
+    # @param cmd [Array<String>] command to execute
+    # @param interactive [Boolean] Interactive TTY on/off
+    # @param shell [Boolean] Shell on/of
+    # @param tty [Boolean] TTY on/of
+    # @return [Integer] exit code
+    def websocket_exec(path, cmd, interactive: false, shell: false, tty: false)
+      write_thread = nil
+
+      query = {}
+      query[:interactive] = interactive if interactive
+      query[:shell] = shell if shell
+      query[:tty] = tty if tty
+
+      url = websocket_url(path, query)
+      token = require_token
       options = {
         headers: {
           'Authorization' => "Bearer #{token.access_token}"
-        }
+        },
+        ssl_params: {
+          verify_mode: ENV['SSL_IGNORE_ERRORS'].to_s == 'true' ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER,
+        },
       }
-      if ENV['SSL_IGNORE_ERRORS'].to_s == 'true'
-        options[:verify_mode] = ::OpenSSL::SSL::VERIFY_NONE
+
+      # we do not expect CloseError, because the server will send an 'exit' message first, which causes us to exit before seeing the close frame.
+      # TODO: handle HTTP 404 errors
+      Kontena::Websocket::Client.connect(url, **options) do |ws|
+        ws.send(JSON.dump({cmd: cmd}))
+
+        # start new thread to write from stdin to websocket
+        write_thread = websocket_exec_write_thread(ws, tty: tty)
+
+        # blocks reading from websocket, returns once exec exits
+        websocket_exec_read(ws)
       end
-      Kontena::Websocket::Client.new(url, options)
+      
+    rescue => exc
+      logger.error exc
+    ensure
+      write_thread.kill if write_thread
+    end
+
+    # Execute command on container using websocket API.
+    #
+    # @param grid [String] Grid ID
+    # @param id [String] Container ID (host/name)
+    # @param cmd [Array<String>] command to execute
+    # @return [Integer] exit code
+    def container_exec(grid, id, cmd, **exec_options)
+      websocket_exec("containers/#{grid}/#{id}/exec", cmd, **exec_options)
     end
   end
 end
