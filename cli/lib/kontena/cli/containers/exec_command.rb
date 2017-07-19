2bd08e1a1a9833a4e3c23cb241a02da47304452b
Tero Marttila <tero.marttila@kontena.io>
2017-07-19 17:52:53 +0300

    rewrite cli container exec command using container_exec()

--- cli/lib/kontena/cli/containers/exec_command.rb
+++ cli/lib/kontena/cli/containers/exec_command.rb
@@ -13,38 +13,15 @@ module Kontena::Cli::Containers
     option ["-i", "--interactive"], :flag, "Keep stdin open"
     option ["-t", "--tty"], :flag, "Allocate a pseudo-TTY"
 
-    def execute
-      exit_with_error "the input device is not a TTY" if tty? && !STDIN.tty?
+    requires_current_master
+    requires_current_grid
 
-      require_api_url
-      token = require_token
-      cmd = JSON.dump({cmd: cmd_list})
-      queue = Queue.new
-      stdin_reader = nil
-      url = ws_url("#{current_grid}/#{container_id}", interactive: interactive?, shell: shell?, tty: tty?)
-      ws = connect(url, token)
-      ws.on :message do |msg|
-        data = parse_message(msg)
-        queue << data if data.is_a?(Hash)
-      end
-      ws.on :open do
-        ws.text(cmd)
-        stdin_reader = self.stream_stdin_to_ws(ws, tty: self.tty?) if self.interactive?
-      end
-      ws.on :close do |e|
-        if e.reason.include?('code: 404')
-          queue << {'exit' => 1, 'message' => 'Not found'}
-        else
-          queue << {'exit' => 1}
-        end
-      end
-      ws.connect
-      while msg = queue.pop
-        self.handle_message(msg)
-      end
-    rescue SystemExit
-      stdin_reader.kill if stdin_reader
-      raise
+    def execute
+      Kontena::Websocket::Logging.initialize_logger(STDERR, Logger::DEBUG)
+
+      exit_status = container_exec(current_grid, self.container_id, interactive: interactive?, shell: shell?, tty: tty?)
+
+      exit exit_status unless exit_status.zero?
     end
   end
 end
