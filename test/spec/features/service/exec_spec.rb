ed344bfaa85b4104057d0773039be7e7716764da
Tero Marttila <tero.marttila@kontena.io>
2017-07-26 15:37:58 +0300

    e2e spec service exec error exits

--- test/spec/features/service/exec_spec.rb
+++ test/spec/features/service/exec_spec.rb
@@ -17,26 +17,31 @@ describe 'service exec' do
     expect(k.out).to eq("test-1-1\r\n")
   end
 
+  it 'exits with error if command fails' do
+    k = kommando("kontena service exec test-1 ls -l /nonexist")
+    k.run
+    expect(k.code).to_not eq 0
+    expect(k.out).to include("/nonexist: No such file or directory")
+  end
+
+  it 'exits with command error' do
+    k = kommando("kontena service exec --shell test-1 exit 32")
+    k.run
+    expect(k.code).to eq(32)
+  end
+
   it 'returns an error if command not found' do
     k = kommando("kontena service exec test-1 thisdoesnotexist")
     k.run
     expect(k.code).to eq(126)
   end
 
-  it 'runs a command inside a service on a given instances' do
-    run("kontena service scale test-1 2")
-    k = kommando("kontena service exec --instance 2 test-1 hostname -s")
-    expect(k.run).to be_truthy
-    expect(k.out).to eq("test-1-2\r\n")
-    run("kontena service scale test-1 1")
-  end
-
   it 'runs a command inside a service with tty' do
     k = kommando("kontena service exec -it test-1 sh")
-    
+
     k.out.on("#") do
       k.in << "ls -la /\r"
-      k.out.on "lib64" do 
+      k.out.on "lib64" do
         k.in << "exit\r"
       end
     end
@@ -49,11 +54,40 @@ describe 'service exec' do
     expect(k.out).to eq('reeb')
   end
 
-  it 'runs a command on every instance with --all' do 
-    run("kontena service scale test-1 2")
-    k = kommando("kontena service exec --all --silent test-1 hostname -s")
-    expect(k.run).to be_truthy
-    expect(k.out).to eq("test-1-1\r\ntest-1-2\r\n")
-    run("kontena service scale test-1 1")
+  context 'with multiple instances' do
+    before(:all) do
+      run("kontena service scale test-1 2")
+    end
+
+    after(:all) do
+      run("kontena service scale test-1 1")
+    end
+
+    it 'runs a command inside a service on a given instances' do
+      k = kommando("kontena service exec --instance 2 test-1 hostname -s")
+      expect(k.run).to be_truthy
+      expect(k.out).to eq("test-1-2\r\n")
+    end
+
+
+    it 'runs a command on every instance with --all' do
+      k = kommando("kontena service exec --all --silent test-1 hostname -s")
+      expect(k.run).to be_truthy
+      expect(k.out).to eq("test-1-1\r\ntest-1-2\r\n")
+    end
+
+    it 'fails early if running a command on every instances with --all fails' do
+      k = kommando("kontena service exec --all --silent --shell test-1 hostname -s && false")
+      expect(k.run).to be_truthy
+      expect(k.code).to_not eq 0
+      expect(k.out).to eq("test-1-1\r\n")
+    end
+
+    it 'keeps going if running a command on every instances with --all --skip' do
+      k = kommando("kontena service exec --all --skip --silent --shell test-1 hostname -s && false")
+      expect(k.run).to be_truthy
+      expect(k.code).to_not eq 0
+      expect(k.out).to eq("test-1-1\r\ntest-1-2\r\n")
+    end
   end
 end
