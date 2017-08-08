588a72938df30ddeb33a4239ae0697939069f5d3
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-08-08 08:27:32 +0300

    specs

--- server/spec/services/scheduler/filter/affinity_spec.rb
+++ server/spec/services/scheduler/filter/affinity_spec.rb
@@ -32,6 +32,28 @@ describe Scheduler::Filter::Affinity do
     end
   end
 
+  describe '#hard_affinities' do
+    it 'returns empty array if no hard affinities' do
+      expect(subject.hard_affinities(['foo==~bar'])).to eq([])
+    end
+
+    it 'returns hard affinities' do
+      affinities = ['foo==bar', 'foo!=bar', 'bar==~baz', 'bar!=~baz']
+      expect(subject.hard_affinities(affinities)).to eq(['foo==bar', 'foo!=bar'])
+    end
+  end
+
+  describe '#soft_affinities' do
+    it 'returns empty array if no soft affinities' do
+      expect(subject.soft_affinities(['foo==bar'])).to eq([])
+    end
+
+    it 'returns soft affinities' do
+      affinities = ['foo==bar', 'foo!=bar', 'bar==~baz', 'bar!=~baz']
+      expect(subject.soft_affinities(affinities)).to eq(['bar==~baz', 'bar!=~baz'])
+    end
+  end
+
   describe '#for_service' do
     it 'returns all nodes if service does not have any affinities defined' do
       service = double(:service, affinity: [])
@@ -157,6 +179,29 @@ describe Scheduler::Filter::Affinity do
         filtered = subject.for_service(service, 1, nodes)
         expect(filtered.size).to eq(1)
         expect(filtered).to eq([nodes[1]])
+      end
+    end
+
+    context 'soft affinity' do
+      it 'returns matching node' do
+        service = double(:service, affinity: ['label==~hdd'])
+        filtered = subject.for_service(service, 1, nodes)
+        expect(filtered.size).to eq(1)
+        expect(filtered).to eq([nodes[1]])
+      end
+
+      it 'returns all nodes if affinity does not match any' do
+        service = double(:service, affinity: ['label==~gpu'])
+        filtered = subject.for_service(service, 1, nodes)
+        expect(filtered.size).to eq(3)
+        expect(filtered).to eq(nodes)
+      end
+
+      it 'returns matching node based on hard-affinity if soft-affinity does not match any' do
+        service = double(:service, affinity: ['label==az-2', 'label==~gpu'])
+        filtered = subject.for_service(service, 1, nodes)
+        expect(filtered.size).to eq(1)
+        expect(filtered).to eq([nodes[2]])
       end
     end
 
