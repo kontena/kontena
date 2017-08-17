bd0ebf3e9c23dbf130d17bcd06b901fc156d0af5
Tero Marttila <tero.marttila@kontena.io>
2017-08-17 16:10:35 +0300

    specs CELLULOID_DEBUG, better TRACE_FIBERS

--- agent/spec/support/fiber_tracer.rb
+++ agent/spec/support/fiber_tracer.rb
@@ -1,16 +1,18 @@
 module FiberTracer
   module ClassMethods
     def yield(*args)
-      $stderr.puts "FIBER YIELD <- \n\t#{caller.join("\n\t")}\n"
+      $stderr.puts "FIBER YIELD #{self.class.name} <- #{args.first.class.name} \n\t#{caller.join("\n\t")}\n"
       out = super
-      $stderr.puts "FIBER YIELD ->\n\t#{caller.join("\n\t")}\n"
+      $stderr.puts "FIBER YIELD #{self.class.name} -> #{out.class.name}\n\t#{caller.join("\n\t")}\n"
       out
     end
   end
 
   def resume(*args)
-    #$stderr.puts "FIBER RESUME:\n\t#{caller.join("\n\t")}\n"
-    super
+    $stderr.puts "FIBER RESUME #{self.class.name} <- #{args.first.class.name}\n\t#{caller.join("\n\t")}\n"
+    out = super
+    $stderr.puts "FIBER RESUME #{self.class.name} -> #{out.class.name}\n\t#{caller.join("\n\t")}\n"
+    out
   end
 
   def self.prepended(base)
