86fd7a0e23f22d067914c3e52e1c1e85f2dbf56b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-08-22 15:27:34 +0300

    create container per pre_start hook

--- agent/lib/kontena/service_pods/lifecycle_hook_manager.rb
+++ agent/lib/kontena/service_pods/lifecycle_hook_manager.rb
@@ -16,29 +16,23 @@ module Kontena
         pre_start_hooks = hooks_for('pre_start')
         return if pre_start_hooks.size == 0
 
-        pre_pod = service_pod.dup
-        pre_pod.entrypoint = '/bin/sh'
-        pre_pod.cmd = ['-c', 'sleep 600']
-        service_config = config_container(pre_pod)
-        service_container = create_container(service_config)
-        service_container.start!
         pre_start_hooks.each do |hook|
-          info "running pre_start hook: #{hook['cmd']}"
-          command = build_cmd(hook['cmd'])
-          log_hook_output(service_container.id, ["running pre_start hook: #{hook['cmd']}"], 'stdout')
-          _, _, exit_code = service_container.exec(command) { |stream, chunk|
-            log_hook_output(service_container.id, [chunk], stream)
-          }
-          if exit_code != 0
-            raise "Failed to execute pre_start hook: #{hook['cmd']} (exit code: #{exit_code}"
+          begin
+            info "running pre_start hook: #{hook['cmd']}"
+            service_config = config_container(service_pod.dup, hook['cmd'])
+            service_container = create_container(service_config)
+            service_container.tap(&:start).attach
+            if service_container.state['ExitCode'] != 0
+              raise "Failed to execute pre_start hook: #{hook['cmd']}, exit code: #{service_container.state['ExitCode']}"
+            end
+          ensure
+            cleanup_container(service_container) if service_container
           end
         end
         true
       rescue => exc
         log_service_pod_event("service:create_instance", exc.message, Logger::ERROR)
         raise exc
-      ensure
-        cleanup_container(service_container) if service_container
       end
 
       # @param [Docker::Container] service_container
@@ -75,6 +69,18 @@ module Kontena
         command.unshift('/w/w') if service_pod.can_expose_ports?
 
         command
+      end
+
+      # @param [Hash] service_config
+      # @param [String] cmd
+      # @return [Hash]
+      def config_container(service_config, cmd)
+        service_config = super(service_config)
+        service_config['HostConfig'].delete('RestartPolicy')
+        service_config['Labels']['io.kontena.container.type'] = 'service_hook'
+        service_config['Cmd'] = build_cmd(cmd)
+
+        service_config
       end
 
       # @param [String] id
