492cc5131b9b873b651ef82f6f42218593450a0d
Tero Marttila <tero.marttila@kontena.io>
2017-08-23 17:59:58 +0300

    benchmark cross-actor observe

---
+++ agent/benchmark/observable-actors.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require_relative './benchmark-support'
+
+class TestObservableActor
+  include Celluloid
+  include Kontena::Observable
+
+  DELAY = getenv('START_DELAY', 1.0) { |x| Float(x) }
+
+  def start(delay: DELAY)
+    sleep delay
+    @ready = Time.now
+    update_observable @ready
+  end
+
+  def ready?
+    @ready
+  end
+end
+
+class TestObserverActor
+  include Celluloid
+  include Kontena::Observer
+
+  def watch
+    ready = observe(Actor[:test_observable])
+    Time.now - ready
+  end
+end
+
+class TestWaitActor
+  include Celluloid
+  include Kontena::Helpers::WaitHelper
+
+  WAIT_INTERVAL = 0.001
+
+  def watch
+    ready = wait_until!("observable ready") { Actor[:test_observable].ready? }
+    Time.now - ready
+  end
+end
+
+
+test_observer = TestObserverActor.new
+test_wait = TestWaitActor.new
+
+benchmark(
+  {
+    'wait'      => ->(id) { test_wait.future.watch },
+    'observer'  => ->(id) { test_observer.future.watch },
+  },
+  before_each: ->() {
+    test_observable = Celluloid::Actor[:test_observable] = TestObservableActor.new
+    test_observable.async.start
+  },
+)
