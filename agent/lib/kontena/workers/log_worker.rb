cb18365811237b4c904d634f62ae4d78be621a92
Tero Marttila <SpComb@users.noreply.github.com>
2017-08-28 14:53:14 +0300

    Fix agent LogWorker start/stop (#2728)
    * agent: fix LogWorker start/stop
    
    * only start if websocket connected, pause process loop instead of stopping it
    
    * fixup initialize attributes

--- agent/lib/kontena/workers/log_worker.rb
+++ agent/lib/kontena/workers/log_worker.rb
@@ -1,5 +1,6 @@
 require_relative 'container_log_worker'
 require_relative '../helpers/rpc_helper'
+require_relative '../helpers/wait_helper'
 
 module Kontena::Workers
   class LogWorker
@@ -7,6 +8,7 @@ module Kontena::Workers
     include Celluloid::Notifications
     include Kontena::Logging
     include Kontena::Helpers::RpcHelper
+    include Kontena::Helpers::WaitHelper
 
     attr_reader :queue, :etcd, :workers
 
@@ -17,57 +19,33 @@ module Kontena::Workers
     ETCD_PREFIX = '/kontena/log_worker/containers'
     QUEUE_MAX_SIZE = 2000
     QUEUE_THROTTLE = (QUEUE_MAX_SIZE * 0.8)
+    WATCH_INTERVAL = 10.0
+
+    def websocket_client
+      Celluloid::Actor[:websocket_client]
+    end
 
     # @param [Boolean] autostart
     def initialize(autostart = true)
       @queue = Queue.new
       @workers = {}
       @etcd = Etcd.client(host: '127.0.0.1', port: 2379)
+      @processing = false
+      @streaming = false
       subscribe('container:event', :on_container_event)
       subscribe('websocket:connected', :on_connect) # from master_info RPC
       subscribe('websocket:disconnected', :on_disconnect)
       info 'initialized'
 
-      async.start if autostart
-    end
-
-    # @param [String] topic
-    # @param [Object] data
-    def on_connect(topic, data)
-      @queue_processing = true
-      async.start
-      info 'started log streaming'
-    end
-
-    # @param [String] topic
-    # @param [Object] data
-    def on_disconnect(topic, data)
-      @queue_processing = false
-      async.stop
-      info 'stopped log streaming'
-    end
-
-    def queue_processing?
-      @queue_processing == true
-    end
-
-    def start
-      sleep 1 until Actor[:etcd_launcher].running?
-      Docker::Container.all.each do |container|
-        begin
-          self.stream_container_logs(container) unless container.skip_logs?
-        rescue Docker::Error::NotFoundError
-          # Could be thrown since container.skip_logs? actually loads the container details
-        end
+      if autostart
+        async.watch_queue
+        async.process_queue
+        async.start if websocket_client && websocket_client.connected?
       end
-
-      async.watch_queue
-      async.process_queue
     end
 
     def watch_queue
-      loop do
-        sleep 10
+      every(WATCH_INTERVAL) do
         if @queue.size > QUEUE_MAX_SIZE
           warn "queue is full (size is #{@queue.size}), log lines are dropped until queue has free space"
         elsif @queue.size > QUEUE_THROTTLE
@@ -78,9 +56,12 @@ module Kontena::Workers
       end
     end
 
+    # Process items from @queue until nil
     def process_queue
       defer {
-        while queue_processing? && data = @queue.pop
+        while data = @queue.pop
+          sleep 1 until @processing
+
           rpc_client.async.notification('/containers/log', [data])
           if @queue.size > 100
             sleep 0.001
@@ -91,21 +72,102 @@ module Kontena::Workers
       }
     end
 
+    # Process queued items
+    #
+    def resume_processing
+      @processing = true
+    end
+
+    # Stop processing items from the queue, and leave them enqueued until re-started.
+    #
+    def pause_processing
+      @processing = false
+    end
+
+    # @param [String] topic
+    # @param [Object] data
+    def on_connect(topic, data)
+      start
+    end
+
+    # @param [String] topic
+    # @param [Object] data
+    def on_disconnect(topic, data)
+      stop
+    end
+
+    def start
+      exclusive {
+        wait_until!("etcd running") { Actor[:etcd_launcher].running? }
+
+        start_streaming unless streaming?
+        resume_processing
+      }
+    end
+
     def stop
-      return unless queue_processing?
+      exclusive {
+        pause_processing
+        stop_streaming if streaming?
+      }
+    end
+
+    def streaming?
+      !!@streaming
+    end
+
+    # requires etcd to be available to read log timestamps
+    def start_streaming
+      info 'start streaming logs from containers'
+
+      Docker::Container.all.each do |container|
+        begin
+          self.stream_container_logs(container) unless container.skip_logs?
+        rescue Docker::Error::NotFoundError => exc
+          # Could be thrown since container.skip_logs? actually loads the container details
+          warn exc.message
+        rescue => exc
+          error exc
+        end
+      end
+
+      @streaming = true
+    end
+
+    # @param [Docker::Container] container
+    def stream_container_logs(container)
+      unless workers[container.id]
+        workers[container.id] = ContainerLogWorker.new(container, queue)
+        since = 0
+        key = etcd.get("#{ETCD_PREFIX}/#{container.id}") rescue nil
+        if key
+          etcd.delete("#{ETCD_PREFIX}/#{container.id}")
+          since = key.value.to_i
+        end
+        workers[container.id].async.start(since.to_i)
+      end
+    end
+
+    # @param [String] container_id
+    def stop_streaming_container_logs(container_id)
+      worker = workers.delete(container_id)
+      if worker
+        # we have to use kill because worker is blocked by log stream
+        Celluloid::Actor.kill(worker) if worker.alive?
+      end
+    rescue => exc
+      error exc
+    end
+
+    # best-effort attempt to write etcd timestamps; may not be possible
+    def stop_streaming
+      @streaming = false
+
+      info 'stop log streaming'
 
       @workers.keys.dup.each do |id|
         self.stop_streaming_container_logs(id)
         self.mark_timestamp(id, Time.now.to_i)
-      end
-    rescue => exc
-      error "#{exc.class.name}: #{exc.message}"
-    end
-
-    def mark_timestamps
-      ts = Time.now.to_i
-      workers.keys.each do |id|
-        self.mark_timestamp(id, ts)
       end
     end
 
@@ -117,35 +179,6 @@ module Kontena::Workers
       nil
     end
 
-    # @param [Docker::Container] container
-    def stream_container_logs(container)
-      exclusive {
-        unless workers[container.id]
-          workers[container.id] = ContainerLogWorker.new(container, queue)
-          since = 0
-          key = etcd.get("#{ETCD_PREFIX}/#{container.id}") rescue nil
-          if key
-            etcd.delete("#{ETCD_PREFIX}/#{container.id}")
-            since = key.value.to_i
-          end
-          workers[container.id].async.start(since.to_i)
-        end
-      }
-    rescue => exc
-      error "#{exc.class.name}: #{exc.message}"
-    end
-
-    # @param [String] container_id
-    def stop_streaming_container_logs(container_id)
-      worker = workers.delete(container_id)
-      if worker
-        # we have to use kill because worker is blocked by log stream
-        Celluloid::Actor.kill(worker) if worker.alive?
-      end
-    rescue => exc
-      error exc.message
-    end
-
     # @param [String] topic
     # @param [Docker::Event] event
     def on_container_event(topic, event)
@@ -154,17 +187,26 @@ module Kontena::Workers
       elsif START_EVENTS.include?(event.status)
         container = Docker::Container.get(event.id) rescue nil
         debug "#{container.name}/#{container.labels}"
-        if container && queue_processing? && !container.skip_logs?
-          stream_container_logs(container)
+        if container && streaming? && !container.skip_logs?
+          exclusive {
+            stream_container_logs(container)
+          }
         elsif container
           mark_timestamp(container.id, Time.now.to_i)
         end
       end
+    rescue => exc
+      error exc
+    end
+
+    # Close the log queue, stopping processing once empty. It cannot be restarted anymore.
+    def close
+      @queue.close
     end
 
     def finalize
-      workers.keys.each{ |id| stop_streaming_container_logs(id) }
-      self.mark_timestamps
+      stop_streaming if streaming?
+      close
     end
   end
 end
