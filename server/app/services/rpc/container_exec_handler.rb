3f65cba817efe2ce63920a2f147e2a2961f87757
Tero Marttila <tero.marttila@kontena.io>
2017-08-28 17:40:47 +0300

    add container exec error protocol

--- server/app/services/rpc/container_exec_handler.rb
+++ server/app/services/rpc/container_exec_handler.rb
@@ -13,5 +13,9 @@ module Rpc
     def exit(uuid, exit_code)
       MongoPubsub.publish("container_exec:#{uuid}", {exit: exit_code})
     end
+
+    def error(uuid, error)
+      MongoPubsub.publish("container_exec:#{uuid}", {error: error})
+    end
   end
 end
