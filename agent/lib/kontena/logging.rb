3f7872026c72a4b93906833b5695adc9f07a45ec
Tero Marttila <tero.marttila@kontena.io>
2017-08-29 11:35:12 +0300

    agent logging: overridable logging_prefix

--- agent/lib/kontena/logging.rb
+++ agent/lib/kontena/logging.rb
@@ -21,32 +21,37 @@ module Kontena
       Kontena::Logging.logger
     end
 
+    # @return [String]
+    def logging_prefix
+      self.class.name
+    end
+
     # Send a debug message
     # @param message [String]
     # @yield optionally set the message using a block
     def debug(message = nil, &block)
-      logger.add(Logger::DEBUG, message, self.class.name, &block)
+      logger.add(Logger::DEBUG, message, self.logging_prefix, &block)
     end
 
     # Send a info message
     # @param message [String]
     # @yield optionally set the message using a block
     def info(message = nil, &block)
-      logger.add(Logger::INFO, message, self.class.name, &block)
+      logger.add(Logger::INFO, message, self.logging_prefix, &block)
     end
 
     # Send a warning message
     # @param message [String]
     # @yield optionally set the message using a block
     def warn(message = nil, &block)
-      logger.add(Logger::WARN, message, self.class.name, &block)
+      logger.add(Logger::WARN, message, self.logging_prefix, &block)
     end
 
     # Send an error message
     # @param message [String]
     # @yield optionally set the message using a block
     def error(message = nil, &block)
-      logger.add(Logger::ERROR, message, self.class.name, &block)
+      logger.add(Logger::ERROR, message, self.logging_prefix, &block)
     end
   end
 end
