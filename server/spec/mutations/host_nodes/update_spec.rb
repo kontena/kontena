a3ebecf0cbbd9abee3b0bd72aeca7640ffed0095
Tero Marttila <SpComb@users.noreply.github.com>
2017-08-30 09:56:24 +0300

    Fix server node update grid notify (#2747)
    * better spec for HostNodes::Common#notify_grid
    
    * fix HostNodes::Common#notify_node use of Agent::NodePlugger
    
    * refactor host node update spec
    
    * spec host node update service instance start/stop notify
    
    * fix host node availability update service notify RPC
    
    * spec node update grid notify
    
    * tweak spec context
    
    * spec reload model during expect

--- server/spec/mutations/host_nodes/update_spec.rb
+++ server/spec/mutations/host_nodes/update_spec.rb
@@ -3,9 +3,14 @@ describe HostNodes::Update do
 
   let(:grid) { Grid.create!(name: 'test') }
   let(:node) { grid.create_node!('node-1', node_id: 'AA') }
+  let(:rpc_client) { instance_double(RpcClient) }
+
+  before do
+    allow(RpcClient).to receive(:new).with(node.node_id, Integer).and_return(rpc_client)
+  end
 
   describe '#run' do
-    context 'labels' do
+    describe 'labels' do
       it 'updates node labels' do
         node.labels = []
         labels = ['foo=bar', 'bar=baz']
@@ -27,18 +32,34 @@ describe HostNodes::Update do
         }.not_to change{ node.labels }
       end
 
-      it 'notifies grid nodes' do
-        mutation = described_class.new(
-          host_node: node,
-          labels: []
-        )
-        expect(mutation).to receive(:notify_grid).once.with(grid)
-        mutation.run
+      context 'with connected nodes' do
+        let(:node2) { grid.create_node!('node-2', node_id: 'BBB', connected: true) }
+        let(:rpc_client2) { instance_double(RpcClient) }
+
+        before do
+          node.set(connected: true)
+          allow(RpcClient).to receive(:new).with(node2.node_id, Integer).and_return(rpc_client2)
+        end
+
+        it 'notifies both grid nodes' do
+          expect(rpc_client).to receive(:notify).with('/agent/node_info', hash_including(id: 'AA'))
+          expect(rpc_client2).to receive(:notify).with('/agent/node_info', hash_including(id: 'BBB'))
+
+          outcome = described_class.run(
+            host_node: node,
+            labels: []
+          )
+          expect(outcome).to be_success
+        end
       end
     end
 
-    context 'availability' do
-      context 'drain' do
+    describe 'availability' do
+      context 'for an active node' do
+        before do
+          node.set(availability: HostNode::Availability::ACTIVE) # default
+        end
+
         it 'updates availability to drain' do
           expect {
             outcome = described_class.new(
@@ -46,48 +67,80 @@ describe HostNodes::Update do
               availability: HostNode::Availability::DRAIN,
               labels: []
             ).run
-            expect(outcome.success?).to be_truthy
+            expect(outcome).to be_success
           }.to change{ node.reload.availability }.from(HostNode::Availability::ACTIVE).to(HostNode::Availability::DRAIN)
         end
 
-        it 'stops stateful services when draining' do
-          mutation = described_class.new(
-            host_node: node,
-            availability: HostNode::Availability::DRAIN,
-            labels: []
-          )
-          expect(mutation).to receive(:stop_stateful_services)
+        context 'with a running stateful service instance' do
+          let!(:stack) { grid.stacks.find_by(name: 'null') }
+          let!(:stateful_service) { grid.grid_services.create!(stack: stack, name: 'redis', image_name: 'redis:latest', stateful: true, state: 'running') }
+          let!(:stateful_service_instance1) { stateful_service.grid_service_instances.create!(host_node: node, instance_number: 1, desired_state: 'running') }
 
-          mutation.run
+          it 'stops stateful services when draining' do
+            expect(rpc_client).to receive(:notify).with('/service_pods/notify_update', 'stop') do
+              expect(stateful_service_instance1.reload.desired_state).to eq 'stopped'
+            end
+
+            expect{
+              outcome = described_class.run(
+                host_node: node,
+                availability: HostNode::Availability::DRAIN,
+                labels: []
+              )
+              expect(outcome).to be_success
+            }.to change{stateful_service_instance1.reload.desired_state}.from('running').to('stopped')
+          end
         end
       end
 
-      context 'active' do
-        it 'updates availability to active' do
-          node.availability = HostNode::Availability::DRAIN
-          expect {
+      context 'for a drained node' do
+        before do
+          node.set(availability: HostNode::Availability::DRAIN)
+        end
 
-            mutation = described_class.new(
+        it 'updates availability to active' do
+          expect {
+            outcome = described_class.run(
               host_node: node,
               availability: HostNode::Availability::ACTIVE,
               labels: []
             )
-            expect(mutation).to receive(:start_stateful_services)
-            mutation.run
+            expect(outcome).to be_success
           }.to change{ node.availability }.from(HostNode::Availability::DRAIN).to(HostNode::Availability::ACTIVE)
         end
-      end
 
-      it 'triggers actions only when availability changes' do
-        node.availability = HostNode::Availability::DRAIN
-        expect(node).not_to receive(:set)
-        expect {
-          described_class.new(
-            host_node: node,
-            availability: HostNode::Availability::DRAIN,
-            labels: []
-          ).run
-        }.not_to change{ node }
+        context 'with a stopped stateful service instance' do
+          let!(:stack) { grid.stacks.find_by(name: 'null') }
+          let!(:stateful_service) { grid.grid_services.create!(stack: stack, name: 'redis', image_name: 'redis:latest', stateful: true, state: 'running') }
+          let!(:stateful_service_instance1) { stateful_service.grid_service_instances.create!(host_node: node, instance_number: 1, desired_state: 'stopped') }
+
+          it 'starts and notifies the service instance' do
+            expect(rpc_client).to receive(:notify).with('/service_pods/notify_update', 'start') do
+              expect(stateful_service_instance1.reload.desired_state).to eq 'running'
+            end
+
+            expect {
+              outcome = described_class.run(
+                host_node: node,
+                availability: HostNode::Availability::ACTIVE,
+                labels: []
+              )
+              expect(outcome).to be_success
+            }.to change{ stateful_service_instance1.reload.desired_state }.from('stopped').to('running')
+          end
+        end
+
+        it 'ignores update to drain' do
+          expect(node).not_to receive(:set)
+          expect {
+            outcome = described_class.new(
+              host_node: node,
+              availability: HostNode::Availability::DRAIN,
+              labels: []
+            ).run
+            expect(outcome).to be_success
+          }.not_to change{ node }
+        end
       end
     end
   end
