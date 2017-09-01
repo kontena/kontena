b6980f55d1da6695b9d7c46d25736625e40e5315
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-09-01 18:16:18 +0300

    add --agree-tos flag for scripting

--- cli/lib/kontena/cli/certificate/register_command.rb
+++ cli/lib/kontena/cli/certificate/register_command.rb
@@ -7,19 +7,24 @@ module Kontena::Cli::Certificate
 
     parameter "EMAIL", "Email to register"
 
+    option '--agree-tos', :flag, "Automatically agree on Let's Encrypt Terms of Service"
+
     def execute
       require_api_url
       token = require_token
 
       data = {email: email}
-      puts "By registering, you agree on Let's Encrypt Terms of Service: https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
-      agree_tos = prompt.yes?("Continue?")
-      if agree_tos
+
+      if self.agree_tos? || ask_continue
         response = client(token).post("certificates/#{current_grid}/register", data)
         puts 'Email registered to LetsEncrypt'
-      else
-        puts "Registration canceled!".colorize(:red)
       end
+    end
+
+    def ask_continue
+      puts "By registering, you agree on Let's Encrypt Terms of Service: https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
+      exit_with_error "Registration canceled!" unless prompt.yes?("Continue?")
+      true
     end
   end
 end
