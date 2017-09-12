0c9b84f32163030e0ffe93ba64ee6023b1242ada
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-09-12 16:26:29 -0400

    fix restart

--- agent/lib/kontena/rpc/service_pods_api.rb
+++ agent/lib/kontena/rpc/service_pods_api.rb
@@ -4,17 +4,35 @@ module Kontena
   module Rpc
     class ServicePodsApi
 
-      # @param [String] service_id
-      # @param [Integer] instance_number
+      # @param service_id [String]
+      # @param instance_number [Integer]
       # @return [Hash]
       def restart(service_id, instance_number)
-        Kontena::ServicePods::Restarter.new(service_id, instance_number).perform
+        service_pod = find_service_pod(service_id, instance_number)
+        raise Kontena::RpcServer::Error.new(404, "Service instance not found") unless service_pod
+
+        Kontena::ServicePods::Restarter.new(service_pod).perform
         {}
       end
 
       def notify_update(reason)
         Celluloid::Notifications.publish('service_pod:update', reason)
         {}
+      end
+
+      private
+
+      # @param service_id [String]
+      # @param instance_number [Integer]
+      def find_service_pod(service_id, instance_number)
+        worker = Celluloid::Actor[:service_pod_manager].workers.find { |w|
+          w.service_pod.service_id == service_id && w.service_pod.instance_number == instance_number
+        }
+        if worker
+          worker.service_pod
+        else
+          nil
+        end
       end
     end
   end
