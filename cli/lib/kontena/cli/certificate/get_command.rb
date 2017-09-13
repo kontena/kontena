c7da95edb8784c64aecdb009c96b1196c267c429
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-09-13 22:17:10 +0300

    new cert request command and old one deprecated

--- cli/lib/kontena/cli/certificate/get_command.rb
+++ cli/lib/kontena/cli/certificate/get_command.rb
@@ -4,16 +4,29 @@ module Kontena::Cli::Certificate
     include Kontena::Cli::Common
     include Kontena::Cli::GridOptions
 
+    BANNER = "This command is now deprecated in favor of 'kontena certificate request' command".colorize(:red)
+
+    banner BANNER
+
+    option '--secret-name', 'SECRET_NAME', 'The name for the secret to store the certificate in'
+    option '--cert-type', 'CERT_TYPE', 'The type of certificate to get: fullchain, chain or cert', default: 'fullchain'
     parameter "DOMAIN ...", "Domain(s) to get certificate for"
 
+
     def execute
+      puts BANNER
+
       require_api_url
       token = require_token
-      data = {domains: domain_list}
+      secret = secret_name || "LE_CERTIFICATE_#{domain_list[0].gsub('.', '_')}"
+      data = {domains: domain_list, secret_name: secret}
 
-      spinner "Requesting certificate for #{domain_list.join(',').colorize(:cyan)} " do
-        response = client(token).post("grids/#{current_grid}/certificates", data)
+      response = client(token).post("certificates/#{current_grid}/certificate", data)
+      puts "Certificate successfully received and stored into vault with keys:"
+      response.each do |secret|
+        puts secret.colorize(:green)
       end
+      puts "Use the #{secret}_BUNDLE with Kontena loadbalancer!"
 
     end
   end
