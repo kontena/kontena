ada4d920171cf168e5efcaae0364bfb8b1957dc7
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-09-15 15:42:18 -0400

    revert restarter changes

--- agent/lib/kontena/rpc/service_pods_api.rb
+++ agent/lib/kontena/rpc/service_pods_api.rb
@@ -8,31 +8,13 @@ module Kontena
       # @param instance_number [Integer]
       # @return [Hash]
       def restart(service_id, instance_number)
-        service_pod = find_service_pod(service_id, instance_number)
-        raise Kontena::RpcServer::Error.new(404, "Service instance not found") unless service_pod
-
-        Kontena::ServicePods::Restarter.new(service_pod).perform
+        Kontena::ServicePods::Restarter.new(service_id, instance_number).perform
         {}
       end
 
       def notify_update(reason)
         Celluloid::Notifications.publish('service_pod:update', reason)
         {}
-      end
-
-      private
-
-      # @param service_id [String]
-      # @param instance_number [Integer]
-      def find_service_pod(service_id, instance_number)
-        worker = Celluloid::Actor[:service_pod_manager].workers.find { |w|
-          w.service_pod.service_id == service_id && w.service_pod.instance_number == instance_number
-        }
-        if worker
-          worker.service_pod
-        else
-          nil
-        end
       end
     end
   end
--- agent/spec/lib/kontena/rpc/service_pods_api_spec.rb
+++ agent/spec/lib/kontena/rpc/service_pods_api_spec.rb
@@ -9,18 +9,9 @@ describe Kontena::Rpc::ServicePodsApi do
 
   describe '#restart' do
     it 'calls service pod restarter' do
-      service_pod = double(:service_pod)
-      allow(subject).to receive(:find_service_pod).and_return(service_pod)
-      expect(Kontena::ServicePods::Restarter).to receive(:new).with(service_pod).and_return(executor)
+      expect(Kontena::ServicePods::Restarter).to receive(:new).and_return(executor)
       expect(executor).to receive(:perform)
       subject.restart('service_id', 2)
-    end
-
-    it 'raises error if service_pod not found' do
-      allow(subject).to receive(:find_service_pod).and_return(nil)
-      expect {
-        subject.restart('service_id', 2)
-      }.to raise_error(Kontena::RpcServer::Error)
     end
   end
 
