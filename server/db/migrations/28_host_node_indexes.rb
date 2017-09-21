cc766bf198774aadf8698862496e4ad8202b2e9b
Jussi Nummelin <jussi.nummelin@kontena.io>
2017-09-21 20:42:30 +0300

    re-order migrations to match better for specs

--- server/db/migrations/28_host_node_indexes.rb
+++ server/db/migrations/28_host_node_indexes.rb
@@ -1,5 +1,6 @@
 class HostNodeIndexes < Mongodb::Migration
   def self.up
+
     # Make sure each node has proper node_number and name
     HostNode.each do |node|
       unless node.node_number
@@ -13,7 +14,7 @@ class HostNodeIndexes < Mongodb::Migration
 
     # Drop old indexes before re-creating
     HostNode.collection.indexes.drop_one('grid_id_1_node_number_1') rescue nil
-    HostNode.collection.indexes.drop_one('node_id_1')
+    HostNode.collection.indexes.drop_one('node_id_1') rescue nil
 
     HostNode.create_indexes
   end
