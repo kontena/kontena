e90ae5ed89db185f848add9fe07d1af5c60eba61
Tero Marttila <tero.marttila@kontena.io>
2017-10-04 13:35:39 +0300

    cli omnibus: update software/liblzma from upstream

--- cli/omnibus/config/software/liblzma.rb
+++ cli/omnibus/config/software/liblzma.rb
@@ -21,8 +21,10 @@ license "Public-Domain"
 license_file "COPYING"
 skip_transitive_dependency_licensing true
 
-source url: "http://tukaani.org/xz/xz-#{version}.tar.gz",
-       md5: "ef68674fb47a8b8e741b34e429d86e9d"
+version("5.2.2") { source md5: "7cf6a8544a7dae8e8106fdf7addfa28c" }
+version("5.2.3") { source md5: "ef68674fb47a8b8e741b34e429d86e9d" }
+
+source url: "http://tukaani.org/xz/xz-#{version}.tar.gz"
 
 relative_path "xz-#{version}"
 
