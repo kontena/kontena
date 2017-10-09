446c420dc05a9bfd5e0689c16dcd6a039446ccef
Tero Marttila <tero.marttila@kontena.io>
2017-10-09 18:21:46 +0300

    also check list exit

--- test/spec/features/plugin/uninstall_spec.rb
+++ test/spec/features/plugin/uninstall_spec.rb
@@ -8,8 +8,9 @@ describe 'plugin uninstall' do
   it 'removes installed plugin' do
     k = run('kontena plugin uninstall aws')
     expect(k.code).to eq(0)
-    
+
     k = run('kontena plugin ls')
+    expect(k.code).to eq(0)
     expect(k.out).to_not match(/aws/)
   end
 end
