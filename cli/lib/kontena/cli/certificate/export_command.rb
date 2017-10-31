1c724c7b5c98b6a491ec6d94af32d2037e867356
Tero Marttila <tero.marttila@kontena.io>
2017-10-31 19:24:52 +0200

    kontena certificate export --certificate --chain --private-key options

--- cli/lib/kontena/cli/certificate/export_command.rb
+++ cli/lib/kontena/cli/certificate/export_command.rb
@@ -9,12 +9,20 @@ module Kontena::Cli::Certificate
     requires_current_master_token
     requires_current_grid
 
+    option ['--certificate', '--cert'], :flag, "Output certificate"
+    option ['--chain'], :flag, "Output chain"
+    option ['--private-key', '--key'], :flag, "Output private key"
+
+    def bundle?
+      ![certificate?, chain?, private_key?].any?
+    end
+
     def execute
       certificate = client.get("certificates/#{current_grid}/#{self.subject}/export")
 
-      puts certificate['certificate_pem']
-      puts certificate['chain_pem']
-      puts certificate['private_key_pem']
+      puts certificate['certificate_pem'] if certificate? || bundle?
+      puts certificate['chain_pem'] if chain? || bundle?
+      puts certificate['private_key_pem'] if private_key? || bundle?
     end
   end
 end
