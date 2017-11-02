50656763f8850d3c3eb510488b071db3c879ed2a
Tero Marttila <tero.marttila@kontena.io>
2017-11-02 11:16:19 +0200

    spec command failures

--- cli/spec/kontena/cli/main_command_spec.rb
+++ cli/spec/kontena/cli/main_command_spec.rb
@@ -15,5 +15,31 @@ describe Kontena::MainCommand do
       expect{subject.run(['testplugin', 'master', 'create'])}.to raise_error(Clamp::UsageError)
     end
   end
-end
 
+  context 'for a command that raises errors' do
+    let(:test_fail_command) { Class.new(Kontena::Command) do
+      def execute
+        fail 'test'
+      end
+    end}
+
+    before do
+      Kontena::MainCommand.subcommand 'test-fail', "Test failures", test_fail_command
+    end
+
+    it 'logs an error and aborts' do
+      expect{subject.run(['test-fail'])}.to raise_error(SystemExit).and output(/\[error\] RuntimeError : test\s+See .* or run the command again with environment DEBUG=true set to see the full exception/m).to_stderr
+    end
+
+    context 'with DEBUG' do
+      before do
+        allow(Kontena).to receive(:debug?).and_return(true)
+      end
+
+      it 'lets the error raise through' do
+        expect{subject.run(['test-fail'])}.to raise_error(RuntimeError, 'test')
+      end
+
+    end
+  end
+end
