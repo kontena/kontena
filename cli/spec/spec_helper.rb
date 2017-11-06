8318f924546156a6f1775ee6e41a3a7387d2f186
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-11-06 11:24:08 +0200

    Use a unique temp dir for specs instead of /tmp

--- cli/spec/spec_helper.rb
+++ cli/spec/spec_helper.rb
@@ -27,7 +27,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
   config.before(:each) do
-    allow(Dir).to receive(:home).and_return('/tmp/')
+    @_temp_home_dir = Dir.mktmpdir
+    allow(Dir).to receive(:home).and_return(@_temp_home_dir)
     allow(ENV).to receive(:[]).with(anything).and_call_original
     allow(ENV).to receive(:[]).with('DEBUG').and_call_original
     Kontena::Cli::Config.reset_instance
@@ -41,7 +42,7 @@ RSpec.configure do |config|
   config.after(:each) do
     RSpec::Mocks.space.proxy_for(File).reset
     RSpec::Mocks.space.proxy_for(Kontena::Cli::Config).reset
-    File.unlink(Kontena::Cli::Config.default_config_filename) if File.exist?(Kontena::Cli::Config.default_config_filename)
+    FileUtils.remove_entry @_temp_home_dir if @_temp_home_dir
   end
 
   config.around(:each) do |example|
