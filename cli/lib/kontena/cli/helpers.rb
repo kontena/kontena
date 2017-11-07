2fe79039b408da435ad0259d4fca9f4bcc0b6c07
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-11-07 10:22:24 +0200

    Adding some autoloads

---
+++ cli/lib/kontena/cli/helpers.rb
@@ -0,0 +1,9 @@
+module Kontena
+  module Cli
+    module Helpers
+      autoload :ExecHelper, 'kontena/cli/helpers/exec_helper'
+      autoload :HealthHelper, 'kontena/cli/helpers/health_helper'
+      autoload :LogHelper, 'kontena/cli/helpers/log_helper'
+    end
+  end
+end
---
+++ cli/lib/kontena/cli/stacks.rb
@@ -0,0 +1,10 @@
+module Kontena
+  module Cli
+    module Stacks
+      autoload :YAML, 'kontena/cli/stacks/yaml'
+      autoload :StackName, 'kontena/cli/stacks/stack_name'
+      autoload :Common, 'kontena/cli/stacks/common'
+      autoload :StacksHelper, 'kontena/cli/stacks/stacks_helper'
+    end
+  end
+end
