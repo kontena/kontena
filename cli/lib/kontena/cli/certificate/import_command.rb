4e0c53e8d0adec48a97e3bcccacb84259dc42696
Tero Marttila <tero.marttila@kontena.io>
2017-11-14 17:29:33 +0200

    CLI certificate import: load cert and determine --subject default

--- cli/lib/kontena/cli/certificate/import_command.rb
+++ cli/lib/kontena/cli/certificate/import_command.rb
@@ -1,3 +1,4 @@
+require 'openssl'
 require_relative './common'
 
 module Kontena::Cli::Certificate
@@ -16,7 +17,7 @@ module Kontena::Cli::Certificate
     parameter 'CERT_FILE', "Path to PEM-encodede X.509 certificate file" do |path|
       open_file(path)
     end
-    option '--subject', 'SUBJECT', "Import cert specific subject", :required => true
+    option '--subject', 'SUBJECT', "Import cert specific subject"
     option ['--private-key', '--key'], 'KEY_FILE', "Path to private key file", :required => true, :attribute_name => :key_file do |path|
       open_file(path)
     end
@@ -28,10 +29,27 @@ module Kontena::Cli::Certificate
     requires_current_master_token
     requires_current_grid
 
+    def load_certificate
+      OpenSSL::X509::Certificate.new(self.cert_file)
+    rescue OpenSSL::OpenSSLError => exc
+      exit_with_error "Invalid certificate at #{self.cert_file.path}: #{exc.class}: #{exc.message}"
+    end
+
+    def certificate_subject(cert)
+      cert.subject.to_a.each do |name, data|
+        return data if name == 'CN'
+      end
+
+      exit_with_error "No CN in certificate subject: #{cert.subject}"
+    end
+
     def execute
+      cert = load_certificate
+      subject = self.subject || self.certificate_subject(cert)
+
       certificate = spinner "Importing certificate from #{cert_file.path}..." do
         client.put("certificates/#{current_grid}/#{subject}",
-          certificate: self.cert_file.read(),
+          certificate: cert.to_pem,
           private_key: self.key_file.read(),
           chain: chain_file_list.map{|chain_file| chain_file.read() },
         )
--- cli/spec/kontena/cli/certificates/import_command_spec.rb
+++ cli/spec/kontena/cli/certificates/import_command_spec.rb
@@ -7,6 +7,7 @@ describe Kontena::Cli::Certificate::ImportCommand do
 
   let(:subject) { described_class.new("kontena") }
 
+  let(:csr_path) { fixture_path('certificates/test/csr.pem') }
   let(:ca_path) { fixture_path('certificates/test/ca.pem') }
   let(:cert_path) { fixture_path('certificates/test/cert.pem') }
   let(:key_path) { fixture_path('certificates/test/key.pem') }
@@ -35,14 +36,26 @@ describe Kontena::Cli::Certificate::ImportCommand do
     ).and_return(certificate)
   end
 
+  it "errors for an invalid cert file" do
+    expect{subject.run(["--private-key=#{key_path}", "--chain=#{ca_path}", csr_path])}.to exit_with_error.and output(/Invalid certificate at .*: OpenSSL::X509::CertificateError: .*/).to_stderr
+  end
+
   it "imports the cert files" do
-    subject.run(["--subject=test", "--private-key=#{key_path}", "--chain=#{ca_path}", cert_path])
+    expect{subject.run(["--private-key=#{key_path}", "--chain=#{ca_path}", cert_path])}.to output(/subject: test/).to_stdout
+  end
+
+  describe '--subject' do
+    it 'overrides the subject' do
+      allow(client).to receive(:put).with('certificates/test-grid/example.com', Hash).and_return(certificate)
+
+      subject.run(["--subject=example.com", "--private-key=#{key_path}", "--chain=#{ca_path}", cert_path])
+    end
   end
 
   describe "with non-existant --private-key= path" do
     it "fails with a usage error" do
       expect{
-        described_class.run('kontena', ["--subject=test", "--private-key=/missing/path", "--chain=#{ca_path}", cert_path])
+        described_class.run('kontena', ["--private-key=/missing/path", "--chain=#{ca_path}", cert_path])
       }.to exit_with_error.and output(/ERROR: option '--private-key': File not found: /).to_stderr
     end
   end
@@ -50,7 +63,7 @@ describe Kontena::Cli::Certificate::ImportCommand do
   describe "with non-existant CERT path" do
     it "fails with a usage error" do
       expect{
-        described_class.run('kontena', ["--subject=test", "--private-key=#{key_path}", "--chain=#{ca_path}", '/nonexist'])
+        described_class.run('kontena', ["--private-key=#{key_path}", "--chain=#{ca_path}", '/nonexist'])
       }.to exit_with_error.and output(/ERROR: parameter 'CERT_FILE': File not found: /).to_stderr
     end
   end
