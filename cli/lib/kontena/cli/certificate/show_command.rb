37cfaf0c01115d9ace01eee3140324da074d37bc
Tero Marttila <tero.marttila@kontena.io>
2017-11-14 16:37:28 +0200

    hand-crafted output

--- cli/lib/kontena/cli/certificate/show_command.rb
+++ cli/lib/kontena/cli/certificate/show_command.rb
@@ -11,24 +11,23 @@ module Kontena::Cli::Certificate
     requires_current_master_token
     requires_current_grid
 
-    # @param id [String]
-    # @param attrs [Hash{String => nil, Object}] elides nil values
-    def show_yaml(id, attrs)
-      puts YAML.dump(
-        id => Hash[attrs.select{|k, v| !!v}]
-      ).sub("---\n", '')
+    def show_certificate(cert)
+      puts "#{cert['id']}:"
+      puts "  subject: #{cert['subject']}"
+      puts "  valid until: #{Time.parse(cert['valid_until']).utc.strftime("%FT%TZ")}"
+      if cert['alt_names'] && !cert['alt_names'].empty?
+        puts "  alt names:"
+        cert['alt_names'].each do |alt_name|
+          puts "    - #{alt_name}"
+        end
+      end
+      puts "  auto renewable: #{cert['auto_renewable']}"
     end
 
     def execute
       cert = client.get("certificates/#{current_grid}/#{self.subject}")
-      alt_names = cert['alt_names']
 
-      show_yaml(cert['id'],
-        'subject'         => cert['subject'],
-        'valid until'     => Time.parse(cert['valid_until']),
-        'alt names'       => (alt_names && !alt_names.empty?) ? alt_names : nil,
-        'auto renewable'  => cert['auto_renewable'],
-      )
+      show_certificate(cert)
     end
   end
 end
