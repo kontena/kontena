9b66d66a3817169c554582383b8436eb156074a9
Tero Marttila <SpComb@users.noreply.github.com>
2017-11-14 16:49:09 +0200

    Fix CLI certificate show output formatting (#2966)
    * spec and fix certificate show output YAML
    
    * spec without alt_names
    
    * use show_yaml with unpacked fields to elide empty alt_names
    
    * dump timestamps as Time objects
    
    * hand-crafted output

---
+++ cli/spec/kontena/cli/certificates/show_command_spec.rb
@@ -0,0 +1,57 @@
+require 'kontena/cli/certificate/show_command'
+
+describe Kontena::Cli::Certificate::ShowCommand do
+  include ClientHelpers
+  include OutputHelpers
+  include FixturesHelpers
+
+  let(:subject) { described_class.new("") }
+
+  let(:certificate) {
+    {
+      'id' => 'test-grid/test.example.com',
+      'subject' => 'test.example.com',
+      'valid_until' => '2017-12-14T13:34:00.000+00:00',
+      'alt_names' => [],
+      'auto_renewable' => true,
+    }
+  }
+
+  before do
+    allow(client).to receive(:get).with('certificates/test-grid/test.example.com').and_return(certificate)
+  end
+
+  it "outputs the certificate info" do
+    expect{subject.run(['test.example.com'])}.to output_lines([
+      'test-grid/test.example.com:',
+      '  subject: test.example.com',
+      "  valid until: 2017-12-14T13:34:00Z",
+      '  auto renewable: true',
+    ])
+  end
+
+  context 'with certificate alt_names' do
+    let(:certificate) {
+      {
+        'id' => 'test-grid/test.example.com',
+        'subject' => 'test.example.com',
+        'valid_until' => '2017-12-14T13:34:00.000+00:00',
+        'alt_names' => [
+            'test2.example.com',
+        ],
+        'auto_renewable' => true,
+      }
+    }
+
+    it "outputs the certificate info" do
+      expect{subject.run(['test.example.com'])}.to output_lines([
+        'test-grid/test.example.com:',
+        '  subject: test.example.com',
+        "  valid until: 2017-12-14T13:34:00Z",
+        '  alt names:',
+        '    - test2.example.com',
+        '  auto renewable: true',
+      ])
+    end
+  end
+end
