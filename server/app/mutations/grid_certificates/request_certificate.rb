d805b132fd31941c02c7d2e6316fa700b98240dd
Tero Marttila <SpComb@users.noreply.github.com>
2017-11-15 11:38:23 +0200

    Kontena certificate import/export (#2988)
    * simplify and fix certificates api roda routes
    
    * kontena certificate export
    
    * kontena certificate export --certificate --chain --private-key options
    
    * cli certificate fixtures
    
    * kontena certificate import
    
    * e2e specs: fixtures helper
    
    * e2e spec for kontena certificate import/export
    
    * e2e spec for certifiate export audit logging
    
    * fix certificate upsert
    
    * fix certificate import with subjectAltNames
    
    * e2e lb certificate spec
    
    * refactor common certificate request/import mutations code
    
    * clamp usage errors for missing file paths
    
    * server: add API docs for the new certificates API
    
    * clarify create certificate purpose
    
    * simplify cert export api JSON to match import API
    
    * doc server certificate import/export API
    
    * split Kontena::Cli::Certificate::Common#show_certificate
    
    * fix certificate import command output
    
    * Certificates::Import with subject
    
    * change certificate import API to PUT /v1/certificates/:grid/:subject
    
    * CLI certificate import: load cert and determine --subject default
    
    * typofix

--- server/app/mutations/grid_certificates/request_certificate.rb
+++ server/app/mutations/grid_certificates/request_certificate.rb
@@ -86,31 +86,15 @@ module GridCertificates
       csr = Acme::Client::CertificateRequest.new(names: self.domains)
       certificate = le_client.new_certificate(csr)
 
-      certificate_model = self.grid.certificates.find_by(subject: self.domains[0])
-
-      if certificate_model
-        certificate_model.alt_names = self.domains[1..-1]
-        certificate_model.valid_until = certificate.x509.not_after
-        certificate_model.private_key = certificate.request.private_key.to_pem
-        certificate_model.certificate = certificate.to_pem
-        certificate_model.chain = certificate.chain_to_pem
-
-        certificate_model.save
-      else
-        certificate_model = Certificate.create!(
-          grid: self.grid,
-          subject: self.domains[0],
-          alt_names: self.domains[1..-1],
-          valid_until: certificate.x509.not_after,
-          private_key: certificate.request.private_key.to_pem,
-          certificate: certificate.to_pem,
-          chain: certificate.chain_to_pem
-        )
-      end
-
-      refresh_grid_services(certificate_model)
-
-      certificate_model
+      return upsert_certificate(Certificate.new(
+        grid: self.grid,
+        subject: self.domains[0],
+        alt_names: self.domains[1..-1],
+        valid_until: certificate.x509.not_after,
+        private_key: certificate.request.private_key.to_pem,
+        certificate: certificate.to_pem,
+        chain: certificate.chain_to_pem
+      ))
 
     rescue Acme::Client::Error => exc
       error exc
@@ -120,16 +104,5 @@ module GridCertificates
     def le_client
       @le_client ||= acme_client(self.grid)
     end
-
-    ##
-    # @param [Certificate]
-    def refresh_grid_services(certificate)
-      certificate.grid.grid_services.where(:'certificates.subject' => certificate.subject).each do |grid_service|
-        info "force service #{grid_service.to_path} update for updated certificate #{certificate.subject}"
-        grid_service.set(updated_at: Time.now.utc)
-      end
-    end
   end
-
 end
-
