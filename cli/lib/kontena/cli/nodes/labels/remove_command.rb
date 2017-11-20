225d89492d36d24736c3cd3fda5a20b99b2ef787
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-11-20 14:15:26 +0200

    Require --force if some items in node label rm list are missing

--- cli/lib/kontena/cli/nodes/labels/remove_command.rb
+++ cli/lib/kontena/cli/nodes/labels/remove_command.rb
@@ -5,14 +5,28 @@ module Kontena::Cli::Nodes::Labels
     parameter "NODE", "Node name"
     parameter "LABEL ...", "Labels"
 
+    option '--force', :flag, "Do not abort if items in label list are not found"
+
     requires_current_master
     requires_current_master_token
     requires_current_grid
 
     def execute
-      node = client.get("nodes/#{current_grid}/#{self.node}")
-      data = { labels: Array(node['labels']).reject {|label| label_list.include?(label) } }
-      client.put("nodes/#{node['id']}", data)
+      node_data = client.get("nodes/#{current_grid}/#{self.node}")
+
+      node_data['labels'] ||= []
+
+      found_labels = label_list.uniq & node_data['labels']
+
+      if !force? && found_labels.size != label_list.uniq.size
+        missing = label_list - found_labels
+        exit_with_error "Label#{'s' if missing.size > 1} #{pastel.cyan(missing.join(', '))} not found on node #{pastel.cyan(node)}"
+      end
+
+      return nil if found_labels.empty?
+
+      data = { labels: node_data['labels'] - found_labels }
+      client.put("nodes/#{node_data['id']}", data)
     end
   end
 end
