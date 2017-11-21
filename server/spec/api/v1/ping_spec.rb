965c8434bb2c24034512cbe715132bf09a5a285f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-11-21 10:38:36 +0200

    Fix ping api to check that process is healthy (#3036)
    * fix ping to check that process is healthy
    
    * fix pubsub check
    
    * log exception

---
+++ server/spec/api/v1/ping_spec.rb
@@ -0,0 +1,21 @@
+describe '/v1/ping', celluloid: true do
+
+  it 'returns success' do
+    response = get '/v1/ping'
+    expect(response.status).to eq(200)
+  end
+
+  it 'returns error if mongo is down' do
+    expect(Grid).to receive(:count).and_raise(Mongo::Error::SocketError)
+    response = get '/v1/ping'
+    expect(response.status).to eq(500)
+  end
+
+  it 'returns error if pubsub is down' do
+    actor = double(:actor)
+    expect(actor).to receive(:alive?).and_raise(Celluloid::DeadActorError)
+    expect(MongoPubsub).to receive(:actor).and_return(actor)
+    response = get '/v1/ping'
+    expect(response.status).to eq(500)
+  end
+end
