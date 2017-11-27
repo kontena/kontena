308a3a22fc2812be1a043e78f4b5826fa4be9237
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-11-27 10:23:57 +0200

    Some fixes, very wip

--- build/travis/deploy_homebrew.sh
+++ build/travis/deploy_homebrew.sh
@@ -19,19 +19,19 @@ install_hub() {
 }
 
 clone_repositories() {
-  rm -rf brew/homebrew-core 2> /dev/null
-  rm -rf brew/homebrew-kontena 2> /dev/null
+  #rm -rf brew/homebrew-core 2> /dev/null
+  #rm -rf brew/homebrew-kontena 2> /dev/null
 
   # Clone and sync the homebrew-core fork
-  git clone https://github.com/Homebrew/homebrew-core.git homebrew-core
+  git clone git@github.com:Homebrew/homebrew-core.git homebrew-core
   cd homebrew-core
   git checkout master
-  git remote add fork https://github.com/kontena/homebrew-core.git
-  git push -f fork master
+  git remote add fork git@github.com:kontena/homebrew-core.git
+  #git push -f fork master
   cd ..
 
   # Clone the brew tap repository
-  git clone https://github.com/kontena/homebrew-kontena.git homebrew-kontena
+  git clone git@github.com:kontena/homebrew-kontena.git homebrew-kontena
 }
 
 prerelease() {
@@ -42,7 +42,7 @@ prerelease() {
   cd homebrew-kontena/Formula
 
   # Add a devel block, alter the "if build.head?" to also check for build.devel?
-  cat << EOB | ruby $CLI_VERSION $LONG_HASH
+  cat << EOB | ruby - $CLI_VERSION $LONG_HASH
     formula = File.read('kontena.rb')
     devel_block = [
       "  devel do",
@@ -56,15 +56,15 @@ prerelease() {
 
     bottle_block = formula.scan(/^  \w+ do$.+?  end$/m).find { |blk| blk.start_with?('  bottle do') }
     abort "Error parsing kontena.rb, 'bottle do' block not found" if bottle_block.nil?
-    formula.sub(bottle_block, "#{devel_block}\n#{bottle_block}")
-    formula.sub("if build.head?", "if build.head? || build.devel?")
+    formula[bottle_block] = "#{devel_block}\n#{bottle_block}"
+    formula["if build.head?"] = "if build.head? || build.devel?"
     File.write('kontena.rb', formula)
 EOB
 
   git add kontena.rb
   git commit -m "Devel $CLI_VERSION"
   git tag $CLI_VERSION
-  git push --tags origin master
+  #git push --tags origin master
 }
 
 stable() {
@@ -86,16 +86,17 @@ EOB
   cp $CWD/../../cli/lib/kontena/cli/version.rb tmp/lib/kontena/cli
 
   cd tmp
-  gem install bundler
+  gem install --no-document bundler
 
   # Install all the gems into gems/
   bundle install --standalone --path gems
 
+  echo "Doing resources ..."
+
   # Scan the gems dir, use rubygems to figure out the shasums
   cat << EOB | ruby > resources.rb
     require 'open-uri'
-    resources = {}
-    Dir["gems/**"].select { |p| File.directory?(p) && p !~ /extensions/ }.each do |path|
+    Dir["gems/ruby/*/gems/*"].select { |p| File.directory?(p) }.each do |path|
       gem_name = File.basename(path)[/^(.+?)\-\d+\./, 1]
       gem_version = File.basename(path)[/\-(\d+\..*)/, 1]
       warn "Could not figure out a gem name for #{path}" unless gem_name
@@ -111,49 +112,53 @@ EOB
     end
 EOB
 
+  echo "Done"
+
   cd ..
 
+  echo "Modifying tap formula .."
   # Modify the tap formula. Remove now outdated "bottle do" block
   # and update the "stable do" block with current version and
   # resource sha's.
-  cat << EOB | ruby $CLI_VERSION $LONG_HASH
+  cat << EOB | ruby - $CLI_VERSION $LONG_HASH
     formula = File.read('kontena.rb')
     new_stable_block = [
       "  stable do",
       "    url \"https://github.com/kontena/kontena.git\",",
-      "        :tag => \"v#{ARGV[0}]\",",
-      "        :revision => \"#{ARGV[1}]\"",
+      "        :tag => \"v#{ARGV[0]}\",",
+      "        :revision => \"#{ARGV[1]}\"",
     ].join("\n")
-    new_stable_block += File.read('tmp/resources.rb')
+    new_stable_block += "\n" + File.read('tmp/resources.rb')
     new_stable_block += "  end\n"
 
     stable_block = formula.scan(/^  \w+ do$.+?  end$/m).find { |blk| blk.start_with?('  stable do') }
     bottle_block = formula.scan(/^  \w+ do$.+?  end$/m).find { |blk| blk.start_with?('  bottle do') }
     abort "Error parsing kontena.rb, 'stable do' block not found" if stable_block.nil?
     if bottle_block
-      formula.sub(bottle_block, "")
+      formula[bottle_block] = ""
     else
       warn "There was no 'bottle do' block in the formula."
     end
-    formula.sub(stable_block, new_stable_block)
+    formula[stable_block] = new_stable_block
     File.write('kontena.rb', formula)
 EOB
+  echo "Done"
   git add kontena.rb
-  git commit -m "Stable #{ARGV[0]}"
-  git tag $CLI_VERSION
-  git push --tags origin master
+  #git commit -m "Stable $CLI_VERSION"
+  #git tag $CLI_VERSION
+  #git push --tags origin master
   cd ../..
 
   # Do the same thing for the homebrew-core fork
   cd homebrew-core
   cd Formula
-  cat << EOB | ruby $CLI_VERSION $LONG_HASH
+  cat << EOB | ruby - $CLI_VERSION $LONG_HASH
     formula = File.read('kontena.rb')
     new_stable_block = [
       "  stable do",
       "    url \"https://github.com/kontena/kontena.git\",",
-      "        :tag => \"v#{ARGV[0}]\",",
-      "        :revision => \"#{ARGV[1}]\"",
+      "        :tag => \"v#{ARGV[0]}\",",
+      "        :revision => \"#{ARGV[1]}\"",
     ].join("\n")
     new_stable_block += File.read('../../homebrew-kontena/Formula/tmp/resources.rb')
     new_stable_block += "  end\n"
@@ -165,12 +170,12 @@ EOB
 EOB
   git checkout -b kontena-cli-$CLI_VERSION
   git add kontena.rb
-  git commit -m "kontena $CLI_VERSION"
-  git push fork kontena-cli-$CLI_VERSION
+  #git commit -m "kontena $CLI_VERSION"
+  #git push fork kontena-cli-$CLI_VERSION
 
   # use Hub to submit a PR
-  install_hub || (echo "Failed to install hub"; exit 1)
-  hub pull-request -m "Automated PR from [travis-$TRAVIS_JOB_ID](https://travis-ci.org/kontena/kontena/jobs/$TRAVIS_JOB_ID) generated by [build/travis/deploy_homebrew.sh](https://github.com/kontena/kontena/blob/master/build/travis/deploy_homebrew.sh)"
+  #install_hub || (echo "Failed to install hub"; exit 1)
+  #hub pull-request -m "Automated PR from [travis-$TRAVIS_JOB_ID](https://travis-ci.org/kontena/kontena/jobs/$TRAVIS_JOB_ID) generated by [build/travis/deploy_homebrew.sh](https://github.com/kontena/kontena/blob/master/build/travis/deploy_homebrew.sh)"
 }
 
 CWD=$(pwd)
@@ -180,7 +185,7 @@ LONG_HASH=$(git rev-parse HEAD)
 
 echo "Creating a homebrew Formula for $CLI_VERSION @ $SHORT_HASH .."
 
-rm -rf brew 2> /dev/null
+#rm -rf brew 2> /dev/null
 mkdir brew 2> /dev/null
 cd brew || (echo "Can't create workdir"; exit 1)
 
