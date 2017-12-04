9fdbfec2db4ae3a6e364d57579f6826780dd1481
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-12-04 16:08:01 +0200

    Super does not return

--- cli/lib/kontena/cli/cloud_command.rb
+++ cli/lib/kontena/cli/cloud_command.rb
@@ -7,7 +7,7 @@ class Kontena::Cli::CloudCommand < Kontena::Command
   subcommand "master", "Master specific commands", load_subcommand('cloud/master_command')
 
   def subcommand_missing(name)
-    super unless %w(platform node org organization image-repository ir region token).include?(name)
+    return super(name) unless %w(platform node org organization image-repository ir region token).include?(name)
     exit_with_error "The #{pastel.cyan('cloud')} plugin has not been installed. Use: #{pastel.cyan('kontena plugin install cloud')}"
   end
 end
