a7ad8b56a602a51f00f4e6101bb79b74b91c81cb
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-12-04 16:05:31 +0200

    Suggest plugin install when using known cloud plugin subcommands

--- cli/lib/kontena/cli/cloud_command.rb
+++ cli/lib/kontena/cli/cloud_command.rb
@@ -1,9 +1,13 @@
 
 class Kontena::Cli::CloudCommand < Kontena::Command
+  include Kontena::Cli::Common
+
   subcommand "login", "Authenticate to Kontena Cloud", load_subcommand('cloud/login_command')
   subcommand "logout", "Logout from Kontena Cloud", load_subcommand('cloud/logout_command')
   subcommand "master", "Master specific commands", load_subcommand('cloud/master_command')
 
-  def execute
+  def subcommand_missing(name)
+    super unless %w(platform node org organization image-repository ir region token).include?(name)
+    exit_with_error "The #{pastel.cyan('cloud')} plugin has not been installed. Use: #{pastel.cyan('kontena plugin install cloud')}"
   end
 end
