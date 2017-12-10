aece0918369afcfc49bf46115788b933765b5558
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-12-10 11:34:00 +0200

    less shuffle when nodes missing with daemon strategy

--- server/spec/services/scheduler/strategy/daemon_spec.rb
+++ server/spec/services/scheduler/strategy/daemon_spec.rb
@@ -4,24 +4,114 @@ describe Scheduler::Strategy::Daemon do
   let(:grid) { Grid.create(name: 'test') }
 
   let(:host_nodes) do
-    [
+    10.times.map do |i|
+      n = i + 1
       HostNode.create!(
-      node_id: 'node1', name: 'node-1', connected: true, grid: grid, node_number: 1
-      ),
-      HostNode.create!(
-        node_id: 'node2', name: 'node-2', connected: true, grid: grid, node_number: 2
-      ),
-      HostNode.create!(
-        node_id: 'node3', name: 'node-3', connected: true, grid: grid, node_number: 3
-      ),
-    ]
+        node_id: "node#{n}", name: "node-#{n}", connected: true, grid: grid, node_number: n
+      )
+    end
   end
+
   let(:scheduler_nodes) do
     host_nodes.map{|n| Scheduler::Node.new(n)}
   end
 
   let(:stateless_service) do
     GridService.create!(name: 'test', grid: grid, image_name: 'foo/bar:latest', stateful: false)
+  end
+
+  describe '#find_node' do
+    it 'finds matching nodes' do
+      nodes = scheduler_nodes
+      nodes.each do |n|
+        node = subject.find_node(stateless_service, n.node_number, nodes)
+        node.schedule_counter += 1
+        expect(node).to eq(nodes[n.node_number - 1])
+      end
+    end
+
+    context 'with existing nodes' do
+      before(:each) do
+        host_nodes.each do |n|
+          stateless_service.grid_service_instances.create!(
+            host_node: n,
+            instance_number: n.node_number
+          )
+        end
+      end
+
+      it 'minimizes shuffle when nodes 2 & 3 are missing' do
+        nodes = scheduler_nodes.tap do |n|
+          n.delete_at(1)
+          n.delete_at(1)
+        end
+
+        scheduled = []
+        nodes.each_with_index do |n, i|
+          node = subject.find_node(stateless_service, i + 1, nodes)
+          node.schedule_counter += 1
+          scheduled << node
+        end
+        expect(scheduled.map {|s| s.node_number}).to eq([
+          1, 10, 9, 4, 5, 6, 7, 8
+        ])
+      end
+
+      it 'minimizes shuffle when nodes 2 & 3 are missing (2 instances per node)' do
+        nodes = scheduler_nodes.tap do |n|
+          n.delete_at(1)
+          n.delete_at(1)
+        end
+
+        scheduled = []
+        16.times do |i| # 2 instances per node
+          node = subject.find_node(stateless_service, i + 1, nodes)
+          node.schedule_counter += 1
+          scheduled << node
+        end
+        expect(scheduled.map {|s| s.node_number}).to eq([
+          1, 10, 9, 4, 5, 6, 7, 8,
+          9, 10, 1, 4, 5, 6, 7, 8
+        ])
+      end
+
+      it 'minimizes shuffle when last two nodes are missing' do
+        nodes = scheduler_nodes[0..-3]
+        scheduled = []
+        nodes.each_with_index do |n, i|
+          node = subject.find_node(stateless_service, i + 1, nodes)
+          node.schedule_counter += 1
+          scheduled << node
+        end
+        expect(scheduled.map {|s| s.node_number}).to eq([
+          1, 2, 3, 4, 5, 6, 7, 8
+        ])
+      end
+    end
+
+    context 'with newly added nodes' do
+      before(:each) do
+        host_nodes[0..-3].each do |n|
+          stateless_service.grid_service_instances.create!(
+            host_node: n,
+            instance_number: n.node_number
+          )
+        end
+      end
+
+      it 'finds correct nodes' do
+        nodes = scheduler_nodes
+        scheduled = []
+        nodes.each_with_index do |n, i|
+          node = subject.find_node(stateless_service, i + 1, nodes)
+          node.schedule_counter += 1
+          scheduled << node
+        end
+        expect(scheduled.map { |s| s.node_number}).to eq([
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ])
+      end
+    end
   end
 
   describe '#instance_count' do
