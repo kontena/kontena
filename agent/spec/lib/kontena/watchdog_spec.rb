94d79d91e917fcaa1fc67622d1697562e9438865
Tero Marttila <tero.marttila@kontena.io>
2017-12-13 17:43:36 +0200

    refactor spec

--- agent/spec/lib/kontena/watchdog_spec.rb
+++ agent/spec/lib/kontena/watchdog_spec.rb
@@ -1,48 +1,50 @@
 describe Kontena::Watchdog, :celluloid => true do
-  let(:context) { double() }
-
   subject do
-    @watchdog = described_class.watch(interval: 0.01, threshold: 0.05, timeout: 0.1, abort_exit: false) do
-      context.ping
-    end
+    @watchdog = described_class.new(block, interval: 0.01, threshold: 0.05, timeout: 0.1, abort_exit: false)
   end
 
   after do
     @watchdog.terminate if @watchdog.alive?
   end
 
-  it "does nothing if the watchdog block is okay" do
-    allow(context).to receive(:ping) do
-      nil
+  context "with a block that is okay" do
+    let(:block) { Proc.new do
+      true
+    end}
+
+    it "does nothing" do
+      expect(subject.wrapped_object).to_not receive(:abort)
+
+      subject
+      sleep 0.2
     end
-
-    expect(subject.wrapped_object).to_not receive(:abort)
-
-    subject
-    sleep 0.2
   end
 
-  it "aborts the thread if the watchdog block raises" do
-    allow(context).to receive(:ping) do
+  context "with a block that raises" do
+    let(:block) { Proc.new do
       raise RuntimeError, 'testing'
+    end}
+
+    it "abort" do
+      expect(subject.wrapped_object).to receive(:abort).with(RuntimeError).and_call_original
+
+      subject
+      sleep 0.2
     end
-
-    expect(subject.wrapped_object).to receive(:abort).with(RuntimeError).and_call_original
-
-    subject
-    sleep 0.2
   end
 
-  it "aborts the thread if the watchdog block timeouts" do
-    allow(context).to receive(:ping) do
+  context "with a block that times out" do
+    let(:block) { Proc.new do
       sleep 1
+    end}
+
+    it "aborts" do
+      expect(subject.wrapped_object).to receive(:bark).at_least(:once).and_call_original
+      expect(subject.wrapped_object).to receive(:bite).once.and_call_original
+      expect(subject.wrapped_object).to receive(:abort).with(Timeout::Error).and_call_original
+
+      subject
+      sleep 0.2
     end
-
-    expect(subject.wrapped_object).to receive(:bark).at_least(:once).and_call_original
-    expect(subject.wrapped_object).to receive(:bite).once.and_call_original
-    expect(subject.wrapped_object).to receive(:abort).with(Timeout::Error).and_call_original
-
-    subject
-    sleep 0.2
   end
 end
