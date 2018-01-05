17e74688117d57419071bc4b269419dc72feec79
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-01-05 08:59:55 +0200

    try harder

--- build/travis/test_e2e.sh
+++ build/travis/test_e2e.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ue
 
+export PATH="$(pwd)/vendor/bundle/ruby/2.4.0/bin/:${PATH}"
+
+gem build cli/kontena-cli.gemspec && \
+  gem install --no-ri --no-rdoc *.gem
+
 cd test && \
-  bundle install --system && \
+  bundle install --path vendor/bundle && \
   rake compose:setup && \
   rake
