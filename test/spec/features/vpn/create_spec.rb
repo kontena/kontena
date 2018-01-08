de5d81bad12b7217241de05aac6d24e91ab98d0d
Tero Marttila <tero.marttila@kontena.io>
2018-01-08 15:23:11 +0000

    cleanup create spec

--- test/spec/features/vpn/create_spec.rb
+++ test/spec/features/vpn/create_spec.rb
@@ -7,14 +7,12 @@ describe 'vpn create' do
   end
 
   after(:each) do
-    run 'kontena stack rm --force vpn'
+    run! 'kontena stack rm --force vpn'
   end
 
   it 'creates a vpn stack' do
-    k = run 'kontena vpn create'
-    expect(k.code).to eq(0)
-    k = run 'kontena stack show vpn'
+    k = run! 'kontena vpn create'
+    k = run! 'kontena stack show vpn'
     expect(k.out.match(/state: running/))
-    run 'kontena stack rm --force vpn'
   end
 end
