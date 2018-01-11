fc0abf96b852a1795aa2301a1f8d273905d975b8
Tero Marttila <tero.marttila@kontena.io>
2018-01-11 11:40:57 +0200

    server: GridDomainAuthorizations::Authorize support for http-01

--- server/spec/mutations/domain_authorizations/authorize_spec.rb
+++ server/spec/mutations/domain_authorizations/authorize_spec.rb
@@ -81,6 +81,61 @@ describe GridDomainAuthorizations::Authorize do
         end
       end
     end
+
+    describe 'authorization_type=http-01' do
+      it 'fails without a linked service' do
+        outcome = described_class.validate(grid: grid, domain: 'example.com',
+          authorization_type: 'http-01',
+        )
+
+        expect(outcome).not_to be_success
+        expect(outcome.errors.message).to eq 'linked_service' => "Service link needs to be given for http-01 authorization type"
+      end
+
+      context 'for a linked service without exposed ports' do
+        let!(:grid_service) { GridService.create(grid: grid, name: 'web', image_name: 'web:latest') }
+
+        it 'fails validation with missing port 80' do
+          outcome = described_class.validate(grid: grid, domain: 'example.com',
+            authorization_type: 'http-01',
+            linked_service: 'null/web',
+          )
+
+          expect(outcome).not_to be_success
+          expect(outcome.errors.message).to eq 'linked_service' => "Linked service does not have port 80 open"
+        end
+      end
+
+      context 'for a linked service with network_mode=host' do
+        let!(:grid_service) { GridService.create(grid: grid, name: 'web', image_name: 'web:latest',
+          net: 'host',
+        ) }
+
+        it 'passes validation' do
+          outcome = described_class.validate(grid: grid, domain: 'example.com',
+            authorization_type: 'http-01',
+            linked_service: 'null/web',
+          )
+
+          expect(outcome).to be_success
+        end
+      end
+
+      context 'for a linked service exposing port 80' do
+        let!(:grid_service) { GridService.create(grid: grid, name: 'web', image_name: 'web:latest',
+          ports: [ { 'node_port' => 80 } ],
+        ) }
+
+        it 'passes validation' do
+          outcome = described_class.validate(grid: grid, domain: 'example.com',
+            authorization_type: 'http-01',
+            linked_service: 'null/web',
+          )
+
+          expect(outcome).to be_success
+        end
+      end
+    end
   end
 
   let(:acme_client) { instance_double(Acme::Client) }
@@ -149,6 +204,63 @@ describe GridDomainAuthorizations::Authorize do
         expect {
           expect(outcome = subject.run).to be_success
         }.to change{GridDomainAuthorization.find_by(domain: 'example.com')}.from(authz)
+      end
+    end
+  end
+
+  describe 'authorization_type=http-01' do
+    let(:challenge_token) { 'LoqXcYV8q5ONbJQxbmR7SCTNo3tiAXDfowyjxAjEuX0' }
+    let(:challenge_content) { 'LoqXcYV8q5ONbJQxbmR7SCTNo3tiAXDfowyjxAjEuX0.9jg46WB3rR_AHD-EBXdN7cBkH1WOu0tA3M9fm21mqTI' }
+    let(:expires_at) { Time.now + 300 }
+    let(:acme_authorization) { double(
+      status: 'pending',
+      expires: expires_at,
+      http01: double(
+        token: challenge_token,
+        file_content: challenge_content,
+        to_h: {}
+      ),
+    ) }
+
+    context 'with a linked service' do
+      let(:linked_service) { GridService.create(grid: grid, name: 'lb', image_name: 'kontena/lb:latest',
+        ports: [ { 'node_port' => 80 } ],
+      ) }
+
+      subject {
+        linked_service
+        described_class.new(grid: grid, domain: 'example.com',
+          authorization_type: 'http-01',
+          linked_service: 'null/lb',
+        )
+      }
+
+      it 'requests domain authorization and creates model' do
+        expect(acme_client).to receive(:authorize).with(domain: 'example.com').and_return(acme_authorization)
+
+        expect(outcome = subject.run).to be_success
+
+        authz = GridDomainAuthorization.find_by(domain: 'example.com')
+
+        expect(authz).to_not be_nil
+        expect(authz).to eq outcome.result
+        expect(authz.domain).to eq 'example.com'
+        expect(authz.authorization_type).to eq 'http-01'
+        expect(authz.challenge_opts).to eq(
+          'token' => challenge_token,
+          'content' => challenge_content,
+        )
+      end
+
+      it 'fails if LE does not offer a http-01 challenge' do
+        expect(acme_client).to receive(:authorize).with(domain: 'example.com').and_return(double(http01: nil))
+
+        expect {
+          outcome = subject.run
+
+          expect(outcome).to_not be_success
+          expect(outcome.errors.message).to eq 'challenge' => "LE did not offer any http-01 challenge"
+        }.not_to change{GridDomainAuthorization.count}
       end
     end
   end
