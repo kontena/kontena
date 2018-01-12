56794913a1df3fb6fb7697a11fa09c98411b3e98
Lauri Nevala <lauri.nevala@gmail.com>
2018-01-12 09:08:31 +0200

    Return only master admins when grid is not found

--- server/spec/services/cloud/websocket_client_spec.rb
+++ server/spec/services/cloud/websocket_client_spec.rb
@@ -327,5 +327,67 @@ describe Cloud::WebsocketClient, :celluloid => true do
         end
       end
     end
+
+    describe '#resolve_users' do
+      let(:grid) do
+        grid = Grid.create!(name: 'test')
+        grid.users << john
+        grid
+      end
+
+      let(:master_admin_role) do
+        Role.create!(name: 'master_admin', description: 'Master admin')
+      end
+
+      let(:john) do
+        john = User.create(email: 'john.doe@example.org', external_id: '12345')
+        john.roles << master_admin_role
+        john
+      end
+
+      let(:jane) do
+        jane = User.create(email: 'jane.doe@example.org', external_id: '67890')
+        grid.users << jane
+        jane
+      end
+
+      context 'when passing grid_id' do
+        before(:each) do
+          grid
+          john
+          jane
+        end
+
+        context 'when grid is found' do
+          it 'returns master admins and grid users' do
+            users = subject.resolve_users(grid.name)
+            expect(users).to eq([john.external_id, jane.external_id])
+          end
+        end
+
+        context 'when grid is not found' do
+          it 'returns master admins' do
+            grid.destroy
+            users = subject.resolve_users(grid.name)
+            expect(users).to eq([john.external_id])
+          end
+        end
+      end
+
+      context 'when grid_id nil' do
+        before(:each) do
+          grid
+          john
+          jane
+        end
+
+
+        it 'returns master admins' do
+          grid.destroy
+          users = subject.resolve_users(nil)
+          expect(users).to eq([john.external_id])
+        end
+      end
+    end
   end
 end
