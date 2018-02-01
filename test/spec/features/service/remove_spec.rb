bdb08f95f7bf2240dc0eb18e750b5c115fe71318
Tero Marttila <tero.marttila@kontena.io>
2018-02-01 11:08:15 +0000

    fix service, vault, volume remove specs to not use imaginary syntax

--- test/spec/features/service/remove_spec.rb
+++ test/spec/features/service/remove_spec.rb
@@ -1,19 +1,19 @@
 describe 'service remove' do
 
     after(:each) do
-      run 'kontena service rm --force $(kontena service ls -q)'
+      run 'kontena service rm --force rm-test'
+      run 'kontena service rm --force rm-test-1'
+      run 'kontena service rm --force rm-test-2'
     end
 
     it 'removes a service' do
-      run 'kontena service create rm-test redis:3-alpine'
-      k = run 'kontena service rm --force rm-test'
-      expect(k.code).to eq(0)
+      run! 'kontena service create rm-test redis:3-alpine'
+      run! 'kontena service rm --force rm-test'
     end
 
     it 'removes multiple services' do
-      run 'kontena service create rm-test-1 redis:3-alpine'
-      run 'kontena service create rm-test-2 redis:3-alpine'
-      k = run 'kontena service rm --force rm-test-1 rm-test-2'
-      expect(k.code).to eq(0)
+      run! 'kontena service create rm-test-1 redis:3-alpine'
+      run! 'kontena service create rm-test-2 redis:3-alpine'
+      run! 'kontena service rm --force rm-test-1 rm-test-2'
     end
   end
