1edb01d1d2f4841654872e438f4b5f6ec3ba857d
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-01 15:52:32 +0200

    Run forrest run

--- test/spec/features/master/token/remove_spec.rb
+++ test/spec/features/master/token/remove_spec.rb
@@ -4,7 +4,7 @@ describe 'token remove' do
   it ' creates a token with description' do
     k = run!('kontena master token create --id')
     token_id = k.out.strip
-    k = run('kontena master token remove %s' % token_id)
+    run!('kontena master token remove %s' % token_id)
     k = run('kontena master token list -q')
     expect(k.out.lines).not_to include token_id
   end
