0a92836bd574219c63db2ae812f5a5192b88709b
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-01 11:21:37 +0200

    Not possible in Kommando to use quoted params?

--- test/spec/features/master/token/create_spec.rb
+++ test/spec/features/master/token/create_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'token create' do
   it ' creates a token with description' do
-    k = run!('kontena master token create --description "token description test --id')
+    k = run!('kontena master token create --description tokendescriptiontest --id')
     token_id = k.out.strip
     k = run('kontena master token list')
-    expect(k.out).to match /token description test/
+    expect(k.out).to match /tokendescriptiontest/
     k = run('kontena master token show %s' % token_id)
-    expect(k.out).to match /description: token description test/
+    expect(k.out).to match /description: tokendescriptiontest/
   end
 end
