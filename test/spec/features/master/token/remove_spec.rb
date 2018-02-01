e0b8a803433517e52b49ccfeb9b8e15555138357
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-01 15:56:32 +0200

    May the --force be with token rm

--- test/spec/features/master/token/remove_spec.rb
+++ test/spec/features/master/token/remove_spec.rb
@@ -4,7 +4,7 @@ describe 'token remove' do
   it ' creates a token with description' do
     k = run!('kontena master token create --id')
     token_id = k.out.strip
-    run!('kontena master token remove %s' % token_id)
+    run!('kontena master token remove --force %s' % token_id)
     k = run('kontena master token list -q')
     expect(k.out.lines).not_to include token_id
   end
