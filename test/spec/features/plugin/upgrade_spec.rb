bc2822ba26a5ea505e4c7d83ca6dfe04f5d78ba2
Tero Marttila <tero.marttila@kontena.io>
2018-02-01 11:23:13 +0000

    run! all the things

--- test/spec/features/plugin/upgrade_spec.rb
+++ test/spec/features/plugin/upgrade_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe 'plugin upgrade' do
-  after(:each) do
-    run('kontena plugin uninstall aws')
-    run('kontena plugin uninstall digitalocean')
-  end
+  context "with older versions of the aws, digitalocean plugins installed" do
+    before(:each) do
+      run! 'kontena plugin uninstall aws'
+      run! 'kontena plugin uninstall digitalocean'
+      run! 'kontena plugin install --version 0.2.7 aws'
+      run! 'kontena plugin install --version 0.2.5 digitalocean'
+    end
 
-  before(:each) do
-    run('kontena plugin uninstall aws')
-    run('kontena plugin uninstall digitalocean')
-    run('kontena plugin install --version 0.2.7 aws')
-    run('kontena plugin install --version 0.2.5 digitalocean')
-  end
+    after(:each) do
+      run! 'kontena plugin uninstall aws'
+      run! 'kontena plugin uninstall digitalocean'
+    end
 
-  it 'upgrades all plugins' do
-    k = run!('kontena plugin upgrade')
-    expect(k.out).to match(/aws/)
-    expect(k.out).to match(/digitalocean/)
-    k = run('kontena plugin list')
-    lines = k.out.split(/[\r\n]/)
-    lines.each do |line|
-      plugin, version, _ = line.split(/\s+/, 3)
-      if plugin == 'aws'
-        expect(Gem::Version.new(version) > Gem::Version.new("0.2.7")).to be_truthy
-      elsif plugin == 'digitalocean'
-        expect(Gem::Version.new(version) > Gem::Version.new("0.2.5")).to be_truthy
+    it 'upgrades all plugins' do
+      k = run!('kontena plugin upgrade')
+      expect(k.out).to match(/aws/)
+      expect(k.out).to match(/digitalocean/)
+      k = run('kontena plugin list')
+      lines = k.out.split(/[\r\n]/)
+      lines.each do |line|
+        plugin, version, _ = line.split(/\s+/, 3)
+        if plugin == 'aws'
+          expect(Gem::Version.new(version) > Gem::Version.new("0.2.7")).to be_truthy
+        elsif plugin == 'digitalocean'
+          expect(Gem::Version.new(version) > Gem::Version.new("0.2.5")).to be_truthy
+        end
       end
     end
   end
