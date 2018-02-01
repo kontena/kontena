b5bb28625c25a6ba264a12d1eb530da6257a1790
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-01 15:36:41 +0200

    Add e2e

--- test/spec/features/plugin/install_spec.rb
+++ test/spec/features/plugin/install_spec.rb
@@ -6,19 +6,22 @@ describe 'plugin install' do
   end
 
   it 'installs a plugin' do
-    k = run('kontena plugin install aws')
-    expect(k.code).to eq(0)
+    k = run!('kontena plugin install aws')
     k = run('kontena plugin ls')
     expect(k.out).to match(/aws/)
   end
 
-  it 'attempts to upgrade a plugin' do
-    k = run('kontena plugin install aws')
-    expect(k.code).to eq(0)
-    k = run('kontena plugin ls')
+  it 'installs a plugin from a URL' do
+    k = run!('kontena plugin install https://rubygems.org/downloads/kontena-plugin-aws-0.3.1.gem')
+    k = run!('kontena plugin ls')
     expect(k.out).to match(/aws/)
-    k = run('kontena plugin install aws')
-    expect(k.code).to eq(0)
+  end
+
+  it 'attempts to upgrade a plugin' do
+    k = run!('kontena plugin install aws')
+    k = run!('kontena plugin ls')
+    expect(k.out).to match(/aws/)
+    k = run!('kontena plugin install aws')
     expect(k.out).to match(/Upgrad/)
   end
 end
