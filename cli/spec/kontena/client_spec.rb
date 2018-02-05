f17eeac426690c15c90e7ec87a19c5f8717bc354
Tero Marttila <tero.marttila@kontena.io>
2018-02-05 11:22:36 +0200

    spec

--- cli/spec/kontena/client_spec.rb
+++ cli/spec/kontena/client_spec.rb
@@ -233,6 +233,25 @@ describe Kontena::Client do
       end
     end
 
+    context "with invalid response JSON" do
+      before :each do
+        allow(subject).to receive(:http_client).and_call_original
+
+        WebMock.stub_request(:any, 'http://localhost/v1/test').to_return(
+          status: 200,
+          headers: {
+            'Content-Type' => 'application/json',
+          },
+          body: 'this is not valid JSON',
+        )
+      end
+
+      it "raises Kontena::Errors::StandardError with 520" do
+        # XXX: for some reason the response.path is empty?
+        expect{subject.get('test')}.to raise_error(Kontena::Errors::StandardError, /Invalid response JSON from server for : JSON::ParserError/)
+      end
+    end
+
     context "with an empty error response" do
       before :each do
         # workaround https://github.com/bblimke/webmock/issues/653
