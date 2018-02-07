6476fb3b6544e90c747f65e85fcd532f6309e0de
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-07 17:26:01 +0200

    Improve show formatting

--- cli/lib/kontena/cli/stacks/registry/show_command.rb
+++ cli/lib/kontena/cli/stacks/registry/show_command.rb
@@ -16,12 +16,37 @@ module Kontena::Cli::Stacks::Registry
       unless versions?
         data = stacks_client.show(stack_name).dig('data', 'attributes')
         puts "#{data['organization-id']}/#{data['name']}:"
-        puts "  description: #{data.dig('latest-version', 'description')}"
+        puts "  description: #{data.dig('latest-version', 'description') || '-'}"
         puts "  latest_version: #{data.dig('latest-version', 'version') || '-'}"
         puts "  created_at: #{data.dig('created-at')}"
         puts "  pulls: #{data.dig('pulls')}"
         puts "  private: #{data.dig('is-private')}"
-        puts "  available_versions:"
+        print "  meta:"
+        meta = data.dig('latest-version', 'meta')
+        if meta
+          puts
+          readme = meta.delete('readme')
+          meta_lines = YAML.dump(meta).split(/[\r\n]/)
+          meta_lines.shift
+          meta_lines.each do |meta_line|
+            puts "    %s" % meta_line
+          end
+          if readme
+            if readme =~ /^http\S+$/
+              puts "    readme: readme"
+            else
+              puts "    readme: |"
+              readme.gsub!(/(\S{#{70}})(?=\S)/, '\1 ')
+              readme.gsub!(/(.{1,#{70}})(?:\s+|$)/, "\\1\n")
+              readme.gsub!(/^/, '      ')
+              puts readme
+            end
+          end
+        else
+          puts "-"
+        end
+
+        puts "  versions:"
       end
 
       stacks_client.versions(stack_name).each do |version|
