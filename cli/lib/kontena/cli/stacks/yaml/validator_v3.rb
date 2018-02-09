c268c24e8ab0830211e72f0da58e5c05f97556e5
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-09 11:00:11 +0200

    Change regexp to disallow servicenames starting with minus

--- cli/lib/kontena/cli/stacks/yaml/validator_v3.rb
+++ cli/lib/kontena/cli/stacks/yaml/validator_v3.rb
@@ -69,8 +69,8 @@ module Kontena::Cli::Stacks
         if yaml.key?('services')
           if yaml['services'].kind_of?(Hash)
             yaml['services'].each do |service, options|
-              unless service =~ /\A[a-z0-9\-]+\z/
-                result[:notifications] << { 'services' => { service => { 'name' => 'A service name should only include a-z, 0-9 and - characters' } } }
+              unless service =~ /\A(?!-)[a-z0-9\-]+\z/
+                result[:notifications] << { 'services' => { service => { 'name' => 'A service name should only include a-z, 0-9 and - characters and not start with the - character' } } }
               end
               unless options.kind_of?(Hash)
                 result[:errors] << { 'services' => { service => { 'options' => "must be a mapping not a #{options.class}"}  } }
