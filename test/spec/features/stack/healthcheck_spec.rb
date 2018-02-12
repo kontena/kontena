23af5af964e1e5ac24d2ea28b58787a9b55c25de
Tero Marttila <tero.marttila@kontena.io>
2018-02-12 15:03:28 +0000

    workaround async stack remove causing container name conflicts

--- test/spec/features/stack/healthcheck_spec.rb
+++ test/spec/features/stack/healthcheck_spec.rb
@@ -6,18 +6,20 @@ describe 'kontena service health_check' do
         run! 'kontena stack build --no-push'
       end
     end
+  
+    let(:stack_name) { 'healthcheck-test' }
 
     before(:each) do
       with_fixture_dir('stack/healthcheck') do
-        run! "kontena stack install -v health_status=#{health_status}"
+        run! "kontena stack install -n #{stack_name} -v health_status=#{health_status}"
       end
     end
 
     after(:each) do
-      run! 'kontena stack rm --force healthcheck'
+      run! "kontena stack rm --force #{stack_name}"
     end
 
-    def check_service_health(service)
+    def check_service_health(service = "#{stack_name}/server")
       out = ''
 
       until match = out.match(/health: (\w+)/)
@@ -30,18 +32,20 @@ describe 'kontena service health_check' do
     end
 
     context "returning HTTP status 200 for healthchecks" do
+      let(:stack_name) { 'healthcheck-test-200' }
       let(:health_status) { 200 }
 
       it "has a healthy status" do
-        expect(check_service_health('healthcheck/server')).to eq 'healthy'
+        expect(check_service_health()).to eq 'healthy'
       end
     end
 
     context "returning HTTP status 500 for healthchecks" do
+      let(:stack_name) { 'healthcheck-test-500' }
       let(:health_status) { 500 }
 
       it "has a healthy status" do
-        expect(check_service_health('healthcheck/server')).to eq 'unhealthy'
+        expect(check_service_health()).to eq 'unhealthy'
       end
     end
 
