a5fabd9e3b99d5b55e446ddabb6775f0ccb96fc2
Tero Marttila <tero.marttila@kontena.io>
2018-02-13 10:46:34 +0200

    server: change service restart to be an async notify

--- server/spec/mutations/grid_services/restart_spec.rb
+++ server/spec/mutations/grid_services/restart_spec.rb
@@ -1,37 +1,49 @@
 describe GridServices::Restart do
-  include AsyncMock
-
   let(:grid) { Grid.create!(name: 'test-grid') }
-  let(:redis_service) { GridService.create(grid: grid, name: 'redis', image_name: 'redis:2.8')}
-  let(:node) { grid.create_node!('node-1') }
+  let(:service) { GridService.create(grid: grid, name: 'redis', image_name: 'redis:2.8') }
 
-  describe '#run' do
-    it 'sends restart' do
-      container = redis_service.containers.create!(
-        name: 'redis-1', container_id: '34', host_node: node, instance_number: 1
-      )
-      subject = described_class.new(grid_service: redis_service)
-      expect(subject).to receive(:restart_service_instance).with(node, container.instance_number).once
-      outcome = subject.run
+  subject { described_class.new(grid_service: service) }
+
+  it 'does not change service state' do
+    expect{
+      subject.run!
+    }.to_not change{service.reload.state}
+  end
+
+  context 'with a service instance' do
+    let!(:service_instance) { GridServiceInstance.create!(grid_service: service, instance_number: 1, host_node: node) }
+
+    context 'without a host node' do
+      let(:node) { nil }
+
+      it 'does not hcnage service instance state' do
+        expect{
+          subject.run!
+        }.to_not change{service_instance.reload.desired_state}
+      end
     end
 
-    it 'sets service state to running' do
-      redis_service.containers.create!(name: 'redis-1', container_id: '34')
-      subject = described_class.new(grid_service: redis_service)
-      allow(subject).to receive(:restart_service_instance)
-      outcome = subject.run
-      expect(redis_service.state).to eq('running')
+    context 'with a host node' do
+      let(:node) { grid.create_node!('node-1') }
+      let(:rpc_client) { instance_double(RpcClient) }
+
+      before do
+        allow(node).to receive(:rpc_client).and_return(rpc_client)
+      end
+
+      it 'notifies service restart' do
+        expect(rpc_client).to receive(:notify).with('/service_pods/restart', service.id.to_s, 1)
+
+        subject.run!
+      end
     end
   end
 
-  describe '#restart_service_instances' do
-    it 'returns service to previous state if exception is raised' do
-      prev_state = redis_service.state
-      redis_service.containers.create!(name: 'redis-1', container_id: '34')
-      subject = described_class.new(grid_service: redis_service)
-      expect(subject).to receive(:restart_service_instance).and_raise(StandardError.new('error'))
-      expect{subject.restart_service_instances}.to raise_exception(StandardError)
-      expect(redis_service.state).to eq(prev_state)
-    end
+  it 'fails on errors' do
+    expect(subject).to receive(:restart_service_instances).and_raise(StandardError.new('test'))
+
+    expect(outcome = subject.run).to_not be_success
+    expect(outcome.errors.message).to eq 'restart' => 'test'
+    expect(outcome.errors.symbolic).to eq 'restart' => :error
   end
 end
--- server/spec/services/docker/service_restarter_spec.rb
+++
@@ -1,3 +0,0 @@
-
-describe Docker::ServiceRestarter do
-end
