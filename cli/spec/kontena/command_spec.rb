a68ef0b8b787e05ffe43b1f7750c0be9da9f7341
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-14 17:42:40 +0200

    Add specs

--- cli/spec/kontena/command_spec.rb
+++ cli/spec/kontena/command_spec.rb
@@ -51,4 +51,79 @@ describe Kontena::Command do
       end
     end
   end
+
+  context 'option placement handling' do
+    subject do
+      Class.new(Kontena::Command) do
+        parameter 'TESTPARAM', 'Test parameter'
+        option '--long-only', 'LONGONLY', 'Option with long only'
+        option ['--long-and-short', '-l'], :flag, 'Flag with short and long', default: false
+
+        def execute
+          { param: testparam, longopt: long_only, shortflag: long_and_short? }
+        end
+      end
+
+    end
+
+    it 'allows using options before parameters' do
+      expect(subject.new('kontena').run(%w(-l --long-only longopt test))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: true
+      )
+      expect(subject.new('kontena').run(%w(--long-only longopt test))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: false
+      )
+      expect(subject.new('kontena').run(%w(--long-and-short test))).to match hash_including(
+        param: 'test', longopt: nil, shortflag: true
+      )
+    end
+
+    it 'allows using options after parameters' do
+      expect(subject.new('kontena').run(%w(test -l --long-only longopt))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: true
+      )
+      expect(subject.new('kontena').run(%w(test --long-only longopt))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: false
+      )
+      expect(subject.new('kontena').run(%w(test --long-and-short))).to match hash_including(
+        param: 'test', longopt: nil, shortflag: true
+      )
+    end
+
+    it 'allows using options mixed with parameters' do
+      expect(subject.new('kontena').run(%w(-l test --long-only longopt))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: true
+      )
+      expect(subject.new('kontena').run(%w(--long-and-short test --long-only longopt))).to match hash_including(
+        param: 'test', longopt: 'longopt', shortflag: true
+      )
+    end
+
+    context 'with allow_options_after_parameters disabled' do
+      subject do
+        Class.new(Kontena::Command) do
+          callback_matcher 'foofoo', 'ssh'
+
+          parameter 'TESTPARAM ...', 'Test parameter'
+          option '--opt', 'OPT', 'Option'
+
+          def execute
+            { param_list: testparam_list, opt: opt }
+          end
+        end
+      end
+
+      it 'does not parse trailing options' do
+        expect(subject.new('kontena').run(%w(--opt hello foo --bar hello))).to match hash_including(
+          param_list: %w(foo --bar hello), opt: 'hello'
+        )
+      end
+
+      it 'parses leading options' do
+        expect(subject.new('kontena').run(%w(--opt hello foo --bar))).to match hash_including(
+          param_list: %w(foo --bar), opt: 'hello'
+        )
+      end
+    end
+  end
 end
