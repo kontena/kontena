e99b4a7c804a0d22f9e848bc4220e595d888042d
Tero Marttila <tero.marttila@kontena.io>
2018-02-14 12:00:39 +0200

    server: change POST v1/services/.../{start,stop} => GridServices::Start/Stop to deploy service in desired state

--- server/app/mutations/grid_services/stop.rb
+++ server/app/mutations/grid_services/stop.rb
@@ -1,29 +1,15 @@
+require_relative 'helpers'
+
 module GridServices
   class Stop < Mutations::Command
+    include Helpers
+
     required do
       model :grid_service
     end
 
     def execute
-      self.grid_service.set_state('stopped')
-      self.stop_service_instances
-    rescue => exc
-      add_error(:stop, :error, exc.message)
-    end
-
-    def stop_service_instances
-      self.grid_service.grid_service_instances.each do |i|
-        i.set(desired_state: 'stopped')
-        notify_service_instance(i, 'stop')
-      end
-    end
-
-    # @param node [HostNode]
-    # @param action [String]
-    def notify_service_instance(service_instance, action)
-      if service_instance.host_node
-        service_instance.host_node.rpc_client.notify('/service_pods/notify_update', action)
-      end
+      deploy_grid_service(grid_service, 'stopped')
     end
   end
 end
