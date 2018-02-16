6f5e6816b5ffb008dc40e19d495cc819f12573b1
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-16 14:42:10 +0200

    Nice typing

--- test/spec/features/stack/registry/pull_spec.rb
+++ test/spec/features/stack/registry/pull_spec.rb
@@ -14,20 +14,20 @@ describe 'stack registry' do
       context 'without version number' do
         it 'displays the YAML' do
           k = run! 'kontena stack registry pull --no-cache kontena/ingress-lb'
-          expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('variables:')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('services:')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('version: 0.1.0')}.to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('variables:')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('services:')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('version: 0.1.0')}).to be_nil
         end
       end
 
       context 'with version number' do
         it 'displays the YAML for that version' do
           k = run! 'kontena stack registry pull --no-cache kontena/ingress-lb:0.1.0'
-          expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('variables:')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('services:')}.not_to be_nil
-          expect(k.out.lines.find { |l| l.start_with?('version: 0.1.0')}.not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('variables:')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('services:')}).not_to be_nil
+          expect(k.out.lines.find { |l| l.start_with?('version: 0.1.0')}).not_to be_nil
         end
       end
     end
