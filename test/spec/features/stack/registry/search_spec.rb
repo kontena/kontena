68635b1471dfd87ece6a656874faa72cad39d94b
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-16 15:23:21 +0200

    Search spec fails?

--- test/spec/features/stack/registry/search_spec.rb
+++ test/spec/features/stack/registry/search_spec.rb
@@ -5,18 +5,18 @@ describe 'stack registry' do
     it 'shows a list of stacks' do
       k = run! 'kontena stack registry search'
       expect(k.out.lines.size > 20).to be_truthy
-      expect(k.out).to match(/kontena\/ingress-lb/)
+      expect(k.out).to match /kontena\/ingress-lb/
     end
 
     it 'shows a list of stacks filtered by name' do
       k = run! 'kontena stack registry search kontena/ingress-lb'
-      expect(k.out).to match(/VERSION/)
-      expect(k.out.lines.find { |l| l.start_with?('kontena/ingress-lb') }).not_to be_nil
+      expect(k.out).to match /VERSION/
+      expect(k.out).to match /kontena\/ingress-lb/
     end
 
     it 'shows a list of stacks filtered by description' do
       k = run! 'kontena stack registry search balancer'
-      expect(k.out.lines.find { |l| l.start_with?('kontena/ingress-lb') }).not_to be_nil
+      expect(k.out).to match /kontena\/ingress-lb/
     end
   end
 end
