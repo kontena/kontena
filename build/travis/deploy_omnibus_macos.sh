2f8cf701b157614ecca315c1ec8f19280b8a76c0
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-02-19 18:20:29 +0200

    travis certs

--- build/travis/deploy_omnibus_macos.sh
+++ build/travis/deploy_omnibus_macos.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -ue
 
+openssl aes-256-cbc -K $encrypted_12d917fc848a_key -iv $encrypted_12d917fc848a_iv -in kontena.p12.enc -out kontena.p12.txt -d
+cat kontena.p12.txt | base64 --decode > kontena.p12
+security create-keychain -p buildpwd build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p buildpwd build.keychain
+security import kontena.p12 -k build.keychain -T /usr/bin/codesign
+
+
 # install github-release
 curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar -xjO > /tmp/github-release
 chmod +x /tmp/github-release
@@ -21,9 +29,9 @@ bundle install
 bundle exec omnibus build kontena --log-level info
 
 # upload kontena pkg to github
-/tmp/github-release upload \
-    --user kontena \
-    --repo kontena \
-    --tag $TRAVIS_TAG \
-    --name "kontena-cli-osx-${TRAVIS_TAG}-amd64.pkg" \
-    --file pkg/kontena-cli-*.pkg
+#/tmp/github-release upload \
+#    --user kontena \
+#    --repo kontena \
+#    --tag $TRAVIS_TAG \
+#    --name "kontena-cli-osx-${TRAVIS_TAG}-amd64.pkg" \
+#    --file pkg/kontena-cli-*.pkg
