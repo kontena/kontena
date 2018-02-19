359c045ba53dc9b9971089dabd0bd03d0ea9eeec
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-19 14:10:16 +0200

    Tweak the gem install command

--- cli/omnibus/config/software/kontena-cli.rb
+++ cli/omnibus/config/software/kontena-cli.rb
@@ -13,7 +13,7 @@ build do
   gem "install rb-readline -v 0.5.4 -N", env: env
   gem "install nokogiri -v 1.8.2 -N", env: env
   gem "build kontena-cli.gemspec", env: env, cwd: File.expand_path('..', Omnibus::Config.project_root)
-  gem "install -N #{Dir[File.expand_path('../kontena-cli-%s.gem' % default_version, Omnibus::Config.project_root)].first}", env: env
+  gem "install -N kontena-cli-%s.gem" % default_version, env: env, cwd: File.expand_path('..', Omnibus::Config.project_root)
   gem "install kontena-plugin-cloud -N", env: env
   copy "sh/kontena", "#{install_dir}/bin/kontena"
 end
