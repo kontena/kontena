7f1e3e8d77b61aa234b9d4d7439e44d515650c7b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-02-20 12:16:44 +0200

    Travis MacOS omnibus cert fix (#3296)

--- build/travis/deploy_omnibus_macos.sh
+++ build/travis/deploy_omnibus_macos.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 set -ue
 
+echo "decrypting kontena.p12.enc ..."
+openssl aes-256-cbc -K $encrypted_12d917fc848a_key -iv $encrypted_12d917fc848a_iv -in ./build/travis/kontena.p12.enc -out kontena.p12.txt -d
+cat kontena.p12.txt | base64 --decode > kontena.p12
+
+echo "creating build.keychain"
+security create-keychain -p buildpwd build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p buildpwd build.keychain
+security set-keychain-settings -t 3600 -u build.keychain
+
+echo "importing kontena.p12 to build.keychain"
+security import kontena.p12 -k build.keychain -P "" -T /usr/bin/productbuild
+security set-key-partition-list -S apple-tool:,apple: -s -k buildpwd build.keychain
+
 # install github-release
 curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar -xjO > /tmp/github-release
 chmod +x /tmp/github-release
