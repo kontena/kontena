2a193fa04b690a4d24237baec490ca658b2eac2d
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-02-22 11:12:10 +0200

    Even more finetunings

--- build/travis/test_e2e.sh
+++ build/travis/test_e2e.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -ue
 
+GEM_HOME=$HOME/.gems
+mkdir -p $GEM_HOME
+
 cd cli && \
   gem build kontena-cli.gemspec && \
-  gem install --no-ri --no-rdoc *.gem && \
+  gem install -N *.gem && \
   kontena -v && \
   cd ..
+
+# Get some usable layers before compose builds
+docker pull kontena/agent:edge
+docker pull kontena/server:edge
+docker pull kontena/cli:edge
 
 cd test && \
   bundle install --system --without development && \
