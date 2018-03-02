f8cac70ca84caa13376f7454383e04127725d864
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-03-02 00:20:57 -0800

    Allow stacks to define their own labels (support for 'kontena stack label remove/rm' command)

--- cli/lib/kontena/cli/stacks/label_command.rb
+++ cli/lib/kontena/cli/stacks/label_command.rb
@@ -1,6 +1,7 @@
 module Kontena::Cli::Stacks
   class LabelCommand < Kontena::Command
     subcommand "add", "Add label to stack", load_subcommand('stacks/labels/add_command')
+    subcommand ["remove", "rm"], "Remove label from stack", load_subcommand('stacks/labels/remove_command')
 
     def execute
     end
---
+++ cli/lib/kontena/cli/stacks/labels/remove_command.rb
@@ -0,0 +1,35 @@
+require_relative '../common'
+
+module Kontena::Cli::Stacks::Labels
+  class RemoveCommand < Kontena::Command
+    include Kontena::Cli::Common
+    include Kontena::Cli::GridOptions
+
+    parameter "NAME", "Stack name"
+    parameter "LABEL ...", "Labels"
+
+    requires_current_master
+    requires_current_master_token
+
+    def execute
+      original_labels = fetch_master_data(name)
+      update_stack(name, { 'labels' => (original_labels - label_list) })
+    end
+
+    private
+
+    def update_stack(name, data)
+      client.patch(stack_url(name), data)
+    end
+
+    def stack_url(name)
+      "stacks/#{current_grid}/#{name}"
+    end
+
+    def fetch_master_data(stackname)
+      original_data = client.get(stack_url(stackname))
+      # ensure we always return either labels or an empty array
+      original_data['labels'] || []
+    end
+  end
+end
