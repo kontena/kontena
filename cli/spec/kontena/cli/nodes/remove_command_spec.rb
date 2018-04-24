0f28b5ccfb422c730e75abae4968838485d2224f
Lauri Nevala <lauri.nevala@gmail.com>
2018-04-24 16:10:10 +0300

    Add more tests

--- cli/spec/kontena/cli/nodes/remove_command_spec.rb
+++ cli/spec/kontena/cli/nodes/remove_command_spec.rb
@@ -47,10 +47,22 @@ describe Kontena::Cli::Nodes::RemoveCommand do
       expect(client).to receive(:get).with('nodes/test-grid/node-1').and_return(node)
     end
 
+    context 'when provider label is set' do
+      it 'returns error message based on provider' do
+        expect{subject.run(['node-1'])}.to exit_with_error.and output(" [error] Node node-1 is still connected. You should terminate the node instead: kontena cloud node terminate node-1\n").to_stderr
+      end
+    end
+
+    context 'when provider is not set' do
+      it 'returns error message based on provider' do
+        node['labels'] = []
+        expect{subject.run(['node-1'])}.to exit_with_error.and output(" [error] Node node-1 is still connected. You must terminate the node before removing it\n").to_stderr
+      end
+    end
+
     it 'does not remove the node' do
       expect(client).not_to receive(:delete)
-
-      expect{subject.run(['node-1'])}.to exit_with_error.and output(" [error] Node node-1 is still connected. You should terminate the node instead: kontena cloud node terminate node-1\n").to_stderr
+      expect{subject.run(['node-1'])}.to exit_with_error
     end
   end
 end
