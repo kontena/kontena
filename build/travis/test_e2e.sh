af758751d1dd7552a5060db745d1951cb4d709a4
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-05-07 11:49:07 +0300

    Export problem?

--- build/travis/test_e2e.sh
+++ build/travis/test_e2e.sh
@@ -3,7 +3,9 @@ set -ue
 
 if which ruby >/dev/null && which gem >/dev/null; then
   PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-  GEM_PATH="$(ruby -rubygems -e 'puts Gem.user_dir');$GEM_PATH"
+  GEM_PATH="$(ruby -rubygems -e 'puts Gem.user_dir'):$GEM_PATH"
+  export PATH
+  export GEM_PATH
 else
   echo "'which ruby' or 'which gem' failed"
   exit 1
@@ -11,9 +13,11 @@ fi
 
 cd cli && \
   gem build kontena-cli.gemspec && \
-  gem install -N --user-install *.gem && \
-  kontena -v && \
-  cd ..
+  gem install -N --user-install *.gem
+
+kontena -v || (echo "Can't run kontena -v"; exit 1)
+
+cd ..
 
 # Get some usable layers before compose builds
 docker pull kontena/agent:edge
