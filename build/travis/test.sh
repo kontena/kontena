86199db4b1ce5a35ca7d68ff1fdeca997a9f52b4
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-08-09 14:54:10 +0300

    Ignore CVE-2018-1000539 (#3364)
    * Ignore CVE-2018-1000539 for now
    
    * Cant comment on multi line command like that

--- build/travis/test.sh
+++ build/travis/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ue
 
+ # CVE-2018-1000539 is a vulnerability in JSON-JWT, used by ancient Acme::Client for sending requests, not for validating.
 cd $TEST_DIR && \
   bundle install --path vendor/bundle && \
-  bundle audit check --update && \
+  bundle audit check --update --ignore CVE-2018-1000539 && \
   bundle exec rspec spec/
