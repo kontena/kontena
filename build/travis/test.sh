0610d68a93e997650c2dd7ee876454dc49e4f3bd
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-08-09 14:18:56 +0300

    Ignore CVE-2018-1000539 for now

--- build/travis/test.sh
+++ build/travis/test.sh
@@ -3,5 +3,5 @@ set -ue
 
 cd $TEST_DIR && \
   bundle install --path vendor/bundle && \
-  bundle audit check --update && \
+  bundle audit check --update --ignore CVE-2018-1000539 && \ # ignore vulnerability in JSON-JWT, used by ancient Acme::Client for sending requests, not for validating.
   bundle exec rspec spec/
